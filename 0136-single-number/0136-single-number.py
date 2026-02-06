adf8cbe015ce337777857325daad2cfa741be50d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-02-06 16:55:29 +0300

    Time: 4287 ms (5.01%), Space: 21.1 MB (36.81%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,23 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums_to_frequency = {}
+        # nums_to_frequency = {}
+        
+        # for i in nums:
+        #     if i not in nums_to_frequency:
+        #         nums_to_frequency[i] = 1
+        #     else:
+        #         nums_to_frequency[i] += 1
+
+        # for key, value in nums_to_frequency.items():
+        #     if value == 1:
+        #         return key
+
+
+        # { 1: 1, 3: 2} frequency: nums
+
 
         for i in nums:
-            if i not in nums_to_frequency:
-                nums_to_frequency[i] = 1
-            else:
-                nums_to_frequency[i] += 1
-        for key, value in nums_to_frequency.items():
-            if value == 1:
-                return key
-                
+            count = nums.count(i)
+            if count == 1:
+                return i
+
