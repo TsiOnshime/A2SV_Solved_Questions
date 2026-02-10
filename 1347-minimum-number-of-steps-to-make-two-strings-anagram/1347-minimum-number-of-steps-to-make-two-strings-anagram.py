02f0807b1ef9f1a7b6f0737a92c424daebeabaa4
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-02-10 20:44:23 +0300

    Time: 66 ms (70%), Space: 19.7 MB (51.94%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -3,17 +3,14 @@ class Solution:
         s_counter = Counter(s)
         t_counter = Counter(t)
 
+
         in_s = s_counter - t_counter
+
        
         changing_letters = [values for values in in_s.values()]
 
-        print(changing_letters)
         return sum(changing_letters)
-        # print(s_counter - t_counter)
-        # print(t_counter - s_counter)
 
-        # print((s_counter - t_counter).values())
-        # print((t_counter - s_counter).values())
 
      
 
