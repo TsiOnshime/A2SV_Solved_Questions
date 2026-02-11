521d0ed9237a230579426fc9ca1fbd10f4d56523
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-02-11 08:41:39 +0300

    Time: 39 ms (94.11%), Space: 36.7 MB (30.06%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -9,9 +9,9 @@ class Solution:
         for num in set_nums:
             #check if it is the start of a sequence
             if (num -1) not in set_nums:
-                length = 0
+                length = 1
                 while (num + length ) in set_nums:
                     length += 1
-                    max_length = max(length, max_length)
+                max_length = max(length, max_length)
         return max_length
             
