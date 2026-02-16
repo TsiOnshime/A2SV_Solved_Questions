a700ab8563cca0ecaa26f8cca8baddfb8c9f0fe0
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-02-16 14:27:49 +0300

    Time: 15 ms (83.82%), Space: 19.3 MB (38.73%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -2,38 +2,38 @@ class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
 
         
-        # dic = {}
-        # for i, num in enumerate(nums):
-        #     dic.setdefault(num, []).append(i)
+        dic = {}
+        for i, num in enumerate(nums):
+            dic.setdefault(num, []).append(i)
 
 
-
-
-        # count = 0
-        # for key, value in dic.items():
-        #     if len(dic[key]) == 1:
-        #         continue
-        #     for i in range(len(dic[key]) - 1):
-        #         j = i + 1
-        #         while j < len(dic[key]):
-        #             if (value[i] * value[j]) % 2 == 0:
-        #                 count += 1
-        #                 j += 1
-        #                 continue
-        #             if j == len(dic[key]) - 1 & i != len(dic[key]) - 2:
-        #                 break
-        #             j += 1
-        # return count
 
 
         count = 0
-
-        for i in range(len(nums)):
-            
-            for j in range(i + 1, len(nums)):
-
-                if nums[i] == nums[j] and (i * j) % k == 0:
-                    count += 1
+        for key, value in dic.items():
+            if len(dic[key]) == 1:
                 continue
-
+            for i in range(len(dic[key]) - 1):
+                j = i + 1
+                while j < len(dic[key]):
+                    if (value[i] * value[j]) % k == 0:
+                        count += 1
+                        j += 1
+                        continue
+                    if j == len(dic[key]) - 1 & i != len(dic[key]) - 2:
+                        break
+                    j += 1
         return count
+
+
+        # count = 0
+
+        # for i in range(len(nums)):
+            
+        #     for j in range(i + 1, len(nums)):
+
+        #         if nums[i] == nums[j] and (i * j) % k == 0:
+        #             count += 1
+        #         continue
+
+        # return count
