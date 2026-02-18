d3adfa55017b1a50ec3fa0824fcc7771903c532d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-02-18 09:28:02 +0300

    Time: 84 ms (5.1%), Space: 19.4 MB (33.13%) - LeetHub

---
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        _sum = 0
+        row_max = 0
+        col_max = 0
+
+        row = len(grid)
+        col = len(grid[0])
+
+        gridNew = [[0 for i in range(row)]for i in range(col)]
+
+        for r in range(row):
+            for c in range(col):
+                row_max = max(grid[r][0:col])
+                # if r == 0:
+                #     print(grid[r][0:col])
+                
+                col_max = max(list(grid[i][c] for i in range(row)))
+                # if c == 0:
+                #     print(list(grid[i][c] for i in range(row)))
+
+                min_height = min(row_max, col_max)
+                gridNew[r][c] = min_height
+
+                _sum += (gridNew[r][c] - grid[r][c])
+
+        return _sum
+               
+     
+        
+
