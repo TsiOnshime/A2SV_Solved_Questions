16d707759b3f76a367ec6d94249ced3a10948084
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-02-20 19:04:22 +0300

    Time: 0 ms (100%), Space: 19.4 MB (51.03%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,20 +1,56 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         
-        n = len(citations)
+        # [0, 1, 3, 5, 6]
+       # {}
+       
+        # {0:5, 1:4, 3:3, 5:2, 6:1}
+        # h-index = 0
+        # return if key == val and key > h-index:
+        #               h-index = key
+        # 
+     # {1,1,3}
+     # {1:1, 3:1}
+# {6,7,8,5,9}
+# {}
+     # h-idex
+        citations.sort()
+        
+        for i in range(len(citations)):
+            # 0 index ?
+            remaining = len(citations) - i
+            
+            if citations[i] >= remaining:
+                return remaining
+        return 0
+# 0 : 5
+# 1 : 4
+# 2 : 3
+# 3 : 3
+# 4 : 2
+# 5 : 2
+# 6 : 1
+# 
+# [0, 1, 3, 5, 6]
 
-        h = 0
+# 0  1   2  3  4
+# 0 index -> paper
+# ? len - index -> 5 - 0 = 5
+# No 
 
-        for i in range(1, n + 1):
-            _max = 0
+# 1 index -> paper
+# ? len - index -> 5 - 1 = 4
+# No
+
+# 2 index -> paper
+# ? len - index -> 5 - 2 = 3
+# Yes -> Return
+
+# 3 index -> paper
+# ? len - index -> 5 - 3 = 2
+# Yes
+
+# len - idx
+# 
 
 
-            for j in range(n):
-
-                if citations[j] >= i:
-                    _max += 1
-                    h_in = i
-            if _max >= i:
-                h = max(h, h_in)
-
-        return h
