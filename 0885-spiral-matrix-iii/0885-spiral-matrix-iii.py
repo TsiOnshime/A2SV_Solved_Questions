0ff62c8077920e2b99e0f2cfd566d52aa2ff85ba
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-02-23 11:29:17 +0300

    Time: 11 ms (64.92%), Space: 20.4 MB (71.64%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -1,34 +1,25 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
         res = []
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
         elem = rows * cols
 
-        T = rStart
-        B = T + 1
-        L = cStart
-        R = cols - 1
+        r, c = rStart, cStart
+        steps = 1
+        i = 0
 
         while len(res) < elem:
 
-            # left to right
-            if T >= 0:
-                for i in range(L, cols):
-                    res.append([T, i])
-                T -= 1
-                L -= 1
-            # Top to bottom
-            if R < cols:
-                for i in range(T, B+1):
-                    res.append([i, R - 1])
-                
-                R += 1
-            # Right to Left
-            if L >= 0:
-                for i in range(cols - 1, L - 1, - 1):
-                    res.append([B, i])
-                B += 1
-            # Bottom to Top
-            if B < rows:
-                for i in range(B, -1,-1):
-                    res.append([i, L])
-        print(res)
+            for x in range(2):
+                dr, dc = directions[i]
+            
+                for j in range(steps):
+                    
+                    if (0 <= r < rows and 0 <= c < cols):
+                        res.append([r, c])
+                    r, c = r + dr, c + dc
+                i = (i + 1) % 4
+            steps += 1
+        return res
+
