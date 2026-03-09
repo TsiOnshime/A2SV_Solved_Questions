397bbf4b1c422b45fa296cbe1f9acb4363e2ac57
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-09 11:39:35 +0300

    Add solution for Find the Highest Altitude

---
+++ leetcode/1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,14 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        # gain = [-5,1,5,0,-7]
+        # gain[i] means the gain in altitude compared to the previous altitude
+        # he begins at altitude at 0
+        # then he goes down -5 so his altitude is at -5
+        # then he gains 1meter which makes is altitude -4 
+        # then he gains 5 meter which makes his altitude 1
+        # so we will use prefix sum to calculate the altitudes
+        altitudes = [0] * (len(gain) + 1)
+        for i in range(len(gain)):
+            altitudes[i + 1] = altitudes[i] + gain[i] 
+        return max(altitudes)
+
