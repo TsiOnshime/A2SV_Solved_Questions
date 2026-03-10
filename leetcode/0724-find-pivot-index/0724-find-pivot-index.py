3cb60658c42d7a40f528d1c5920cbdd96763c0ef
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-10 07:32:32 +0300

    Add solution for Find Pivot Index

--- leetcode/0724-find-pivot-index/0724-find-pivot-index.py
+++ leetcode/0724-find-pivot-index/0724-find-pivot-index.py
@@ -5,16 +5,13 @@ class Solution:
         # 1 and 3 + 6 + 5 + 6 = 24
         # 1 + 7 = 8 and 6 + 5 + 6 = 17
         # 1 + 7 + 3 = 11 and 5 + 6 = 11
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
+        total = sum(nums)
         left = 0
-        right = 0
+
         for i in range(len(nums)):
-            if i == 0:
-                left = 0 
-            else:
-                left = nums[i - 1]
-            right = nums[len(nums) - 1] - nums[i]
+            right = total - left - nums[i]
             if left == right:
                 return i
+            left += nums[i]
+            
         return -1
