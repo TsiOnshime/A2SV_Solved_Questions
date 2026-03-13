4ee339c498ef122f92f5eddb3fb4450c7a5010d2
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-13 07:01:21 +0300

    Add solution for Number of Students Unable to Eat Lunch

--- leetcode/1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ leetcode/1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,21 +1,12 @@
 class Solution:
     def countStudents(self, students, sandwiches):
-        # [1,1,0,0]
-        #  i
-        # [1,1,0,0,1]
-        #      i
-        j = 0
-        i = 0
-        count = 0
-        while j < len(sandwiches):
-            if students[i] == sandwiches[j]:
-                j += 1
-                
-                count = 0
+        res = len(students)
+        count = Counter(students)
+        for s in sandwiches:
+            if count[s] > 0:
+                res -= 1
+                count[s] -= 1
             else:
-                count += 1
-                students.append(students[i])
-            if count == len(sandwiches) - j:
-                return count
-            i += 1
-        return 0
+                return res
+        return res
+        
