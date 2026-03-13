5b17584d7cc9e19b3a96a612652ed4fc021c2620
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-13 13:42:24 +0300

    Add solution for Minimum Operations to Make Binary Array Elements Equal to One I

--- leetcode/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ leetcode/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,20 +1,18 @@
+from collections import deque
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
+        flipq = deque()
+        count = 0
+ 
+        for i in range(len(nums)):
+            while flipq and flipq[0] + 2 < i:
+                flipq.popleft()
+            
+            if (nums[i] + len(flipq)) % 2 == 0:
+                if i + 2 >= len(nums):
+                    return -1
+                count += 1
+                flipq.append(i)
         
-# what we will do is be greedy think about the first element only the other two don't worry about them
-# we will iterate till the last two elements because we have to have three elements that have to be flipped
-# use a helper function to flip
-# [1,0,0,1,1,0,1,1,1] => [1,0,0,1,1,0,1,1,1] => [1,1,1,0,1,0,1,1,1] => [1,1,1,0,1,0,1,1,1] => [1,1,1,1,0,1,1,1,1] => [1,1,1,1,1,0,0,1,1] => [1,1,1,1,1,1,1,0,1]
-        def flip(number):
-            return 0 if number else 1
-        min_flip = 0
-        for i in range(len(nums) -2):
-            if nums[i] == 0:
-                nums[i] = flip(nums[i])
-                nums[i + 1] = flip(nums[i + 1])
-                nums[i + 2] = flip(nums[i + 2])
-                min_flip += 1
-    
-        if not nums[-1] or not nums[-2]:
-            return -1
-        return min_flip
+        return count
+
