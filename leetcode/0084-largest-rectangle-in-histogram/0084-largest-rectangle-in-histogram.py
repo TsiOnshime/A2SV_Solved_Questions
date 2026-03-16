a49a26934bc89c2102df0c30c7b921b9643c2ba4
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-16 07:39:56 +0300

    Add solution for Largest Rectangle in Histogram

--- leetcode/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ leetcode/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,17 +1,26 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        # when a lesser element comes we pop the element at the top and calculate the max area = we substract (new element's index - the index of the element being popped) * value
+        # after we pop all elements the index of the new element will be changed to the last element's index (which is popped)
+        # then we append the element to the stack
+        # after all this when we finish iterating through the list we may have element in our stack that were not popped because there were no elements lesser than them at the right of them 
+        # so inorder to get the max area they form 
+        # we pop each of them and multiply them with the (top element's index + 1 - their index ) * value
+
         stack = []
-        area  = 0
+        max_area = 0
         for i in range(len(heights)):
-            start = i
+            idx = i
             while stack and stack[-1][1] > heights[i]:
-                index, height = stack.pop()
-                area = max(area, height * abs((i - index)))
-                start = index
-            
-            stack.append([start, heights[i]])
-   
-        for i, h in stack:
-            area = max(area, h * (len(heights) - i))
-      
-        return area
+                popped = stack.pop()
+                idx = popped[0]
+                max_area = max(max_area, (i - idx) * popped[1])
+
+            stack.append([idx, heights[i]])
+
+        if stack:
+            idx = len(heights) - 1
+        while stack:
+            popped = stack.pop()
+            max_area = max(max_area, (idx - popped[0] + 1) * popped[1])
+        return max_area
