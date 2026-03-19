87baf280bd7bdf734f8d459b7440811992d18073
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-19 12:13:33 +0300

    Add solution for Decode String

--- leetcode/0394-decode-string/0394-decode-string.py
+++ leetcode/0394-decode-string/0394-decode-string.py
@@ -1,22 +1,75 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
 
-        for i in range(len(s)):
-            if s[i] != "]":
-                stack.append(s[i])
-            else:
-                substring = ""
-                while stack[-1] != "[":
-                    substring = stack.pop() + substring
-                stack.pop()
-
-                k = ""
-                while stack and stack[-1].isdigit():
-                    k = stack.pop() + k
-
-                stack.append(int(k) * substring)
+# when we find an openning bracket we recurssively call
+# the next function
+# wehn we get ]
+        def decode(i):
+            result = ""
+            k = 0
+            
+            while i < len(s):
+                if s[i].isdigit():
+                    k = k * 10 + int(s[i])
+                
+                elif s[i] == '[':
+                    sub, i = decode(i + 1)
+                    result += sub * k
+                    k = 0
+                
+                elif s[i] == ']':
+                    return result, i
+                
+                else:
+                    result += s[i]
+                
+                i += 1
+            
+            return result, i
         
-        return "".join(stack)
+        return decode(0)[0]
+            
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stack = []
+
+        # for i in range(len(s)):
+        #     if s[i] != "]":
+        #         stack.append(s[i])
+        #     else:
+        #         substring = ""
+        #         while stack[-1] != "[":
+        #             substring = stack.pop() + substring
+        #         stack.pop()
+
+        #         k = ""
+        #         while stack and stack[-1].isdigit():
+        #             k = stack.pop() + k
+
+        #         stack.append(int(k) * substring)
+        
+        # return "".join(stack)
             
                     
