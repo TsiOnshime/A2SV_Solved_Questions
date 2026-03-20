83888fb048ba67eb9034f76538dc4ddd37b4e9aa
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-20 06:17:49 +0300

    Add solution for Count Good Numbers

---
+++ leetcode/1922-count-good-numbers/1922-count-good-numbers.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        odd = n // 2
+        even = n - odd
+        mod = 10 ** 9 + 7
+
+        def power(x, y):
+
+            if y == 0:
+                return 1
+            
+            res = power(x, y//2)
+            res *= res
+            res %= mod
+            if y % 2:
+                res *= x
+            res %= mod
+
+            return res
+        
+        possibilities = (power(5 , even) * power(4, odd)) % mod
+
+        return possibilities
+
+            
