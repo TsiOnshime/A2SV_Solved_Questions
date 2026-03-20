41f606714ccfbc31cbedd4ad22edabac153833ac
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-20 10:29:03 +0300

    Time: 3 ms (53%), Space: 20.7 MB (17.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic = {}
+        hashMap = {nums[i]: i for i in range(len(nums))}
+        # {2:0, 7:1, 11:2, 15:3}
+        # {3:1}
+        for i in range(len(nums)): # 0    1
+            complement = target - nums[i] # 3
+            if complement in hashMap and hashMap[complement] != i:
+                return [i,hashMap[complement]]
 
-        for val in nums:
-            dic[val] = target - val
-            if dic[val] == val and nums.count(val) == 1:
-                continue
-            elif dic[val] in nums:
-                if dic[val] == val:
-                    first = nums.index(val)
-                    nums.remove(val)
-                    second = nums.index(dic[val]) + 1
-                    return[first, second]
-                return[nums.index(val), nums.index(dic[val])]
-        
-        
-        
+        return []
