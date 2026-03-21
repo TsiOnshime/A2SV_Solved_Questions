7deb7da6844f7d4a81de5ca91531612b1ae89dbe
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-21 18:14:24 +0300

    Add solution for Flip Square Submatrix Vertically

---
+++ leetcode/3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
+        for i in range(y, y + k):
+            t = x
+            b = x + k - 1
+            while t < b:
+                grid[t][i], grid[b][i] = grid[b][i], grid[t][i]
+
+                t += 1
+                b -= 1
+        return grid
+            
