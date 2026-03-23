4de239ea8558244ea2f5b80e6e4812e22f0cde19
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-23 05:44:59 +0300

    Add solution for Construct Binary Tree from Preorder and Inorder Traversal

---
+++ leetcode/0105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/0105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        
+        def tree(preorder, inorder):
+            # check if we're at a leaf node
+            if not preorder or not inorder:
+                return None
+
+
+            # get the parent node
+            parent = preorder[0]
+            # initialize our node
+            binary_tree = TreeNode(parent)
+            # get the indext in which the left subtree and right subtree split for that particular parent node
+            mid = inorder.index(parent)
+
+            binary_tree.left = tree(preorder[1:mid + 1], inorder[:mid + 1])
+            binary_tree.right = tree(preorder[mid + 1:], inorder[mid + 1:])
+
+            return binary_tree
+        
+        return tree(preorder, inorder)
