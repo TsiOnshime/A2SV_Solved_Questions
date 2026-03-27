e1f2bd2d339e1db11a055e790f0662116c46a1f2
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-27 10:55:45 +0300

    Add solution for Permutations

--- leetcode/0046-permutations/0046-permutations.py
+++ leetcode/0046-permutations/0046-permutations.py
@@ -1,20 +1,38 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        path = []
-        ans = []
-
-        def backtrack(nums):
-            if not nums:
-                ans.append(path.copy())
-
-            for i in nums:
-                path.append(i)
-
-                potential_candidate = nums[:]
-                potential_candidate.remove(i)
-                backtrack(potential_candidate)
-                path.pop()
         
 
-        backtrack(nums)
-        return ans
+       
+        def is_valid_state(state):
+            # check if it is a valid state
+            if len(state) == len(nums):
+                return True
+
+        
+        def get_candidates(state):
+            res = []
+            for i in range(len(nums)):
+                if used[i] == False:
+                    res.append(nums[i])
+            return res
+
+        
+        def search(state, solutions):
+            if is_valid_state(state):
+                solutions.append(state[:])
+            
+            for candidate in get_candidates(state):
+                state.append(candidate)
+                used[nums.index(candidate)] = True
+                search(state, solutions)
+                state.remove(candidate)
+                used[nums.index(candidate)] = False
+        
+        
+        used = [False] * len(nums)
+        state = []
+        solutions = []
+        search(state, solutions)
+        return solutions
+            
+        
