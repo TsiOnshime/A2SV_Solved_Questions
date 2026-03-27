c77a22dacc5b0ca7a9f88f42db9faed5de3b37dd
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-27 12:29:51 +0300

    Add solution for Combinations

--- leetcode/0077-combinations/0077-combinations.py
+++ leetcode/0077-combinations/0077-combinations.py
@@ -1,21 +1,33 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         
-        path = []
-        output = []
-        def backtrack(i):
-            if len(path) == k:
-                output.append(path[:])
-                return 
-            if i > n:
-                return 
-            path.append(i)
-            backtrack(i + 1)
-            path.pop()
-
-            backtrack(i + 1)
         
-        backtrack(1)
-        return output
+        def is_valid_state(state, j):
+            # check the validity
+            if len(state) == k:
+                return True
+
+    
+        
+        def get_candidates(j):
+            res = []
+            for i in range(j, n + 1):
+                res.append(i)
+            return res
+
+        def search(j):
+            if is_valid_state(state, j):               
+                solution.append(state.copy())
+                return
+            
+            for candidate in get_candidates(j):
+                state.append(candidate)
+                search(candidate + 1)
+                state.pop()
+        
+        solution = []
+        state = []
+        search(1)
+        return solution
 
             
