a5dcb22028fb6f359b0ab734258e7831889f9148
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-29 20:59:50 +0300

    Add solution for Sign of the Product of an Array

---
+++ leetcode/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        
+
+        def signFunc(x):
+            if x < 0:
+                return -1
+            elif x > 0:
+                return 1
+            else:
+                return 0
+        
+        _product = 1
+        for i in nums:
+            _product *= i
+        return signFunc(_product)
