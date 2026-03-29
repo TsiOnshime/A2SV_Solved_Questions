9f68ac05f4395bb6d8e1f13bf5431a1f7ad28ff7
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-29 19:25:05 +0300

    Add solution for Check if the Sentence Is Pangram

--- leetcode/1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ leetcode/1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,10 +1,15 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        freq = [0] * 26
+        # freq = [0] * 26
 
-        offset = ord('a')
-        for i in sentence:
-            freq[ord(i) - offset] += 1
-        if 0 not in freq:
+        # offset = ord('a')
+        # for i in sentence:
+        #     freq[ord(i) - offset] += 1
+        # if 0 not in freq:
+        #     return True
+        # return False
+
+        count = Counter(sentence)
+        if len(count) == 26:
             return True
         return False
