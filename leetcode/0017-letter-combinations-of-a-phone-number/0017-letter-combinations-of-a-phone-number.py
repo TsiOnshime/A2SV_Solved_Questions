bc95aad016b3409a468eb8a3a41838510315f53c
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-03-30 09:34:10 +0300

    Add solution for Letter Combinations of a Phone Number

--- leetcode/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ leetcode/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,28 +1,19 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         mapping = {'2': ["a", "b", "c"], '3': ["d", "e", "f"], '4':["g", "h", "i"], '5':["j", "k","l"], '6':["m", "n", "o"], '7':["p", "q", "r","s"], '8':["t", "u", "v"], '9':["w", "x", "y", "z"]}
+
         solution = []
         state = []
 
-        def solve():
-
-            search(0)
-            return solution
-        def is_valid_state(i):
+        def search(i):
             if len(state) == len(digits):
                 solution.append("".join(state.copy()))
-            return i == len(digits)
-
-        def get_candidates(i):
-            return mapping[digits[i]]
-        
-        def search(i):
-            if is_valid_state(i):
                 return
 
-            for candidate in get_candidates(i):         
-                state.append(candidate)
+            possibilities = mapping[digits[i]]
+            for char in possibilities:
+                state.append(char)
                 search(i + 1)
                 state.pop()
-        return solve()
-
+        search(0)
+        return solution
