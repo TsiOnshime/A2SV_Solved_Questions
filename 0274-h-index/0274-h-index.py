43387f946c642d5217ad752a2d07db160114bede
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-01 12:47:27 +0300

    Time: 0 ms (100%), Space: 19.5 MB (45.85%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,56 +1,20 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         
-        # [0, 1, 3, 5, 6]
-       # {}
-       
-        # {0:5, 1:4, 3:3, 5:2, 6:1}
-        # h-index = 0
-        # return if key == val and key > h-index:
-        #               h-index = key
-        # 
-     # {1,1,3}
-     # {1:1, 3:1}
-# {6,7,8,5,9}
-# {}
-     # h-idex
-        citations.sort()
+        count = [0] * (len(citations) + 1)
+        print(count)
+
+        for citation in citations:
+            if citation >= len(count):
+                count[-1] += 1
+            else:
+                count[citation] += 1
+        print(count)
+
         
-        for i in range(len(citations)):
-            # 0 index ?
-            remaining = len(citations) - i
-            
-            if citations[i] >= remaining:
-                return remaining
+        paper_count = 0
+        for i in range(len(count) - 1, -1, -1):
+            paper_count += count[i]
+            if paper_count >= i:
+                return i
         return 0
-# 0 : 5
-# 1 : 4
-# 2 : 3
-# 3 : 3
-# 4 : 2
-# 5 : 2
-# 6 : 1
-# 
-# [0, 1, 3, 5, 6]
-
-# 0  1   2  3  4
-# 0 index -> paper
-# ? len - index -> 5 - 0 = 5
-# No 
-
-# 1 index -> paper
-# ? len - index -> 5 - 1 = 4
-# No
-
-# 2 index -> paper
-# ? len - index -> 5 - 2 = 3
-# Yes -> Return
-
-# 3 index -> paper
-# ? len - index -> 5 - 3 = 2
-# Yes
-
-# len - idx
-# 
-
-
