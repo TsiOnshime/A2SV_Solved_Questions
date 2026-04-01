55ed4ea95439b9c4456ab675af5ce719abb65621
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-01 12:47:28 +0300

    Add solution for H-Index

---
+++ leetcode/0274-h-index/0274-h-index.py
@@ -0,0 +1,20 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        count = [0] * (len(citations) + 1)
+        print(count)
+
+        for citation in citations:
+            if citation >= len(count):
+                count[-1] += 1
+            else:
+                count[citation] += 1
+        print(count)
+
+        
+        paper_count = 0
+        for i in range(len(count) - 1, -1, -1):
+            paper_count += count[i]
+            if paper_count >= i:
+                return i
+        return 0
