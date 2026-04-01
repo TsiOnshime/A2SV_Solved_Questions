2ce19f735b11f5b9940c0b1946ca5ae31dec3d1e
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-01 10:41:20 +0300

    Add solution for Koko Eating Bananas

---
+++ leetcode/0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        
+
+        def possible(k):
+            time = 0
+            for pile in piles:
+                time += (pile + k - 1) // k 
+            return time
+# l = 4, u = 30
+# mid = 13
+# time = 4 [4, 11, 23, 4, 20]
+#
+        l = 1
+        u = max(piles)
+
+        while l <= u:
+            mid = l + (u - l)// 2
+           
+            needed = possible(mid)
+
+            if needed > h:
+                l = mid + 1
+            else:
+                u = mid - 1
+        return l
+o = Solution()
+print(o.minEatingSpeed([30,11,23,4,20], 5))
