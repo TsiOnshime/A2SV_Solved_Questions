66162b4a2d5f4480165faaf6d9d6ea729f78f881
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-02 06:35:35 +0300

    Add solution for Fair Distribution of Cookies

---
+++ leetcode/2305-Fair-Distribution-of-Cookies/2305-Fair-Distribution-of-Cookies.py
@@ -0,0 +1,30 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+
+        min_unfairness = float('inf')
+        n = len(cookies)
+        children = [0] * k
+
+        def solve():
+            nonlocal min_unfairness
+            search(0)
+            return min_unfairness
+
+
+        def is_valid_state(i):
+            nonlocal min_unfairness
+            if i == n or max(children) >= min_unfairness:
+                return True
+
+        def search(i):
+            nonlocal min_unfairness
+
+            if is_valid_state(i):
+                min_unfairness = min(min_unfairness, max(children))
+                return
+            
+            for j in range(len(children)):
+                children[j] += cookies[i]
+                search(i + 1)
+                children[j] -= cookies[i]
+        return solve()
