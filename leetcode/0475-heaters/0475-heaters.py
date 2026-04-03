d7b317a506519f59c5ccff839ed03330691b507a
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-03 10:10:02 +0300

    Add solution for Heaters

--- leetcode/0475-heaters/0475-heaters.py
+++ leetcode/0475-heaters/0475-heaters.py
@@ -1,29 +1,30 @@
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         
-        houses.sort()
+        heaters.append(float('inf'))
+        heaters.append(float('-inf'))
+
         heaters.sort()
-
-        def calc_distance(i):
-
-            l = 0
-            r = len(heaters) - 1
-
-            min_distance = float('inf')
-
-            while l <= r:
-                mid = l + (r - l)//2
-                min_distance = min(min_distance, abs(heaters[mid] - houses[i]))
-
-
-                if heaters[mid] >= houses[i]:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            return min_distance
+        houses.sort()
 
         radius = 0
+        min_distance = float('inf')
+
+        j = 0
         for i in range(len(houses)):
-            radius = max(radius, calc_distance(i))
+
+            while heaters[j] < houses[i]:
+                j += 1
+            if j > len(heaters):
+                break
+
+            left_dist = float('inf') if heaters[j - 1] == float('-inf') else abs(heaters[j-1] - houses[i])
+            right_dist = float('inf') if heaters[j] == float('inf') else abs(heaters[j] - houses[i])
+            min_distance = min(left_dist, right_dist)
+
+            radius = max(radius, min_distance)
 
         return radius
+
+
+
