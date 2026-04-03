237763113e0cb1408e1451d63df0b22558c0f5b7
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-03 18:38:27 +0300

    Add solution for Word Break II

---
+++ leetcode/0140-Word-Break-II/0140-Word-Break-II.py
@@ -0,0 +1,28 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        # to make my lookup faster
+        wordDict = set(wordDict)
+
+        sentences = []
+        state = []
+
+        def solve():
+            search(0)
+            return sentences
+
+        # i => my start index
+        # j => my end index
+        def search(i):
+
+            if i == len(s):
+                sentences.append(" ".join(state.copy()))
+                return
+
+            for j in range(i + 1, len(s) + 1):
+                substring = s[i:j]
+
+                if substring in wordDict:
+                    state.append(substring)
+                    search(j)
+                    state.pop()
+        return solve()
