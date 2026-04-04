e98de76d740679a0b4af83f50d2dd9361d2ced4b
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-04 17:00:40 +0300

    Add solution for Binary Tree Maximum Path Sum

---
+++ leetcode/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        
+        res = float('-inf')
+
+
+        def recurse(curr):
+            nonlocal res
+
+            if not curr:
+                return 0
+            
+            leftChild = recurse(curr.left)
+            rightChild = recurse(curr.right)
+
+            res = max(res, curr.val + max(leftChild, 0) + max(rightChild, 0))
+
+            return max(curr.val, curr.val + max(leftChild, rightChild))
+
+        recurse(root)
+        return res
+
