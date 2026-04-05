cb47a8721008ef3ccbeaa2e63944dd123364f0f2
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-05 20:46:14 +0300

    Time: 15 ms (94.18%), Space: 25.3 MB (86.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,39 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # prefix_prod = [1, 1, 2, 6]
-        # surrix_prod = [24,12,4,1]
-        # prod = [24, 12, 8, 6]
-
-        # prefix_prod = [1] * len(nums)
-        # for i in range(1, len(nums)):
-        #     prefix_prod[i] = nums[i - 1] * prefix_prod[i - 1]
-
-        # suffix_prod = [1] * len(nums)
-        # for i in range(len(nums) - 2, -1, -1):
-        #     suffix_prod[i] = nums[i + 1] * suffix_prod[i + 1]
-
-        # prod = [0] * len(nums)
-        # for i in range(len(nums)):
-        #     prod[i] = prefix_prod[i] * suffix_prod[i]
-        
-        # return prod
-
-        prod = [1] * len(nums)
+        output = [1] * len(nums)
 
         prefix = 1
+
         for i in range(len(nums)):
-            prod[i] = prefix
+            output[i] = prefix
             prefix *= nums[i]
         
         postfix = 1
+
         for i in range(len(nums) - 1, -1, -1):
-            prod[i] *= postfix
+            output[i] *= postfix
             postfix *= nums[i]
-        return prod
-
-        # prod = [1, 1, 1, 1]   nums= [1,2,3,4]
-        # prefix = 1 => 1 => 2 => 6 => 24
-        # prod = [1, 1, 2, 6]
-        # postfix = 1 => 4 => 12 => 24 => 24
-        # prod = [24, 12, 8, 6]
-
+        
+        return output
