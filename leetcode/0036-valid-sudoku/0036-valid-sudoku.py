aab79278c61fd14d284cbdf7dc48813e8e00593e
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-06 13:43:54 +0300

    Add solution for Valid Sudoku

--- leetcode/0036-valid-sudoku/0036-valid-sudoku.py
+++ leetcode/0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,15 +1,26 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [[False] * 9 for _ in range(9)]
-        cols = [[False] * 9 for _ in range(9)]
-        boxes = [[False] * 9 for _ in range(9)]
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
 
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != '.':
-                    num = ord(board[i][j]) - ord('1')
-                    boxIndex = (i // 3) * 3 + (j // 3)
-                    if rows[i][num] or cols[j][num] or boxes[boxIndex][num]:
-                        return False
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = True
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or 
+                board[r][c] in cols[c] or 
+                board[r][c] in squares[(r//3, c//3)]):
+
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+
         return True
+
+
+
+
+
