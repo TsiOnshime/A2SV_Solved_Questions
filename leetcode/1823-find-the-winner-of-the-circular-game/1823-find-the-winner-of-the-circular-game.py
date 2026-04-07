445e511bea9a00d3078dfd69f39ded7f9d606b12
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-07 06:14:54 +0300

    Add solution for Find the Winner of the Circular Game

--- leetcode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ leetcode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,18 +1,13 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
 
-        queue = deque()
+        def helper(n, k):
+            if n == 1:
+                return 0
+            
+            return (helper(n - 1, k) + k) % n
 
-        for i in range(1, n + 1):
-            queue.append(i)
-
-        while len(queue) > 1:
-            for i in range(k - 1):
-                queue.append(queue.popleft())
-
-            queue.popleft()
-
-        return queue[0]
+        return helper(n, k) + 1
 
             
                  
