fed996d0da2c18babb62e610b9f9cb723e67fdf7
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-09 13:37:57 +0300

    Add solution for Binary Search

--- leetcode/0704-binary-search/0704-binary-search.py
+++ leetcode/0704-binary-search/0704-binary-search.py
@@ -1,18 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # [1,2, 5, 6]
-        # target = 3
-        
-        l = 0
-        r = len(nums) - 1
-        while l <= r:
-            mid = (l + r) // 2
+        l, r = 0, len(nums) - 1
 
-            if target == nums[mid]:
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
                 return mid
-            elif target > nums[mid]:
-                l = mid + 1
-            else:
+            elif nums[mid] > target:
                 r = mid - 1
+            else:
+                l = mid + 1
         
         return -1
