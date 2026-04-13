95e3e7381e7b836cd22039889ca906346a1c8d41
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-13 11:33:46 +0300

    Time: 3 ms (46.9%), Space: 19.7 MB (23.78%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,34 +1,31 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        total = len(nums1) + len(nums2)
+    def findMedianSortedArrays(self, A: List[int], B: List[int]) -> float:
+
+        if len(A) > len(B):
+            A, B = B, A
+
+        l, r = 0, len(A) - 1
+        total = len(A) + len(B)
         half = total // 2
-        def binarySearch(first):
+        while True:
+            i = l + (r - l) // 2
+            j = half -  i - 2
 
-            l = 0
-            r = len(first) - 1
+            Aleft = A[i] if i >= 0 else float('-inf')
+            Aright = A[i + 1] if i + 1 < len(A) else float('inf')
+            Bleft = B[j] if j >= 0 else float('-inf')
+            Bright = B[j + 1] if j + 1 < len(B) else float('inf')
 
-            while True:
-                i = (l + r) // 2 # for pointing to first array
-                j = half - i - 2
-
-                firstLeft = first[i] if i >= 0 else float('-inf')
-                firstRight = first[i + 1] if i + 1 < len(first) else float('inf')
-                secondLeft = second[j] if j >= 0 else float('-inf')
-                secondRight = second[j + 1] if j + 1 < len(second) else float('inf')
-
-                if firstLeft <= secondRight and secondLeft <= firstRight:
-                    if total % 2:
-                        return min(secondRight, firstRight)
-                    return (max(firstLeft, secondLeft) + min(firstRight, secondRight)) / 2
-                elif firstLeft > secondRight:
-                    r = i - 1
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2:
+                    return min(Aright, Bright)
                 else:
-                    l = i + 1
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else: 
+                l = i + 1
 
-        if len(nums1) < len(nums2):
-            second = nums2
-            return binarySearch(nums1)
-        else:
-            second = nums1
-            return binarySearch(nums2)
+                
+
 
