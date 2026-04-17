73a33fb0140d6a5ccbde0108a0bc84e3057e0e57
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-17 18:11:48 +0300

    Add solution for Next Greater Element I

--- leetcode/0496-next-greater-element-i/0496-next-greater-element-i.py
+++ leetcode/0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,17 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashMap = defaultdict(lambda:-1)
         stack = []
-        dictt = {}
 
-        for i in range(len(nums2)-1, -1,-1):
-            while stack and stack[-1] <= nums2[i]:
-                stack.pop()
-            
-            dictt[nums2[i]] = stack[-1] if stack else -1
-            stack.append(nums2[i])
-        
-        for i in range(len(nums1)):
-            nums1[i] = dictt[nums1[i]]
-        return nums1
+        for num in nums2:
+            while stack and stack[-1] < num:
+                elem = stack.pop() 
+                hashMap[elem] = num
+            stack.append(num)
+
+        return [hashMap[i] for i in nums1]
+
         
 
