05c7907799d981a815e5870ec29e19fa0433c778
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-23 20:19:37 +0300

    Time: 1404 ms (36.28%), Space: 88.4 MB (5.37%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,26 +1,36 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        res = [0] * len(nums)
-        sort = []
-        def binarySearch(sort, num):
-            l = 0
-            r = len(sort) - 1
-            ans = l
-            while l <= r:
-                mid = l + (r - l)//2
-                if sort[mid] >= num:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            
-            sort.insert(l, num)
-            
-            return l
-
-        for i in range(len(nums) - 1, -1, -1):
-            rank = binarySearch(sort, nums[i])
-            res[i] = rank
         
-    
-        return res
+        ans = [0] * len(nums)
+        numsPair = []
+        for i, val in enumerate(nums):
+            numsPair.append([i, val])
+
+        def divide(l, r):
+            if l == r:return 
+            mid = l + (r - l)//2
+            divide(l, mid)
+            divide(mid + 1, r)
+            merge(l, mid, r)
+
+        def merge(l, mid, r):
+            nonlocal ans
+
+            temp = []
+            low = l
+            high = mid + 1
+            while low <= mid and high <= r:
+                if numsPair[low][1] > numsPair[high][1]:
+                    temp.append(numsPair[low])
+                    ans[numsPair[low][0]] += r - high + 1
+                    low += 1
+                else:
+                    temp.append(numsPair[high])
+                    high += 1
+            temp.extend(numsPair[low:mid+1])
+            temp.extend(numsPair[high:r+1])
+            for i in range(l, r+1):
+                numsPair[i] = temp[i - l]
+        divide(0, len(nums) - 1)
+        return ans
 
