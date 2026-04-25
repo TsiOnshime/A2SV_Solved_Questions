45ee3257ae44f0aaed2b2b1ed148a14fa06c1b96
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-25 15:36:17 +0300

    Add solution for Sort Colors

---
+++ leetcode/0075-Sort-Colors/0075-Sort-Colors.py
@@ -0,0 +1,32 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def divide(l, r):
+            if l == r:
+                return 
+            mid = l + (r - l)//2
+            divide(l, mid)
+            divide(mid + 1, r)
+            merge(l, mid, r)
+        def merge(l, mid, r):
+            temp = []
+            low = l
+            high = mid + 1
+
+            while low <= mid and high <= r:
+                if nums[low] <= nums[high]:
+                    temp.append(nums[low])
+                    low += 1
+                else:
+                    temp.append(nums[high])
+                    high += 1
+            temp.extend(nums[low:mid + 1])
+            temp.extend(nums[high:r + 1])
+
+            for i in range(l, r + 1):
+                nums[i] = temp[i - l]
+
+        divide(0, len(nums) - 1)
+        return nums
