c7b20ac5e42107b4795942adb999597e47736594
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-27 15:23:28 +0300

    Add solution for All Paths From Source to Target

---
+++ leetcode/0797-All-Paths-From-Source-to-Target/0797-All-Paths-From-Source-to-Target.py
@@ -0,0 +1,19 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        res = []
+        def dfs(start, dest, path):
+            path.append(start)
+            if start == dest:
+                res.append(path.copy())
+
+            else:
+                for neigh in graph[start]:
+                    dfs(neigh, dest, path)
+
+            path.pop()
+
+        dfs(0, len(graph) - 1, [])
+        return res
+
+        # time: O(2**N * N)
+        # space: O(N)
