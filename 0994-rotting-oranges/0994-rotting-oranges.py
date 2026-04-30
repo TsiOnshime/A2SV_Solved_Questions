ad9e2aae27228d996aa7ff44336698429ba0929d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-04-30 10:16:21 +0300

    Time: 3 ms (76.06%), Space: 19.3 MB (79.05%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,44 +3,32 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         queue = deque()
         visited = set()
-        noRot = False
-
-       
-
-        def rotFruit(r, c):
-            if r not in range(rows) or c not in range(cols) or (r, c) in visited or grid[r][c] != 1:
-                return 
-            queue.append((r, c))
-            visited.add((r, c))
+        fresh = 0
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
                     queue.append((r, c))
                     visited.add((r, c))
-                if grid[r][c] == 1:
-                    noRot = True
-        if not noRot:
-            return 0
-        
-        
-        
-        
-        time = 0
-
+                elif grid[r][c] == 1:
+                    fresh += 1
+        minute = 0
         while queue:
-            for i in range(len(queue)):
-                r, c = queue.popleft()
-                grid[r][c] = 2
-                rotFruit(r - 1, c)
-                rotFruit(r + 1, c)
-                rotFruit(r , c- 1)
-                rotFruit(r, c + 1)
+            n = len(queue)
+            for i in range(n):
+                cr, cc = queue.popleft()
+                for dr, dc in directions:
+                    nr, nc = cr + dr, cc + dc
+                    if nr < 0 or nr == rows or nc < 0 or nc == cols or (nr, nc) in visited or grid[nr][nc] != 1:
+                        continue
+                    grid[nr][nc] = 2
+                    queue.append((nr, nc))
+                    visited.add((nr, nc))
+                    fresh -= 1
+            minute += 1
+        
+        if fresh > 0:
+            return -1
+        return max(0, minute - 1)
 
-            time += 1
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    return -1
-
-        return time -1
