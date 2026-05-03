d50562989b6cefb00edda67e621d728010c2f88e
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-03 17:53:02 +0300

    Time: 55 ms (76.11%), Space: 47.5 MB (66.97%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,25 +1,36 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         adj = defaultdict(list)
-        ancestorList = []
+        indegree = [0] * n
+        ancestorList = [set() for i in range(n)]
+        queue = deque()
+
         for u, v in edges:
-            adj[v].append(u)
-        def dfs(start, visited):
-            visited[start] = 1
-            for neigh in adj[start]:
-                if not visited[neigh]:
-                    dfs(neigh, visited)
-        for i in range(n):
-            visited = [0] * n
-            ancestors = []
-            dfs(i, visited)
+            adj[u].append(v)
+            indegree[v] += 1
 
-            for j in range(n):
-                if j == i:
-                    continue
-                if visited[j]:
-                    ancestors.append(j)
 
-            ancestorList.append(ancestors)
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        order = []
+        while queue:
+            node = queue.popleft()
+            order.append(node)
+            for neigh in adj[node]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    queue.append(neigh)
+    
+        for node in order:
+            for neigh in adj[node]:
+                ancestorList[neigh].add(node)
+                ancestorList[neigh].update(ancestorList[node])
+
+        for i in range(len(ancestorList)):
+            ancestorList[i] = list(sorted(ancestorList[i]))
 
         return ancestorList
+
+        
