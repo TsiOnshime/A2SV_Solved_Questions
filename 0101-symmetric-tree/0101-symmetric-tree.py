3e50d8a70bbbee83912d4e97bd914d999ad8a6a5
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-04 15:11:44 +0300

    Time: 0 ms (100%), Space: 19.4 MB (53.99%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root.left and not root.right:
+            return True
+        if not root.left or not root.right:
+            return False
+        if root.left.val != root.right.val:
+            return False
+        prev = root.left
+        queue1 = deque([root.left])
+        queue2 = deque([root.right])
+        while queue1 and queue2:
+            node1 = queue1.popleft()
+            node2 = queue2.popleft()
+            if not node1 and not node2:
+                continue
+            elif not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            for neigh in [node1.left, node1.right]:
+                
+                queue1.append(neigh)
+            for neigh in [node2.right, node2.left]:
+
+                queue2.append(neigh)
+            
+        if len(queue1) != len(queue2):
+            return False
+
+        return True
