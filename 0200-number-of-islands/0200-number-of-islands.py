34d51e5cee0306ea7e7e41daa78ca0d3fb76340a
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-07 10:53:22 +0300

    Time: 269 ms (19.92%), Space: 26.9 MB (17.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,34 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
         rows, cols = len(grid), len(grid[0])
+        count = 0
         visited = set()
-        islands = 0
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+       
+        def is_valid(r, c):
+           
+            if 0 <= r < rows and 0<= c < cols and (r, c) not in visited and grid[r][c] == "1":
+                return True
+            return False
 
-        def bfs(r, c):
-            q = deque()
-            visited.add((r,c))
-            q.append((r,c))
-
-            while q:
-                row, col = q.popleft()
-                directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-
+        def bfs(start, visited):
+            queue = deque()
+            queue.append(start)
+            visited.add(start)
+            while queue:
+                cr, cc = queue.popleft()
                 for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if (r in range(rows) and 
-                        c in range(cols) and 
-                        grid[r][c] == "1" and 
-                        (r, c) not in visited):
-                        q.append((r, c))
-                        visited.add((r, c))
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    bfs(r, c)
-                    islands += 1
+                    nr, nc = cr + dr, cc + dc
+                    if is_valid(nr, nc):
+                        
+                        queue.append((nr, nc))
+                        visited.add((nr, nc))
+           
+        
+        for i in range(rows):
+            for j in range(cols):
+                if (i,j) not in visited and grid[i][j] == "1":
+                    bfs((i, j), visited)
+                    count += 1
 
-        return islands
+        return count
