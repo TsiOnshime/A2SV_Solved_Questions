9036cffd3b3498a52c4989ebee608204e2f015ff
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-07 10:36:42 +0300

    Time: 3 ms (75.37%), Space: 20.4 MB (74.37%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,44 +1,20 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        order = []
-        queue = deque()
         indegree = [0] * numCourses
-        pathVisited = [0] * numCourses
-        visited = [0] * numCourses
-        adj = defaultdict(list)
+        queue = deque()
+        order = []
+        adj = {c:[] for c in range(numCourses)}
 
         for u, v in prerequisites:
             adj[v].append(u)
             indegree[u] += 1
-
-        def detectCycle(start):
-            visited[start] = 1
-            pathVisited[start] = 1
-
-            for neigh in adj[start]:
-                if not visited[neigh]:
-                    if detectCycle(neigh):
-                        return True
-            
-                else:
-                    if pathVisited[neigh]:
-                        return True
-            pathVisited[start] = 0
-            return False
-            
-
-
-        for i in range(numCourses):
-            if not visited[i]:
-                cycle = detectCycle(i)
-
-                if cycle:
-                    return []
-
+        
+    
         for i in range(len(indegree)):
             if indegree[i] == 0:
                 queue.append(i)
-
+        
+        
         while queue:
             node = queue.popleft()
             order.append(node)
@@ -46,5 +22,6 @@ class Solution:
                 indegree[neigh] -= 1
                 if indegree[neigh] == 0:
                     queue.append(neigh)
+            
 
-        return order
+        return order if len(order) == numCourses else []
