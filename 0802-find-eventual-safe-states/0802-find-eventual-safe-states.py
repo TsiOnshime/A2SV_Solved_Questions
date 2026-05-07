383d0b0f4e97657e880e24993b5a73ac5ba9ae61
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-07 05:58:15 +0300

    Time: 26 ms (85.21%), Space: 28.1 MB (40.23%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,33 +1,32 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
-        adj = {c: [] for c in range(n)}
-        queue = deque()
         safe = [False] * n
-        indegree = [0] * n
+        colour = [0] * n
         safe_nodes = []
 
-        for u in range(len(graph)):
-            for v in graph[u]:
-                adj[v].append(u)
-                indegree[u] += 1
+        
+                
+
+        def dfs(node):
+            if colour[node] == 1:
+                return False
+            if colour[node] == 2:
+                return True
+            colour[node] = 1
+            
+            for neigh in graph[node]:
+                if not dfs(neigh):
+                    return False
+            
+            colour[node] = 2
+            safe[node] = True
+            return True
 
         for i in range(n):
-            if indegree[i] == 0:
-                queue.append(i)
-        
-        while queue:
-            node = queue.popleft()
-            safe[node] = True
-            for neigh in adj[node]:
-                indegree[neigh] -= 1
-                if indegree[neigh] == 0:
-                    queue.append(neigh)
-
+            if colour[i] == 0:
+                dfs(i)
         for i in range(n):
             if safe[i] == True:
                 safe_nodes.append(i)
-
         return safe_nodes
-                
-        
