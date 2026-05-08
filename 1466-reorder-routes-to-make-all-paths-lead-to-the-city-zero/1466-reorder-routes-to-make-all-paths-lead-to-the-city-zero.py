2624eeea664d88c00691b56b3caa06abd6b6f01e
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-08 17:54:29 +0300

    Time: 199 ms (16.76%), Space: 96.9 MB (6.18%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -3,23 +3,23 @@ class Solution:
         adj = {c: [] for c in range(n)}
         queue = deque()
         visited = set()
-
+        count = 0
         for u, v in connections:
             adj[u].append([v, 1])
             adj[v].append([u, 0])
 
-        queue.append(0)
-        visited.add(0)
-        count = 0
-        while queue:
-            node = queue.popleft()
+        def dfs(node, sign):
+            nonlocal count
+            if node in visited:
+                return 0
+            count += sign
+            visited.add(node)
             for [neigh, sign] in adj[node]:
-                if neigh not in visited:
-                    count += sign
-                    queue.append(neigh)
-                    visited.add(neigh)
-
+                dfs(neigh, sign)
+        dfs(0, 0)
         return count
+
+
 
 
 
