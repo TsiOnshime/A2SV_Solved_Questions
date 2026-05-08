c28020af168c43c3d4a9dccf9574dca9fe6ff0b9
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-08 15:44:26 +0300

    Time: 143 ms (14.29%), Space: 20.6 MB (32.14%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,38 +2,31 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        visited = set()
+        queue = deque()
 
-        def is_valid(r, c, visited, peri):
-        
-            if r in range(rows) and c in range(cols) and grid[r][c] == 1 and (r, c) not in visited:
+        def is_valid(r, c):
+            if 0 <= r < rows and 0 <= c < cols and (r, c) not in visited and grid[r][c] == 1:
                 return True
-            
-
-                
+            return False
         def bfs(r, c):
-            visited = set()
-            queue = deque()
-            count = 0
-
             queue.append((r, c))
             visited.add((r, c))
+            peri = 0
+
             while queue:
-                peri = 4
-                cr, cc = queue.popleft()
+                cr, cc = queue.popleft() 
                 for dr, dc in directions:
                     nr, nc = cr + dr, cc + dc
-                    if is_valid(nr, nc, visited, peri):
+                    if is_valid(nr, nc):
                         queue.append((nr, nc))
-                        visited.add((nr,nc))
-                        peri -= 1
-                    elif (nr,nc) in visited:
-                        peri -= 1
-                    
-                count += peri
-            return count
+                        visited.add((nr, nc))
+                    else:
+                        if (nr, nc) not in visited:
+                            peri += 1
 
+            return peri
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
                     return bfs(r, c)
-                
