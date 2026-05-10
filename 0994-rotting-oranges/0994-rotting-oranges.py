10812de597f5689579b5c966660ef1ed3322dda5
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-10 15:25:24 +0300

    Time: 7 ms (16.58%), Space: 19.3 MB (78.76%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,34 +1,39 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
+        fresh = 0
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
         queue = deque()
         visited = set()
-        fresh = 0
-        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        minutes = 0
+
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 2:
+                if grid[r][c] == 1:
+                    fresh += 1
+                elif grid[r][c] == 2:
                     queue.append((r, c))
                     visited.add((r, c))
-                elif grid[r][c] == 1:
-                    fresh += 1
-        minute = 0
+
+        if fresh == 0:
+            return 0
+
+        def is_valid(r, c):
+            if r in range(rows) and c in range(cols) and (r, c) not in visited and grid[r][c] == 1:
+                return True
+            return False
+
         while queue:
             n = len(queue)
-            for i in range(n):
+            for _ in range(n):
                 cr, cc = queue.popleft()
                 for dr, dc in directions:
-                    nr, nc = cr + dr, cc + dc
-                    if nr < 0 or nr == rows or nc < 0 or nc == cols or (nr, nc) in visited or grid[nr][nc] != 1:
-                        continue
-                    grid[nr][nc] = 2
-                    queue.append((nr, nc))
-                    visited.add((nr, nc))
-                    fresh -= 1
-            minute += 1
-        
-        if fresh > 0:
-            return -1
-        return max(0, minute - 1)
-
+                    nr, nc = dr + cr, dc + cc
+                    if is_valid(nr, nc):
+                        queue.append((nr, nc))
+                        visited.add((nr, nc))
+                        fresh -= 1
+            minutes += 1
+   
+        return minutes - 1 if fresh == 0 else -1
