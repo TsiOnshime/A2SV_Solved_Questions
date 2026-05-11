f74cdeaa376b56bb833c2c304812680bba4aa392
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-11 07:44:16 +0300

    Time: 93 ms (39.81%), Space: 43.7 MB (16.18%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -2,30 +2,23 @@ class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         visited = set()
-        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-        queue = deque()
 
-        def bfs(r, c):
-            if grid[r][c] == 0:
+        def dfs(r, c):
+            if not 0 <= r < rows or not 0 <= c < cols or (r, c) in visited or grid[r][c] == 0:
                 return 
-            queue.append([r, c])
             visited.add(tuple([r, c]))
-
-            while queue:
-                cr, cc = queue.popleft()
-                for dr, dc in directions:
-                    nr, nc = cr + dr, cc + dc
-                    if nr in range(rows) and nc in range(cols) and (nr, nc) not in visited and grid[nr][nc] == 1:
-                        queue.append([nr, nc])
-                        visited.add(tuple([nr, nc]))
+            dfs(r - 1, c)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
 
         for i in range(cols):
-            bfs(0, i)
-            bfs(rows - 1, i)
+            dfs(0, i)
+            dfs(rows - 1, i)
 
         for i in range(rows):
-            bfs(i, 0)
-            bfs(i, cols - 1)
+            dfs(i, 0)
+            dfs(i, cols - 1)
         count = 0
         for r in range(rows):
             for c in range(cols):
