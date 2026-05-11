394aa74844d7d4f7623d7ee3367bfe806eeb96c0
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-11 09:17:44 +0300

    Time: 273 ms (16.58%), Space: 26.8 MB (17.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,31 +3,29 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         count = 0
         visited = set()
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-       
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+
         def is_valid(r, c):
-            if 0 <= r < rows and 0<= c < cols and (r, c) not in visited and grid[r][c] == "1":
+            if 0<= r < rows and 0 <= c < cols and (r, c) not in visited and grid[r][c] == "1":
                 return True
             return False
 
-        def bfs(start, visited):
-            queue = deque()
-            queue.append(start)
-            visited.add(start)
+        def bfs(r, c):
+            queue= deque()
+            queue.append((r, c))
+            visited.add((r, c))
             while queue:
-                cr, cc = queue.pop()
+                cr, cc = queue.popleft()
                 for dr, dc in directions:
-                    nr, nc = cr + dr, cc + dc
+                    nr, nc = dr + cr, dc + cc
                     if is_valid(nr, nc):
-                        
                         queue.append((nr, nc))
-                        visited.add((nr, nc))
-           
-        
-        for i in range(rows):
-            for j in range(cols):
-                if (i,j) not in visited and grid[i][j] == "1":
-                    bfs((i, j), visited)
+                        visited.add((nr,nc))
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
                     count += 1
-
+        
         return count
+
