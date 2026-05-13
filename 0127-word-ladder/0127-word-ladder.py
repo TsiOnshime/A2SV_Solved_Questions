ea5d3627c41abf6f6c7db207f76c04044539fa74
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-13 09:19:50 +0300

    Time: 59 ms (74.91%), Space: 24.8 MB (12.81%) - LeetHub - Time: O(?), Space: O(?)

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,30 +1,38 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        adj = defaultdict(list)
+        if endWord not in wordList:
+            return 0
+
         wordList.append(beginWord)
-        for i in range(len(wordList)):
-            for j in range(len(beginWord)):
-                word = wordList[i]
-                pattern = word[:j] + "*" + word[j + 1:]
-                adj[pattern].append(word)
-        visited = set()
+        n = len(beginWord)
+        adj = defaultdict(set)
         queue = deque()
+        visited = set()
+
+        for i in range(len(wordList)):
+            word = wordList[i]
+            for j in range(n):
+                pat = word[:j] + "*" + word[j + 1:]
+                adj[pat].add(word)
+
         queue.append([beginWord, 1])
         visited.add(beginWord)
         while queue:
-            word, dist = queue.popleft()
-            if word == endWord:
+            node, dist = queue.popleft()
+            if node == endWord:
                 return dist
-            neighbors = []
-            for j in range(len(word)):
-                pattern = word[:j] + "*" + word[j + 1:]
-                for k in adj[pattern]:
-                    if k == word: 
-                        continue
-                    neighbors.append(k)
-            for neigh in neighbors:
-                if neigh not in visited:
-                    queue.append([neigh, dist + 1])
-                    visited.add(neigh)
+            for j in range(n):
+                pat = node[:j] + "*" + node[j + 1:]
+                for neigh in adj[pat]:
+                    if neigh not in visited:
+                        queue.append([neigh, dist + 1])
+                        visited.add(neigh)
 
         return 0
+
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
