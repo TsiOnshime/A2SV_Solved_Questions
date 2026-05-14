88f4253aca43b130f7a42bbee57c35d15827915d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-14 18:00:35 +0300

    Add solution for Last Stone Weight

--- leetcode/1046-last-stone-weight/1046-last-stone-weight.py
+++ leetcode/1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,56 +1,20 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        n = len(stones)
+        stones = [-stones[i] for i in range(len(stones))]
+        print(stones)
+        heapq.heapify(stones)
+        print(stones)
+        while len(stones) > 1:
+            first = abs(heapq.heappop(stones))
+            second = abs(heapq.heappop(stones))
+            if first > second:
+                heapq.heappush(stones, -(first - second))
 
-        if n == 0:
-            return 0
-        if n == 1:
-            return stones[0]
+        if len(stones) == 1:
+            return abs(stones[0])
+        return 0
 
-        # build max heap
-        for i in range(n // 2 - 1, -1, -1):
-            self.heapify(stones, i, n)
 
-        # find second largest
-        second_idx = 1
-        if n > 2 and stones[2] > stones[1]:
-            second_idx = 2
-
-        first = stones[0]
-        second = stones[second_idx]
-
-        if first == second:
-            self.remove(stones, second_idx)
-            self.remove(stones, 0)
-        else:
-            stones[0] = first - second
-            self.remove(stones, second_idx)
-
-        return self.lastStoneWeight(stones)
-
-    def remove(self, stones, i):
-        stones[i], stones[-1] = stones[-1], stones[i]
-        stones.pop()
-
-        n = len(stones)
-
-        for i in range(n // 2 - 1, -1, -1):
-            self.heapify(stones, i, n)
-
-    def heapify(self, stones, idx, n):
-        left = 2 * idx + 1
-        right = 2 * idx + 2
-        largest = idx
-
-        if left < n and stones[left] > stones[largest]:
-            largest = left
-
-        if right < n and stones[right] > stones[largest]:
-            largest = right
-
-        if largest != idx:
-            stones[idx], stones[largest] = stones[largest], stones[idx]
-            self.heapify(stones, largest, n)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
