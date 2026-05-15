e963930e73db0a7c3227937c34261519e2e06296
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-15 15:27:39 +0300

    Time: 67 ms (67.22%), Space: 31.1 MB (18.8%) - LeetHub - Time: O(?), Space: O(?)

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,29 +1,17 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        i = 0
+        while len(heap) < k:
+            heapq.heappush(heap, nums[i])
+            i += 1
 
-        n = len(nums)
-        for i in range(n// 2 - 1, -1, -1):
-            self.max_heapify(nums, i, n)
-        for i in range(n - 1, -1, -1):
-            nums[0], nums[i] = nums[i], nums[0]
-            self.max_heapify(nums, 0, i)
-
-       
-        return nums[-k]
-
-    def max_heapify(self, nums, i, n):
-        left = 2 * i + 1
-        right = 2 * i + 2
-        largest = i
-        if left < n and nums[left] > nums[i]:
-            largest = left
-        if right < n and nums[right] > nums[largest]:
-            largest = right
-
-        if largest != i:
-            nums[i], nums[largest] = nums[largest], nums[i]
-            self.max_heapify(nums, largest, n)
-
+        while i < len(nums):
+            if heap[0] < nums[i]:
+                heapq.heapreplace(heap,nums[i])
+            i += 1
+            
+        return heap[0]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
