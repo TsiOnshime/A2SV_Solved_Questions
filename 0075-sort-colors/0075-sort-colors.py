1cf8bc28e1a521b1dc19009f0471b83f96901f3a
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-15 18:01:53 +0300

    Time: 0 ms (100%), Space: 19.4 MB (22.61%) - LeetHub - Time: O(n), Space: O(1)

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,25 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        l, r = 0, len(nums) - 1
-        i = 0
+        l = mid = 0
+        h = len(nums) - 1
 
-        def swap(i, j):
-            nums[i], nums[j] = nums[j], nums[i]
-
-        while i <= r:
-            if nums[i] == 0:
-                swap(l, i)
+        while mid <= h:
+            if nums[mid] == 0:
+                nums[l], nums[mid] = nums[mid], nums[l]
+                mid += 1
                 l += 1
-            elif nums[i] == 2:
-                swap(i, r)
-                r -= 1
-                i -= 1
-            i += 1
-
-        return nums
-            
-
-
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[h] = nums[h], nums[mid]
+                h -= 1
         
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
