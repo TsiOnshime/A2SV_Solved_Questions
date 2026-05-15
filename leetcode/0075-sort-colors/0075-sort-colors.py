3d1ac1dea3de951ae0a09c8b62fa4db0ea30c6aa
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-15 18:01:54 +0300

    Add solution for Sort Colors

--- leetcode/0075-sort-colors/0075-sort-colors.py
+++ leetcode/0075-sort-colors/0075-sort-colors.py
@@ -3,34 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        l = mid = 0
+        h = len(nums) - 1
+
+        while mid <= h:
+            if nums[mid] == 0:
+                nums[l], nums[mid] = nums[mid], nums[l]
+                mid += 1
+                l += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[h] = nums[h], nums[mid]
+                h -= 1
         
-        #  nums = [2,0,2,1,1,0]
-        k = max(nums)
-        counts = [0] * (k + 1)
-        
-        for num in nums:
-            counts[num] += 1
-            
-        # counts = [2, 2, 2]
-        
-        starting_index = 0
-        
-        for i, count in enumerate(counts): # 0 2    1, 2    2 2
-            counts[i] = starting_index # [0]       [0,2]    [0,2,4]
-            starting_index += count # 2     4       6
-        
-        sorted_nums = [0] * len(nums)
-        
-        for elem in nums:
-            sorted_nums[counts[elem]] = elem
-            
-            counts[elem] += 1
-            
-        for i in range(len(sorted_nums)):
-            nums[i] = sorted_nums[i]
-            
-        return nums
-        
-            
-            
-        
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
