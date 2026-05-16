9f139e038aed75b1bccc23684713e25f541abdf3
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-16 06:19:03 +0300

    Add solution for Kth Smallest Element in a Sorted Matrix

--- leetcode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ leetcode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,17 +1,38 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         rows, cols = len(matrix), len(matrix[0])
-        max_heap = []
-            
-            
+        _min, _max = matrix[0][0], matrix[rows - 1][cols - 1]
 
-        for r in range(rows):
-            for c in range(cols):
-                heapq.heappush_max(max_heap, matrix[r][c])
-                if len(max_heap) > k:
-                    heapq.heappop_max(max_heap)
+        ans = -1 
+        while _min <= _max:
+            mid = _min + (_max - _min) // 2
+            count = self.smaller_elements_count(matrix, mid, rows, cols)
+            if count >= k:
+                ans = mid
+                _max = mid - 1
+            else:
+                _min = mid + 1
+        return ans
 
-        return max_heap[0]
+
+    def smaller_elements_count(self, matrix, elem, rows, cols):
+        count = 0
+        for n in range(rows):
+            l = 0
+            r = cols - 1
+            while l <= r:
+                mid = l + (r - l)//2
+                if matrix[n][mid] > elem:
+                    r = mid - 1
+                else:
+              
+                    l = mid + 1
+
+            count += l
+        print(count)
+
+        return count
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
