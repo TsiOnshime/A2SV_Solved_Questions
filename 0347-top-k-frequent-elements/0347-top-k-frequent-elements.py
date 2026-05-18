8896f5de22005a6498d649972c72f32ebaeea0b2
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-18 18:32:18 +0300

    Time: 0 ms (100%), Space: 22.9 MB (42.45%) - LeetHub - Time: O(?), Space: O(?)

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        topK = []
-
         count = Counter(nums)
+        min_heap = []
 
-        return list(count.keys())[0:k]
-            
+
+       
+        # elem, frequency
+        for key, val in count.items():
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, (val, key))
+            else:
+                heapq.heappushpop(min_heap, (val, key))
+
+        return [h[1] for h in min_heap]
+        
+
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
