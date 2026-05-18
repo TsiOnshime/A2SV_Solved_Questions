9394a0c7932cd5c8887d3307b471c71863bfeee5
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-18 11:34:53 +0300

    Time: 1113 ms (5.02%), Space: 38.7 MB (5.23%) - LeetHub - Time: O(?), Space: O(?)

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,12 +1,35 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums = [-i for i in nums]
-        heapq.heapify(nums)
-        while k > 0:
-            kthLargest = -heapq.heappop(nums)
-            k -= 1
-        
-        return kthLargest
+        # Dutch National Flag algo
+
+        target = len(nums) - k
+
+        def quickSelect(l, r):
+
+            low = mid = l
+            high = r
+            pivot = nums[r]
+
+            while mid <= high:
+                if nums[mid] < pivot:
+                    nums[mid], nums[low] = nums[low], nums[mid]
+                    mid += 1
+                    low += 1
+                elif nums[mid] > pivot:
+                    nums[mid], nums[high] = nums[high], nums[mid]
+                    high -= 1
+                else:
+                    mid += 1
+
+            if target < low:
+                return quickSelect(l, low - 1)
+            elif target > high:
+                return quickSelect(high + 1, r)
+            else:
+                return nums[target]
+
+
+        return quickSelect(0, len(nums) - 1)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
