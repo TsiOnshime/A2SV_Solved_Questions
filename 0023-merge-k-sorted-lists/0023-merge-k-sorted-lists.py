5e6fc54c3717431db1f4dfbef3c0c29209f1cc8f
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-19 06:01:15 +0300

    Time: 11 ms (55.06%), Space: 22.6 MB (93.48%) - LeetHub - Time: O(?), Space: O(?)

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,47 +3,37 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-class Solution:    
+class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
-        if len(lists) == 1:
-            return lists[0]
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.merge(l1, l2))
+            lists = mergedLists
+        return lists[0]
 
-        l1 = lists[0]
-        l2 = lists[1]
+    def merge(self, l1, l2):
+        dummy = curr = ListNode()
 
-        def divide(l, r):  
-            if l > r:
-                return     # 0, 2      # 0, 1     merge(0, 1)  #2,2
-            if l == r:
-                return lists[l]
-            mid = l + (r - l) // 2
-
-            left = divide(l, mid)
-            right = divide(mid + 1, r)
-            return merge(left, right)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        if l1:
+            curr.next = l1
+        if l2:
+            curr.next = l2
         
+        return dummy.next
 
-        def merge(l1, l2):
-            
-
-            dummy = tail = ListNode()
-            while l1 and l2:
-                if l1.val <= l2.val:
-                    tail.next = l1
-                    l1 = l1.next
-                else:
-                    tail.next = l2
-                    l2 = l2.next
-                tail = tail.next
-            if l1:
-                tail.next = l1
-            if l2:
-                tail.next = l2
-            return dummy.next
-            
-    
-
-        return divide(0, len(lists) - 1)
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
