ca267c7a75685ae5fff19679730e4ba90945e553
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-20 10:14:26 +0300

    Time: 15 ms (31.28%), Space: 20.1 MB (71.49%) - LeetHub - Time: O(?), Space: O(?)

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,18 +1,30 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        
-            ls = [0] * 128
-            new_string = list()
-            for char in s:
-                ls[ord(char)] += 1
-            
-            for i in range(len(s)):
+        # ascii values
+        # upper case = A - Z = 65 - 90
+        # lower case = a - z = 97 - 122
+        #  digits = 0 - 9 = 48 - 57
 
-                maxx = max(ls)
-                if maxx == 0:
-                    break
-                new_string.append(chr(ls.index(maxx)) * maxx)
-                ls[ord(chr(ls.index(maxx)))] = 0
+        counts = [[0, 0] for i in range(125)]
+        n = len(s)
+        for i in range(n):
+            idx = ord(s[i])
+            counts[idx][0] += 1
+            counts[idx][1] = i
 
-            return ("").join(new_string)
+        res = []
+        counts.sort(key=lambda x: x[0], reverse=True)
+       
+        for i in range(len(counts)):
+            if counts[i][0] == 0:
+                break
+            freq, letter = counts[i]
+            letter = s[letter]
+            res.append(letter * freq)
 
+        res = "".join(res)
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
