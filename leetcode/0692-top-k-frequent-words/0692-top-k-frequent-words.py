9f44f075ae656bb6ea0ef78f8b0fcba445565267
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-20 18:54:02 +0300

    Add solution for Top K Frequent Words

--- leetcode/0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ leetcode/0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,37 +1,22 @@
-class HeapItem:
-    def __init__(self, word, count):
-        self.word = word
-        self.count = count
-    
-    def __lt__(self, to_compare):
-        if self.count == to_compare.count:
-            return self.word > to_compare.word
-        return self.count < to_compare.count
-
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        n = len(words)
         word_count = Counter(words)
-        heap = []
+        bucket = [[] for i in range(n + 1)]
         res = []
-        for word, cnt in word_count.items():
-            item = HeapItem(word, cnt)
-            if len(heap) < k:
-                heapq.heappush(heap,item)
-            else:
-                if not item < heap[0]:
-                    heapq.heappop(heap)
-                    heapq.heappush(heap, item)
 
-        while k:
-            item = heapq.heappop(heap)
-            res.append(item.word)
-            k -= 1
+        for word, freq in word_count.items():
+            bucket[freq].append(word)
 
-        res = list(reversed(res))
-        return res
+        for i in range(len(bucket)):
+            bucket[i].sort()
 
-
-        
+        for i in range(n, -1, -1):
+            
+            for w in bucket[i]:
+                res.append(w)
+                if len(res) == k:
+                    return res
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
