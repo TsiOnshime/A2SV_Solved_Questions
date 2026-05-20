7e2edf43bf3a7049e79b0e461bda59581a00616d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-20 15:51:06 +0300

    Time: 274 ms (16.03%), Space: 26.8 MB (21.85%) - LeetHub - Time: O(?), Space: O(?)

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,42 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        count = 0
         visited = set()
-        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        count = 0
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-        def is_valid(r, c):
-            if 0<= r < rows and 0 <= c < cols and (r, c) not in visited and grid[r][c] == "1":
-                return True
-            return False
-
-        def bfs(r, c):
-            queue= deque()
-            queue.append((r, c))
-            visited.add((r, c))
-            while queue:
-                cr, cc = queue.popleft()
-                for dr, dc in directions:
-                    nr, nc = dr + cr, dc + cc
-                    if is_valid(nr, nc):
-                        queue.append((nr, nc))
-                        visited.add((nr,nc))
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r, c) not in visited:
-                    bfs(r, c)
+                    self.explore(r, c, visited, directions, rows, cols, grid)
                     count += 1
-        
+
         return count
 
+    def is_valid(self, r, c, rows, cols, visited, grid):
+        if 0 <= r < rows and 0 <= c < cols and (r, c) not in visited and grid[r][c] == "1": 
+            return True
+        return False
+
+
+    def explore(self, r, c, visited, directions, rows, cols, grid):
+        visited.add((r, c))
+        queue = deque()
+        queue.append([r, c])
+
+        while queue:
+            cr, cc = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = cr + dr, cc + dc
+                if self.is_valid(nr, nc, rows, cols, visited, grid):
+                    queue.append([nr, nc])
+                    visited.add((nr, nc))
+
+
+
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
