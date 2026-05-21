68d44eee8cf7fd6931bc4fd7aacfaac8c52c1da8
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-21 05:41:15 +0300

    Time: 7 ms (39.99%), Space: 20.6 MB (26.42%) - LeetHub - Time: O(?), Space: O(?)

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,16 +1,27 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        path =[]
         res = []
-        def dfs(start, dest, path):
-            path.append(start)
-            if start == dest:
+        target = len(graph) - 1
+        def get_candidates(i):
+            return graph[i]
+        def is_valid(node):
+            if node == target:
+                return True
+            return False
+        def search(src, node):
+            if is_valid(node):
+                path.append(node)
                 res.append(path.copy())
-
-            else:
-                for neigh in graph[start]:
-                    dfs(neigh, dest, path)
-
-            path.pop()
-
-        dfs(0, len(graph) - 1, [])
+                return
+            # [0,1]
+            path.append(node)
+            for candidate in get_candidates(node):
+                search(src, candidate)
+                path.pop()
+        search(0, 0)
         return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
