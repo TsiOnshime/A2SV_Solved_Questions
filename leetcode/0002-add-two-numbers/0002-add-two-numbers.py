ab43f50e99286307013c38cf2e3adece672e89ff
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-23 12:34:58 +0300

    Add solution for Add Two Numbers

--- leetcode/0002-add-two-numbers/0002-add-two-numbers.py
+++ leetcode/0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,22 +5,38 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = curr = ListNode()
-
-
+        dummy = ListNode()
+        dummy.next = l1
+        tail = dummy
         carry = 0
 
+        def calculate_sum_carry(val1, val2, carry):
+            _sum = val1 + val2 + carry 
+            return [_sum % 10, _sum // 10]
+            
         while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
 
-            value = val1 + val2 + carry
-            carry = value // 10
-            value = value % 10
-            curr.next = ListNode(value)
-
-            curr = curr.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-
+            if l1 and l2:
+                l1.val, carry = calculate_sum_carry(l1.val, l2.val, carry)
+                tail = l1
+                l1 = l1.next
+                l2 = l2.next
+            elif l1:
+                l1.val, carry = calculate_sum_carry(l1.val, 0, carry)
+                tail = l1
+                l1 = l1.next
+            elif l2:
+                val, carry = calculate_sum_carry(0, l2.val, carry)
+                tail.next = ListNode(val)
+                tail = tail.next
+                l2 = l2.next
+            else:
+                val, carry = carry, 0
+                tail.next = ListNode(val)
+                tail = tail.next
+        
         return dummy.next
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
