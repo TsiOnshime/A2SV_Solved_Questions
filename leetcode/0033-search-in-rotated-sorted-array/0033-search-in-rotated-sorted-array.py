02773bd8bb2857c06b5e1f7ed04e8811631be8a9
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-23 10:15:29 +0300

    Add solution for Search in Rotated Sorted Array

--- leetcode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ leetcode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,31 +1,38 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+# find the pivot where nums[p - 1] < nums[p] < nums[p + 1]
+# if target > nums[l] search (l:p)
+# if target < nums[r] search(p:r)
 
 
-        l = 0
-        r = len(nums) - 1
+# calculate mid
 
-        ans = -1
+# check if it is in the left sorted array
+#  how? if nums[l] < nums[mid]: we are in the left sorted array
+#        check if our target belongs in that sub array how? if nums[l] < target: r = mid - 1
+#         else: l = mid + 1
 
+# else: we are on the right sorted array:
+#       if nums[mid] < target < nums[r]: l = mid + 1
+#       else: r = mid - 1
+        l, r = 0, len(nums) - 1
         while l <= r:
-            mid = l + (r - l)//2
-
+            mid = l + (r - l) // 2
             if nums[mid] == target:
-                ans = mid
-                break
-            elif nums[mid] <= nums[r]:
-                if nums[mid] <= target <= nums[r]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            
-            elif nums[mid] >=  nums[l]:
-                if nums[l] <= target <= nums[mid]:
+                return mid
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
-            
-        return ans
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r =mid - 1
 
-          
+        return -1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
