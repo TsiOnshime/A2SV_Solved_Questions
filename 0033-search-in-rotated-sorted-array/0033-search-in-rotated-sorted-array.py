167c120bb7f5a05c4b01bdc8329025ca326421e7
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-23 10:15:28 +0300

    Time: 0 ms (100%), Space: 19.3 MB (77.83%) - LeetHub - Time: O(log n), Space: O(1)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,29 +1,38 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+# find the pivot where nums[p - 1] < nums[p] < nums[p + 1]
+# if target > nums[l] search (l:p)
+# if target < nums[r] search(p:r)
 
-        def searchTarget(l, r):
-            m = (l + r) // 2
 
-            if target == nums[m]:
-                return m
-            if l > r:
-                return -1
+# calculate mid
 
-            #left portion
-            if nums[l] <= nums[m]:
-                if target < nums[l] or target > nums[m]:
-                    l = m + 1
-                    return searchTarget(l, r)
+# check if it is in the left sorted array
+#  how? if nums[l] < nums[mid]: we are in the left sorted array
+#        check if our target belongs in that sub array how? if nums[l] < target: r = mid - 1
+#         else: l = mid + 1
+
+# else: we are on the right sorted array:
+#       if nums[mid] < target < nums[r]: l = mid + 1
+#       else: r = mid - 1
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
                 else:
-                    r = m - 1
-                    return searchTarget(l, r)
-            # right portion
+                    l = mid + 1
             else:
-                if target > nums[r] or target < nums[m]:
-                    r = m - 1
-                    return searchTarget(l, r)
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
                 else:
-                    l = m + 1
-                    return searchTarget(l, r)
-        return searchTarget(0, len(nums) - 1)
+                    r =mid - 1
+
+        return -1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
