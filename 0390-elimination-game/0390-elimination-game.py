9dd333ef0ed616e6a0e1fc792499fcb69ea83211
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-25 17:43:07 +0300

    Time: 5 ms (29.07%), Space: 20 MB (6.71%) - LeetHub - Time: O(?), Space: O(?)

--- 0390-elimination-game/0390-elimination-game.py
+++ 0390-elimination-game/0390-elimination-game.py
@@ -1,6 +1,23 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
-        
+        # is all about mapping
+        # at any point our number should be in range [1 - n]
+        # so when we go from left to right we will always be left with even numbers since we start our elimination from odd number so the mapping could be 
+        #      valid element    current element
+        #           1               2
+        #           2               4
+        #           3               6
+        # to make our recurssion easier we will use the valid elements but when we come back to it we should map the valid elements to the actual elements and we do that by 2 * validElem
+
+        # when we go from right to left though things change since n could be odd or even
+        # if it is odd we will be left with the even elements for which we can simply use the earlier mapping 2 * validElem
+        # if it is even since we start eliminating from even numbers we will be left with odd numbers 
+        #      valid element        current element
+        #           1                   1
+        #           2                   3
+        #           3                   5
+
+        # to get the current element from the valid element we would have to do (2 * validElem) - 1
         def last(n, left):
             if n == 1:
                 return 1
