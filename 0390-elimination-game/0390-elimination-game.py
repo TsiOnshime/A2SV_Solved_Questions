acf141b0e401e1bb53aafa9083fe25b79e1fdb78
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-25 17:32:39 +0300

    Time: 8 ms (10.38%), Space: 20.1 MB (6.71%) - LeetHub - Time: O(?), Space: O(?)

--- 0390-elimination-game/0390-elimination-game.py
+++ 0390-elimination-game/0390-elimination-game.py
@@ -1,22 +1,22 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
         
-# left = True
-
         def last(n, left):
             if n == 1:
                 return 1
             
             if left:
-                return 2 * last(n//2, False)
+                return 2 * last(n // 2, False)
             else:
                 if n % 2:
-                    return 2 * last(n//2, True)
+                    return 2 * last(n // 2, True)
                 else:
-                    return 2 * last(n//2, True) - 1
+                    return 2 * last(n // 2, True) - 1
+
 
         
-
-
-
         return last(n, True)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
