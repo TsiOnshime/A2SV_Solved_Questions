0fa96769828c4786b18b9500253d98f0a40e901f
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-25 05:34:52 +0300

    Add solution for Trapping Rain Water

--- leetcode/0042-trapping-rain-water/0042-trapping-rain-water.py
+++ leetcode/0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,24 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        maxLeft = [0] * len(height)
         maxRight = [0] * len(height)
+        maxLeft = [0] * len(height)
 
+        rain = 0
 
         for i in range(1, len(height)):
-            maxLeft[i] = max(maxLeft[i - 1], height[i - 1])
+            maxLeft[i] = max(height[i - 1], maxLeft[i - 1])
 
         for i in range(len(height) - 2, -1, -1):
-            maxRight[i] = max(maxRight[i + 1], height[i + 1])
-
-        _min = [0] * len(height)
+            maxRight[i] = max(height[i + 1], maxRight[i + 1])
 
         for i in range(len(height)):
-            _min[i] = min(maxLeft[i], maxRight[i])
-        
-        rain = 0
-        for i in range(len(height)):
-            trapped = _min[i] - height[i]
-            if trapped > 0:
-                rain += trapped
+            diff = min(maxLeft[i], maxRight[i]) - height[i]
+            if diff > 0:
+                rain += diff
 
         return rain
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
