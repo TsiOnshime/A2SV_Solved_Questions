6fc658264200ef43ad117708053b0cc3e71e3512
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-26 08:59:56 +0300

    Add solution for Merge k Sorted Lists

--- leetcode/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ leetcode/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,3 +1,8 @@
+class Wrapper:
+    def __init__(self, node):
+        self.node = node
+    def __lt__(self, to_compare):
+        return self.node.val < to_compare.node.val
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,34 +10,27 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return None
-        while len(lists) > 1:
-            mergedLists = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if (i + 1) < len(lists) else None
-                mergedLists.append(self.merge(l1, l2))
-            lists = mergedLists
-        return lists[0]
-
-    def merge(self, l1, l2):
-        dummy = curr = ListNode()
-
-        while l1 and l2:
-            if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
-            else:
-                curr.next = l2
-                l2 = l2.next
-            curr = curr.next
-        if l1:
-            curr.next = l1
-        if l2:
-            curr.next = l2
         
+        min_heap = []
+        for node in lists:
+            if node:
+                heapq.heappush(min_heap, Wrapper(node))
+
+        dummy = tail = ListNode()
+
+        while min_heap:
+            smallest = heapq.heappop(min_heap).node
+            tail.next = smallest
+            tail = tail.next
+            nxt = smallest.next
+            if nxt:
+                heapq.heappush(min_heap, Wrapper(nxt))
+
         return dummy.next
+
+        
+
+            
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
