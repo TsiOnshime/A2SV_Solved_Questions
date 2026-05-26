0c84dcfc283ffcf69a740e60fae1cb29ee01bd72
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-26 16:39:11 +0300

    Time: 12 ms (16.08%), Space: 20.4 MB (53.3%) - LeetHub - Time: O(?), Space: O(?)

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,46 +1,45 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        """
-          1 2 3
-        1[1,1,0]
-        2[1,1,0]
-        3[0,0,1]
+        rows, cols = len(isConnected), len(isConnected[0])
+        parent = [i for i in range(rows)]
+        rank = [0] * rows
 
-        unionFind 
+        def find(node):
 
+            while node != parent[node]:
+                parent[node] = find(parent[node])
+                node = parent[node]
+            return node
 
-        """
-        n = len(isConnected)
+        
+        def union(a, b):
+            p1, p2 = find(a), find(b)
 
-        parent = [i for i in range(n)]
-        rank = [1] * n
-
-        def find(n1):
-            res = n1
-            while res != parent[res]:
-                parent[res] = parent[parent[res]]
-                res = parent[res]
-
-            return res
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
             if p1 == p2:
                 return 0
-            if rank[p1] > rank[p2]:
-                rank[p1] += rank[p2]
+
+            if rank[p1] == rank[p2]:
+                parent[p1] = p2
+                rank[p2] += 1
+            elif rank[p1] > rank[p2]:
                 parent[p2] = p1
             else:
-                rank[p2] += rank[p1]
                 parent[p1] = p2
-            return 1
 
-        provinces = n
-        for r in range(n):
-            for c in range(n):
-                if r != c:
-                    if isConnected[r][c] ==1:
-                        provinces -= union(r, c)
+            return 1
+        provinces = rows
+        for r in range(rows):
+            for c in range(cols):
+                if isConnected[r][c] == 1:
+                    provinces -= union(r, c)
 
         return provinces
+        
 
 
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
