eb07e4c127ff86a445f09694e195b9e3a943c4a2
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-26 09:31:58 +0300

    Time: 85 ms (22.45%), Space: 20 MB (8.29%) - LeetHub - Time: O(?), Space: O(?)

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,13 +1,13 @@
-from collections import Counter
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-
         count = Counter(s)
 
-        appearing_once = ("").join([key for key, value in count.items() if value == 1])
-        if len(appearing_once) == 0:
-            return - 1
-
-
-        return s.index(appearing_once[0])
+        for i in range(len(s)):
+            if count[s[i]] == 1:
+                return i
         
+        return -1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
