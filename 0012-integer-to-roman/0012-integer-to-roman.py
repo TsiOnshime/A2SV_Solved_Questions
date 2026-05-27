a9ce4fc34276f62e7958939122da9dd07ead1f29
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-27 18:40:30 +0300

    Time: 5 ms (49.55%), Space: 19.3 MB (26.02%) - LeetHub - Time: O(?), Space: O(?)

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,32 +1,34 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        int_to_rom = {
-            "M": 1000,
-            "CM": 900,
-            "D": 500,
-            "CD": 400,
-            "C": 100,
-            "XC": 90,
-            "L": 50,
-            "XL": 40,
-            "X": 10, 
-            "IX": 9,
-            "V": 5,
-            "IV": 4,
-            "I": 1
+        
+        Int_Rom = {
+            1000: "M",
+            900: "CM",
+            500: "D",
+            400: "CD", 
+            100: "C",
+            90: "XC", 
+            50: "L",
+            40: "XL", 
+            10: "X",
+            9: "IX",
+            5: "V",
+            4: "IV",
+            1: "I"
+
         }
 
-        result = ""
-        for key, value in int_to_rom.items():
-            number_of_symbols = num // value
-            if (number_of_symbols != 0):
-                result += key * number_of_symbols
+        rom = []
+        for key, val in Int_Rom.items():
+            freq = num // key
+            if freq != 0:
+                rom.append(Int_Rom[key] * freq)
+            num %= key
 
-            num = num % value
+        return "".join(rom)
 
-        return result 
+            
 
-                    
-                
-
-                
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
