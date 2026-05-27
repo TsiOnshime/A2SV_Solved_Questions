9fb3a4d94380f6cb13058ca2b3c5ef764f524ec9
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-05-27 08:28:15 +0300

    Add solution for Check if There is a Valid Path in a Grid

--- leetcode/1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ leetcode/1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,47 +1,113 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * (n)
+        
+    def find(self,node):
+        if node != self.parent[node]:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+
+    def union(self, u, v):
+        p1, p2 = self.find(u), self.find(v)
+        if p1 == p2:
+            return 
+            
+        if self.rank[p1] == self.rank[p2]:
+            self.parent[p1] = p2
+            self.rank[p2] += 1
+        elif self.rank[p1] < self.rank[p2]:
+            self.parent[p1] = p2
+        else:
+            self.parent[p2] = p1
+
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
+        # 1: left, right
+        # 2: up, down
+        # 3: left, down
+        # 4: right, down
+        # 5: left, up
+        # 6: right, up
+
+        # 1: [3, 4, 5, 6]
+        # 2: [3, 4, 5, 6]
+        # 3: [1, 2, 4, 5]
+        # 4: [1, 2, 3, 5]
+        # 5: [1, 2, 4, 6]
+        # 6: [1, 2, 4, 5]
         rows, cols = len(grid), len(grid[0])
-
-        streets = {
-            1: {(0, 1), (0, -1)},
-            2: {(1, 0), (-1, 0)},
-            3: {(0, -1), (1, 0)},
-            4: {(0, 1), (1, 0)},
-            5: {(0, -1), (-1, 0)},
-            6: {(0, 1), (-1, 0)}
-        }
-
-        path = set()
-
-
-        def is_valid(r, c, needed):
-            if r < 0 or c < 0 or c >= cols or r >= rows or (r, c) in path:
-                return False
-            if needed is None:
-                return True
-
-            return needed in streets[grid[r][c]]
+        uf = UnionFind(rows * cols)
 
 
 
-        def dfs(r, c, needed):
 
-            if not is_valid(r, c, needed):
-                return 
-            if r == rows - 1 and c == cols - 1:
-                return True
+        # 1: left, right
+        # 2: up, down
+        # 3: left, down
+        # 4: right, down
+        # 5: left, up
+        # 6: right, up
+        def getId(r, c):
+            return r * cols + c
 
-            path.add((r, c))
-            for dr, dc in streets[grid[r][c]]:
-                nr, nc = r + dr, c + dc
-                if dfs(nr, nc, (-dr, -dc)):
-                    return True
-            path.remove((r, c))
+        def detectLeft(r, c):
+            if c - 1 >= 0  and grid[r][c - 1] in {1, 4, 6}:
+                uf.union(getId(r, c), getId(r, c - 1))
+            
+        def detectRight(r, c):
+            if c + 1 < cols and grid[r][c + 1] in {1, 3, 5}:
+                uf.union(getId(r, c), getId(r, c + 1))
+
+        def detectUp(r, c):
+            if r - 1 >= 0 and grid[r - 1][c] in {2, 3, 4}:
+                uf.union(getId(r, c), getId(r - 1, c))
         
-        if dfs(0, 0, None):
-            return True
+        def detectDown(r, c):
+            if r + 1 < rows and grid[r+1][c] in {2, 5, 6}:
+                uf.union(getId(r, c), getId(r + 1, c))
 
-        return False
+        def handler(r, c):
+            if grid[r][c] == 1:
+                detectLeft(r,c)
+                detectRight(r,c)
+            elif grid[r][c] == 2:
+                detectUp(r,c)
+                detectDown(r,c)
+            elif grid[r][c] == 3:
+                detectLeft(r, c)
+                detectDown(r, c)
+            elif grid[r][c] == 4:
+                detectRight(r, c)
+                detectDown(r, c)
+            elif grid[r][c] == 5:
+                detectLeft(r, c)
+                detectUp(r, c)
+            else:
+                detectRight(r, c)
+                detectUp(r, c)
+
+
+
+
+
+        for r in range(rows):
+            for c in range(cols):
+                handler(r, c)
+
+        return uf.find(getId(0, 0)) == uf.find(getId(rows - 1, cols - 1))
+        # def handler(r, c, neigh):
+        #     if not is_valid(r, c):
+        #         return 
+            
+
+            
+
+
+        
+
+
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
