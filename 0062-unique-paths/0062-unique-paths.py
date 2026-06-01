d84f56b7048e5d4ddcb8f08a7dfcdd30f6b99e76
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-01 19:12:14 +0300

    Time: 3 ms (19.96%), Space: 19.2 MB (74.54%) - LeetHub - Time: O(?), Space: O(?)

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        paths = [[0] * n for i in range(m)]
-        print(paths)
-        for i in range(m):
-            paths[i][0] = 1
-        for j in range(n):
-            paths[0][j] = 1
-
+        rows = [1] * n
+# [1, 1, 1]
         for i in range(1, m):
             for j in range(1, n):
-                paths[i][j] = paths[i - 1][j] + paths[i][j - 1]
-        return paths[m - 1][n - 1]
-
+                rows[j] = rows[j] + rows[j - 1]
+        
+        return rows[-1]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
