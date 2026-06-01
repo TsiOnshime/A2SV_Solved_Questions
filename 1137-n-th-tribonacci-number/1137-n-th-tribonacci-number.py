ce0646d73cf00a36ce7d8b8961d21e6c0a143db2
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-01 17:39:38 +0300

    Time: 0 ms (100%), Space: 19.3 MB (17.62%) - LeetHub - Time: O(?), Space: O(?)

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,16 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:return 0
-        if n == 1:return 1
-        if n == 2:return 1
-        values = [0] * (n + 1)
-        values[1] = 1
-        values[2] = 1
+        cache = {}
+        def triSum(n):
+            if n == 0: return 0
+            if n == 1: return 1
+            if n == 2: return 1
+            if n in cache: return cache[n]
 
-        for i in range(3, n + 1):
-            values[i] = values[i - 3] + values[i - 2] + values[i - 1]
-
-        return values[n]
+            val = triSum(n - 3) + triSum(n - 2) + triSum(n - 1)
+            cache[n] = val
+            return val
+        
+        return triSum(n)
 
 
 # Synced seamlessly with LeetHub Pro
