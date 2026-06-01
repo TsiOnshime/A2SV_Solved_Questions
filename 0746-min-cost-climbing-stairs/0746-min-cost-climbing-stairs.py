041203c215f2eef43bf4966894f0d58e8a61c21a
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-01 17:58:32 +0300

    Time: 6 ms (19.32%), Space: 21.8 MB (9.6%) - LeetHub - Time: O(?), Space: O(?)

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,12 +1,23 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
 
-        c1, c2 = cost[0], cost[1]
-        for i in range(2, len(cost)):
-            _min = min(c1, c2)
-            c1, c2 = c2, _min + cost[i]
+        cache = {}
 
-        return min(c1, c2)
+        def calc_min_cost(i):
+            if i == 0:
+                return cost[i]
+            if i == 1:
+                return cost[i]
+            if i in cache:
+                return cache[i]
+            if i == len(cost):
+                val = min(calc_min_cost(i - 1), calc_min_cost(i - 2))
+            else:
+                val = min(calc_min_cost(i - 1), calc_min_cost(i - 2)) + cost[i]
+            cache[i] = val
+            return val
+
+        return calc_min_cost(len(cost))
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
