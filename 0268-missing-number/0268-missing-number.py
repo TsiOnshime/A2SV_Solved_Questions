b6d8f63d28ff939464d232cd04cfb09af52e011d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-02 09:25:16 +0300

    Time: 11 ms (23.72%), Space: 20.6 MB (19.67%) - LeetHub - Time: O(?), Space: O(?)

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        maximum = max(nums)
-        minimum = min(nums)
-
-        for i in range(minimum, maximum):
-            if i not in nums:
+        count = {}
+        for i in nums:
+            if i not in count:
+                count[i] = 1
+            else:
+                count[i] += 1
+        
+        for i in range(len(nums)):
+            if i not in count:
                 return i
-        return maximum + 1
+
+        return len(nums)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
