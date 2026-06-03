d9ab021731626b22cc0fb7f4e7e0579398500725
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-03 17:40:42 +0300

    Time: 0 ms (100%), Space: 19.1 MB (98.98%) - LeetHub - Time: O(?), Space: O(?)

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -3,13 +3,14 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
 
-        def robbery(nums):
-            if len(nums) == 1:
-                return nums[0]
-            prev2 = nums[0]
-            prev = max(nums[1], nums[0])
+        def robbery(start, end):
 
-            for i in range(2, len(nums)):
+            if (start - end + 1) == 1:
+                return nums[start]
+            prev2 = nums[start]
+            prev = max(nums[start + 1], nums[start])
+
+            for i in range(start + 2, end + 1):
                 curr = max(prev, nums[i] + prev2)
                 prev2, prev = prev, curr
 
@@ -17,7 +18,7 @@ class Solution:
 
 
 
-        _max = max(robbery(nums[:-1]), robbery(nums[1:]))
+        _max = max(robbery(0, len(nums) - 2), robbery(1, len(nums) - 1))
 
         return _max
 
