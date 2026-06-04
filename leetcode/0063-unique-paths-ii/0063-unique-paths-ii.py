7855c9462481b71f604e11f3ce6dc5a2392d00b8
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-04 19:12:58 +0300

    Add solution for Unique Paths II

--- leetcode/0063-unique-paths-ii/0063-unique-paths-ii.py
+++ leetcode/0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,29 +1,20 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        # how many possible paths are there from (i, j) to (m - 1, n - 1)
+        # how many possible paths are there from start to current?
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        def path(i, j):
-            if i >= m or j >= n:
-                return 0
-            if obstacleGrid[i][j] == 1:
-                return 0
-            if i == m - 1 and j == n - 1:
-                return 1
-            if memo[i][j] != -1:
-                return memo[i][j]
-            val = path(i + 1, j) + path(i, j + 1)
 
-            memo[i][j] = val
-            return val
-
-        memo = [[-1] * n for i in range(m)]
-
-        return path(0, 0)
-
-
-
-        
-
+        dp = [0] * n
+        dp[0] = 1
+        if obstacleGrid[0][0] == 1:
+            return 0
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                else:
+                    if j - 1 >= 0:
+                        dp[j] = dp[j - 1] + dp[j]
+        return dp[-1]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
