90a3f9a8edc3be81fb2c38a76efa8377f89e5704
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-04 18:59:09 +0300

    Time: 0 ms (100%), Space: 19.5 MB (33.03%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        def path(i, j):
+            if i >= m or j >= n:
+                return 0
+            if obstacleGrid[i][j] == 1:
+                return 0
+            if i == m - 1 and j == n - 1:
+                return 1
+ 
+            if memo[i][j] != -1:
+                return memo[i][j]
+            val = path(i + 1, j) + path(i, j + 1)
+
+            memo[i][j] = val
+            return val
+
+        memo = [[-1] * n for i in range(m)]
+
+        return path(0, 0)
+
+
+
+        
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
