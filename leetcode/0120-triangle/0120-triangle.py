3a9b3e015664083e537e3aab19bf907b5d7cd95f
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-05 07:19:59 +0300

    Add solution for Triangle

--- leetcode/0120-triangle/0120-triangle.py
+++ leetcode/0120-triangle/0120-triangle.py
@@ -2,19 +2,20 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         rows, cols = len(triangle), len(triangle[-1])
 
-        dp = [[-1] * cols for i in range(rows)]
+        dp = [-1] * cols
 
         for i in range(cols):
-            dp[-1][i] = triangle[-1][i]
+            dp[i] = triangle[-1][i]
+
 
         for i in range(rows - 2, -1, -1):
-            for j in range(i, -1, -1):
-                down = dp[i + 1][j]
-                diagonal = dp[i + 1][j + 1]
+            for j in range(0, i + 1):
+                down = dp[j]
+                diagonal = dp[j + 1]
                 val = min(down, diagonal)
-                dp[i][j] = val + triangle[i][j]
+                dp[j] = val + triangle[i][j]
 
-        return dp[0][0]
+        return dp[0]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
