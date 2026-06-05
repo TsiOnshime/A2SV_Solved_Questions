d2aaa43687b1e354f4fff48fac7172ce9c2d3662
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-05 19:01:15 +0300

    Time: 0 ms (100%), Space: 19.5 MB (8.41%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,23 +6,18 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        queue = deque()
-        queue.append(root)
         order = []
-        while queue:
-            n = len(queue)
-
-            for i in range(n):
-                node = queue.popleft()
-                for neigh in [node.left, node.right]:
-                    if neigh:
-                        queue.append(neigh)
-                if i == n - 1:
-                    order.append(node.val)
-
+        def dfs(root, level):
+            if not root:
+                return []
+            if len(order) == level:
+                order.append(root.val)
+            dfs(root.right, level + 1)
+            dfs(root.left, level + 1)
+        dfs(root, 0)
         return order
+
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
