5bd8f73550b331a07e2ae761a26cab98502fdff0
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-05 07:12:35 +0300

    Time: 0 ms (100%), Space: 20.7 MB (18.93%) - LeetHub - Time: O(?), Space: O(?)

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,30 +1,20 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         rows, cols = len(triangle), len(triangle[-1])
-        
+
         dp = [[-1] * cols for i in range(rows)]
 
-        for i in range(rows):
-            cols = i + 1
-            j = 0
-            while j < cols:
-                if i == 0 and j == 0:
-                    dp[i][j] = triangle[i][j]
-                else:
-                    up = float('inf')
-                    upleft = float('inf')
+        for i in range(cols):
+            dp[-1][i] = triangle[-1][i]
 
-                    if i - 1 >= 0 and j < prevCol:
-                        up = dp[i - 1][j]
-                    if j - 1 >= 0:
-                        upleft = dp[i - 1][j - 1]
-                    val = min(up, upleft)
-                    dp[i][j] = val + triangle[i][j]
-                
-                j += 1
-            prevCol = cols
-        return min(dp[-1])
+        for i in range(rows - 2, -1, -1):
+            for j in range(i, -1, -1):
+                down = dp[i + 1][j]
+                diagonal = dp[i + 1][j + 1]
+                val = min(down, diagonal)
+                dp[i][j] = val + triangle[i][j]
 
+        return dp[0][0]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
