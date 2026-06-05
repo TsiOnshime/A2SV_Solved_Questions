036a68d32f0ea5e672fd479a13cfb80e1deb4a5f
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-05 06:57:48 +0300

    Add solution for Triangle

--- leetcode/0120-triangle/0120-triangle.py
+++ leetcode/0120-triangle/0120-triangle.py
@@ -1,22 +1,30 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        rows = len(triangle)
-
-        def pathSum(i, j):
-            if i == rows - 1:
-                return triangle[i][j]
-            if (i,j) in memo:
-                return memo[(i, j)]
-            down = triangle[i][j] + pathSum(i + 1, j)
-            diagonal = triangle[i][j] + pathSum(i + 1, j + 1)
-
-            val = min(down, diagonal)
-            memo[(i,j)] = val
-            return val
+        rows, cols = len(triangle), len(triangle[-1])
         
+        dp = [[-1] * cols for i in range(rows)]
 
-        memo = {}
-        return pathSum(0, 0)
+        for i in range(rows):
+            cols = i + 1
+            j = 0
+            while j < cols:
+                if i == 0 and j == 0:
+                    dp[i][j] = triangle[i][j]
+                else:
+                    up = float('inf')
+                    upleft = float('inf')
+
+                    if i - 1 >= 0 and j < prevCol:
+                        up = dp[i - 1][j]
+                    if j - 1 >= 0:
+                        upleft = dp[i - 1][j - 1]
+                    val = min(up, upleft)
+                    dp[i][j] = val + triangle[i][j]
+                
+                j += 1
+            prevCol = cols
+        return min(dp[-1])
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
