58fb57881e2720bb3bb2741e541db881f92e8bfd
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-08 15:50:53 +0300

    Time: 0 ms (100%), Space: 19.7 MB (5.95%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        prev = None
+        def preOrder(root):
+            nonlocal prev
+            if not root:
+                return 
+
+            preOrder(root.right)
+            preOrder(root.left)
+
+            root.right = prev
+            root.left = None
+            prev = root
+
+        preOrder(root)
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
