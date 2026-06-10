2f154e40d2479e9216900f3ed6c03f4c8108fd16
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-10 09:11:01 +0300

    Time: 3 ms (73.54%), Space: 19.4 MB (51.71%) - LeetHub - Time: O(?), Space: O(?)

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,27 +1,27 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        failed = set()
-        wordDict.sort(reverse=True)
-        print(wordDict)
-        def search(s_idx):
-            if s_idx >= len(s):
+        if len(s) == 1:
+            return s in wordDict
+        queue = deque()
+        queue.append(0)
+        wordDict = set(wordDict)
+        visited = set()
+        
+        while queue:
+            start = queue.popleft()
+
+            if start == len(s):
                 return True
-            if s_idx in failed:
-                return 
-            for word in wordDict:
-                end = len(word)
-                if s[s_idx:s_idx + end] == word:
-                    if search(s_idx + end):
-                        return True
-            failed.add(s_idx)
-        if search(0):
-            return True
+            visited.add(start)
+
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+                if word in wordDict and end not in visited:
+                    queue.append(end)
+                    visited.add(end)
+        # queue = [0], visited = {}, start = 0, end = 
         return False
-
-                
-
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
