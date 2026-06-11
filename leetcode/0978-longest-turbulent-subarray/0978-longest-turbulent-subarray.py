2c4427ee75a26a5b377618f9778b2ca35dde9256
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-11 11:30:31 +0300

    Add solution for Longest Turbulent Subarray

--- leetcode/0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ leetcode/0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -1,31 +1,27 @@
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        dp = [0] * len(arr)
+        
+        prev = ""
+        res = 1
 
-        dp[0] = 1
-        if len(set(arr)) == 1:
-            return 1
+        l, r = 0, 1
 
-        def calculateSize(i):
-            nonlocal dp
-            if i >= len(arr) - 1:
-                return 
-            # turbulent
-            if arr[i - 1] > arr[i] < arr[i + 1] or arr[i - 1] < arr[i] > arr[i + 1]:
-                dp[i] = dp[i - 1] + 1
-      
+        while r < len(arr):
+            if arr[r - 1] < arr[r] and prev != "<":
+                res = max(res, r - l + 1)
+                r += 1
+                prev = "<"
+            elif arr[r - 1] > arr[r] and prev != ">":
+                res = max(res, r - l + 1)
+                r += 1
+                prev = ">"
             else:
-                dp[i] = 1
-            
-            calculateSize(i + 1)
+                r = r + 1 if arr[r-1] == arr[r] else r
+                l = r - 1
+                prev = ""
 
-        calculateSize(1)
+        return res
 
-        ans = max(dp) + 1
-        return ans
-
-# 8 < 12 < 16
-# [1, 2, 1, 0]
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 # Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
