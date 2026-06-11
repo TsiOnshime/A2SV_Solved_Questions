61bbdc249a7dc1466bdfe6c3b6136ab6fc00474a
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-11 12:06:48 +0300

    Added solution - LeetHub

---
+++ 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.py
@@ -0,0 +1,30 @@
+class Solution:
+    def knapsack(self, W, val, wt):
+        
+
+
+        prev = [0] * (W + 1)
+        curr = [0] * (W + 1)
+        
+
+        for i in range(W + 1):
+            if wt[0] <= i:
+                prev[i] = val[0]
+                
+        for i in range(1, len(wt)):
+            for j in range(W + 1):
+                nosteal = prev[j]
+                steal = float('-inf')
+                if wt[i] <= j:
+                    steal = val[i] + prev[j - wt[i]]
+                
+                curr[j] = max(steal, nosteal)
+            prev = curr.copy()
+                
+        return prev[W]
+                    
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
