7dddc47a90d2f0d089fc8dd397fabfccef0f6072
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-12 06:59:16 +0300

    Added solution - LeetHub

--- 704512-knapsack-with-duplicate-items-GFG/704512-knapsack-with-duplicate-items.py
+++ 704512-knapsack-with-duplicate-items-GFG/704512-knapsack-with-duplicate-items.py
@@ -3,24 +3,38 @@ class Solution:
 
         dp = [[float('-inf')] * (capacity + 1) for _ in range(len(val))]
 
-        def getItem(i, w):
+        # def getItem(i, w):
             
-            if i == 0:
-                return (w//wt[0]) * val[0]
-            if dp[i][w] != float('-inf'):
-                return dp[i][w]
+        #     if i == 0:
+        #         return (w//wt[0]) * val[0]
+        #     if dp[i][w] != float('-inf'):
+        #         return dp[i][w]
                 
-            notake = 0 + getItem(i - 1, w)
+        #     notake = 0 + getItem(i - 1, w)
             
-            take = float('-inf')
-            if wt[i] <= w:
-                take = val[i] + getItem(i, w - wt[i])
+        #     take = float('-inf')
+        #     if wt[i] <= w:
+        #         take = val[i] + getItem(i, w - wt[i])
                 
-            value = max(notake, take)
-            dp[i][w] = value
-            return value
+        #     value = max(notake, take)
+        #     dp[i][w] = value
+        #     return value
             
-        return getItem(len(wt) - 1, capacity)
+        # return getItem(len(wt) - 1, capacity)
+        
+        for i in range(capacity + 1):
+            dp[0][i] = (i //wt[0]) * val[0]
+            
+        for i in range(1, len(wt)):
+            for j in range(capacity + 1):
+                notake = 0 + dp[i - 1][j]
+                take = float('-inf')
+                if wt[i] <= j:
+                    take = val[i] + dp[i][j - wt[i]]
+                    
+                dp[i][j] = max(notake, take)
+                
+        return dp[len(wt) - 1][capacity]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
