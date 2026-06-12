ee400bba6dece93713a17b48e0c606a7452f7b96
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-12 18:51:13 +0300

    Add solution for Longest Common Subsequence

--- leetcode/1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ leetcode/1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,17 +1,17 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n, m = len(text1), len(text2)
-        dp = [[float('-inf')] * m for _ in range(n)]
+        dp = [[float('-inf')] * (m + 1) for _ in range(n + 1)]
 
         def subsequence(i, j):
-            if i < 0 or j < 0:
+            if i == 0 or j == 0:
                 return 0
             if dp[i][j] != float('-inf'):
                 return dp[i][j]
 
             matched = float('-inf')
             notmatched = float('-inf')
-            if text1[i] == text2[j]:
+            if text1[i - 1] == text2[j - 1]:
                 matched = 1 + subsequence(i - 1, j - 1)
             else:
                 notmatched = max(subsequence(i - 1, j), subsequence(i, j - 1))
@@ -21,7 +21,7 @@ class Solution:
 
             return val
 
-        return subsequence(n - 1, m - 1)
+        return subsequence(n, m)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
