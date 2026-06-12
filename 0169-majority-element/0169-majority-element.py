b35c50d249c917e5f62a4f39b05281cbf22bfb78
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-12 15:11:56 +0300

    Time: 6 ms (61.56%), Space: 21.2 MB (52.43%) - LeetHub - Time: O(n), Space: O(1)

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        return [keys for keys, values in sorted(count.items(), key = lambda items: items[1], reverse=True)][0]
+        elem = 0
+        count = 0
+
+        for i in range(len(nums)):
+            if count == 0:
+                elem = nums[i]
+                count = 1
+            elif elem == nums[i]:
+                count += 1
+            else:
+                count -= 1
+        
+        return elem
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
