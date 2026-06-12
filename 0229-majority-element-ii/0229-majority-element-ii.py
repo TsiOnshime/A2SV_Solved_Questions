01906d58ef8a89bffea75d674aea9ad6f9f10d34
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-12 14:22:16 +0300

    Time: 7 ms (66.93%), Space: 23.3 MB (34.9%) - LeetHub - Time: O(n), Space: O(n)

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,11 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        
+        count = {}
+        n = len(nums)
+        for i in nums:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
+        res = []
+        for key, val in count.items():
+            if val > n // 3:
+                res.append(key)
 
-        count = Counter(nums)
+        return res
 
-        result = [keys for keys, value in count.items() if value > (len(nums) // 3)]
-       
-        return result
-
-        
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
