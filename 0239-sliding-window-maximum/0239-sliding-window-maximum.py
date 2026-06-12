6bfcca77830990c2821e37b582d9030547cf28ab
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-12 13:41:01 +0300

    Time: 187 ms (62.3%), Space: 35.2 MB (59.61%) - LeetHub - Time: O(n), Space: O(k)

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,30 +1,39 @@
-from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # monotonic decreasing queue
-        # if we get an element that is greater than the top element of our queue we pop
-        # if the left pointer is out of bounds we pop from the zeroth index
-        # how do we check if left is out of bounds?
-            # if the index at the zeroth index in our queue is less than our left index that means the que[0] is out of bounds
-        # how do we append an element to our output?
-            # if r + 1 is greater than or equal to k that means our right pointer has reached the window of 3 so we append queue[0] and increase the value of right pointer
-        # at each iteration we increment the value of our left pointer by 1
         queue = deque()
         output = []
-
-        l = r = 0
-        while r < len(nums):
-            while queue and nums[queue[-1]] < nums[r]:
-                queue.pop()
-            queue.append(r)
-
-            
-            if l > queue[0]:
-                queue.popleft()
-            if r + 1 >= k:
-                output.append(nums[queue[0]])
-                l += 1
-
+        l = 0
+        r = 0
+        while r < k:
+            if not queue or nums[r] <= queue[-1]:
+                queue.append(nums[r])
+            else:
+                while queue and queue[-1] < nums[r]:
+                    queue.pop()
+                queue.append(nums[r])
             r += 1
-        return output
         
+
+        if queue:
+            output.append(queue[0])
+
+        while r < len(nums):
+            if queue[0] == nums[l]:
+                queue.popleft()
+            l += 1
+            if not queue or nums[r] <= queue[-1]:
+                queue.append(nums[r])
+            else:
+                while queue and queue[-1] < nums[r]:
+                    queue.pop()
+                queue.append(nums[r])
+            r += 1
+            output.append(queue[0])
+
+        return output
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
