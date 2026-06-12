aedbeece1c4182396b4ba60aad859a4a114b0120
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-12 19:01:50 +0300

    Time: 756 ms (19.07%), Space: 19.3 MB (92.5%) - LeetHub - Time: O(n*m), Space: O(m)

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,12 +1,8 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n, m = len(text1), len(text2)
-        dp = [[float('-inf')] * (m + 1) for _ in range(n + 1)]
-
-        for i in range(n + 1):
-            dp[i][0] = 0
-        for j in range(m + 1):
-            dp[0][j] = 0
+        prev = [0] * (m + 1)
+        curr = [0] * (m + 1)
 
         for i in range(1, n + 1):
             for j in range(1, m + 1):
@@ -14,13 +10,14 @@ class Solution:
                 notmatched = float('-inf')
 
                 if text1[i - 1] == text2[j - 1]:
-                    matched = 1 + dp[i - 1][j - 1]
+                    matched = 1 + prev[j - 1]
                 else:
-                    notmatched = max(dp[i - 1][j], dp[i][j - 1])
+                    notmatched = max(prev[j], curr[j - 1])
                 val = max(matched, notmatched)
-                dp[i][j] = val
+                curr[j] = val
+            prev = curr.copy()
 
-        return dp[n][m]
+        return prev[m]
 
 
 # Synced seamlessly with LeetHub Pro
