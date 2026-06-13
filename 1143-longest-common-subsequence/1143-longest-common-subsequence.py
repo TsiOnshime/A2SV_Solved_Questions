1c7002e2231b1380156e868744242071b67e0a4d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-13 11:30:24 +0300

    Time: 1286 ms (8.64%), Space: 46.8 MB (20.25%) - LeetHub - Time: O(n*m), Space: O(n*m)

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,24 +1,25 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n, m = len(text1), len(text2)
-        prev = [0] * (m + 1)
-        curr = [0] * (m + 1)
+        dp = [[float('-inf')] * m for _ in range(n)]
 
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                matched = float('-inf')
-                notmatched = float('-inf')
+        def common(i, j):
 
-                if text1[i - 1] == text2[j - 1]:
-                    matched = 1 + prev[j - 1]
-                else:
-                    notmatched = max(prev[j], curr[j - 1])
-                val = max(matched, notmatched)
-                curr[j] = val
-            prev = curr.copy()
+            if i < 0 or j < 0:
+                return 0
+            if dp[i][j] != float('-inf'):
+                return dp[i][j]
 
-        return prev[m]
+            matches, nomatches = float('-inf'), float('-inf')
+            if text1[i] == text2[j]:
+                matches = 1 + common(i - 1, j - 1)
+            else:
+                nomatches = 0 + max(common(i - 1, j), common(i, j - 1))
+            val = max(matches, nomatches)
+            dp[i][j] = val
+            return val
 
+        return common(n - 1, m - 1)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
