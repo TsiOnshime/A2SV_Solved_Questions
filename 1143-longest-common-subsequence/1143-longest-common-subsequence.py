27d83b91ae7402c3d8307d31093dad65508fa103
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-13 11:35:23 +0300

    Time: 850 ms (18.21%), Space: 44.5 MB (45.79%) - LeetHub - Time: O(n*m), Space: O(n*m)

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,25 +1,24 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n, m = len(text1), len(text2)
-        dp = [[float('-inf')] * m for _ in range(n)]
+        dp = [[float('-inf')] * (m + 1) for _ in range(n + 1)]
 
-        def common(i, j):
+        for i in range(n + 1):
+            dp[i][0] = 0
+        for j in range(m + 1):
+            dp[0][j] = 0
+        
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                matches, nomatches = float('-inf'), float('-inf')
+                if text1[i - 1] == text2[j - 1]:
+                    matches = 1 + dp[i - 1][j - 1]
+                else:
+                    nomatches = 0 + max(dp[i - 1][j], dp[i][j - 1])
+                val = max(matches, nomatches)
+                dp[i][j] = val         
+        return dp[n][m]
 
-            if i < 0 or j < 0:
-                return 0
-            if dp[i][j] != float('-inf'):
-                return dp[i][j]
-
-            matches, nomatches = float('-inf'), float('-inf')
-            if text1[i] == text2[j]:
-                matches = 1 + common(i - 1, j - 1)
-            else:
-                nomatches = 0 + max(common(i - 1, j), common(i, j - 1))
-            val = max(matches, nomatches)
-            dp[i][j] = val
-            return val
-
-        return common(n - 1, m - 1)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
