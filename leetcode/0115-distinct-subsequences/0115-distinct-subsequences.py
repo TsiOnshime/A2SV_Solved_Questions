e7fffc417b343e091f7aa80aeda6166d30cb50b3
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-15 19:40:34 +0300

    Add solution for Distinct Subsequences

--- leetcode/0115-distinct-subsequences/0115-distinct-subsequences.py
+++ leetcode/0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -4,18 +4,14 @@ class Solution:
 
         n, m = len(s), len(t)
         prev = [0] * (m + 1)
-        curr = [0] * (m + 1)
-        curr[0] = 1
         prev[0] = 1
 
         for i in range(1, n + 1):
-            for j in range(1, m + 1):
+            for j in range(m, 0, -1):
                 if s[i - 1] == t[j - 1]:
-                    curr[j] = prev[j - 1] + prev[j]
-                else:
-                    curr[j] = prev[j]
+                    prev[j] = prev[j - 1] + prev[j]
 
-            prev = curr.copy()
+
 
         return prev[m]
 
