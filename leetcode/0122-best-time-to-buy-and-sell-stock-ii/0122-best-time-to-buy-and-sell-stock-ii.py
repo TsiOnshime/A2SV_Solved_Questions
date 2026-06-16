618d61bbeccf3db49915ffa747b4e28552b1e34d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-16 17:57:24 +0300

    Add solution for Best Time to Buy and Sell Stock II

--- leetcode/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ leetcode/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # 1 -> Buy
-        dp = [[float('-inf')] * 2 for _ in range(len(prices) + 1)]
-        for i in range(2):
-            dp[len(prices)][i] = 0
-        for i in range(len(prices) - 1, -1, -1):
-            for j in range(2):
-                if j:
-                    profit = max(-prices[i] + dp[i + 1][0], dp[i + 1][1])
-                else:
-                    profit = max(prices[i] + dp[i + 1][1], dp[i + 1][0])
-                
-                dp[i][j] = profit
+        frontBuy = frontNotBuy = 0
 
-        return dp[0][1]
+
+        for i in range(len(prices) - 1, -1, -1):
+
+            currBuy = max(-prices[i] + frontNotBuy, frontBuy)
+
+            currNotBuy = max(prices[i] + frontBuy, frontNotBuy)
+                
+            frontBuy = currBuy
+            frontNotBuy = currNotBuy
+
+        return frontBuy
         
 
 # Synced seamlessly with LeetHub Pro
