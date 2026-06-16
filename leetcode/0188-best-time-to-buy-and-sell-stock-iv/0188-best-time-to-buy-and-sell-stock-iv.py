2c5fa6d8f604bd405d1c281352c3140344304eb5
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-16 19:27:45 +0300

    Add solution for Best Time to Buy and Sell Stock IV

--- leetcode/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ leetcode/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,22 +1,24 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        
+        # t = 0, 1, 2, 3, 4
+        #     b. s. b. s. b
         n = len(prices)
-        front = [[0] * (k + 1) for _ in range(2)] 
-        curr = [[0] * (k + 1) for _ in range(2)] 
+        dp = [[float('-inf')] * (2 * k) for _ in range(len(prices))]
+        def calcProfit(i, t):
+            if i == n:
+                return 0
+            if t == 2 * k:
+                return 0
+            if dp[i][t] != float('-inf'):
+                return dp[i][t]
 
-        for i in range(len(prices) - 1, -1, -1):
-            curr = [[0] * (k + 1) for _ in range(2)] 
-            for j in range(2):
-                for l in range(1,k + 1):
-                    if j:
-                        profit = max(-prices[i] + front[0][l], front[1][l])
-                    else:
-                        profit = max(prices[i] + front[1][l - 1], front[0][l])
-                    
-                    curr[j][l] = profit
-            front = curr
-        return front[1][k]
+            if t % 2 == 0:
+                profit = max(-prices[i] + calcProfit(i + 1, t + 1), calcProfit(i + 1, t))
+            else:
+                profit = max(prices[i] + calcProfit(i + 1, t + 1), calcProfit(i + 1, t))
+            dp[i][t] = profit
+            return profit
+        return calcProfit(0, 0)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
