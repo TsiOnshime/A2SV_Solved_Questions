205cda224c3cd237f7f7aca7ec60277429670993
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-16 09:04:08 +0300

    Add solution for Wildcard Matching

--- leetcode/0044-wildcard-matching/0044-wildcard-matching.py
+++ leetcode/0044-wildcard-matching/0044-wildcard-matching.py
@@ -3,29 +3,33 @@ class Solution:
         # if the strings match / ? decrement i and j
         # if they don't match and the p[j] == * treat that * as empty and move j - 1 or treat * as sequence of characters and move i - 1 but if they don't match and p[j] != * return False
 
-        dp = [[float('inf')] * (len(p) + 1) for _ in range(len(s) + 1)]
-        dp[0][0] = True
-        for i in range(1, len(s) + 1):
-            dp[i][0] = False
+        prev = [float('inf')] * (len(p) + 1) 
+        curr = [float('inf')] * (len(p) + 1) 
+        prev[0] = True
+
         
         for j in range(1, len(p) + 1):
             flag = True
             for k in range(j):
                 if p[k] != "*":
                     flag = False
-            dp[0][j] = flag
+            prev[j] = flag
 
         for i in range(1, len(s) + 1):
+            curr[0] = False
             for j in range(1, len(p) + 1):
 
                 if s[i - 1] == p[j - 1] or p[j - 1] == "?":
-                    dp[i][j] =  dp[i - 1][j - 1]
+                    curr[j] =  prev[j - 1]
                 else:
                     if p[j - 1] != "*":
-                        dp[i][j] = False
+                        curr[j] = False
                     else:
-                        dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
-        return dp[len(s)][len(p)]
+                        curr[j] = curr[j - 1] or prev[j]
+            
+            prev = curr.copy()
+            
+        return prev[len(p)]
                         
 
  
