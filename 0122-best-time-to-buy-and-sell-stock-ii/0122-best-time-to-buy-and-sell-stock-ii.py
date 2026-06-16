acf1a50cbcf9a2ea6b3a82231148c998ba83b7f5
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-16 09:41:25 +0300

    Time: 2 ms (66.92%), Space: 20.4 MB (51.66%) - LeetHub - Time: O(n), Space: O(1)

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -8,11 +8,10 @@ class Solution:
         while sell < len(prices):
             if prices[buy] < prices[sell]:
                 profit += (prices[sell] - prices[buy])
-                sell += 1
-                buy += 1
-            else:
-                buy += 1
-                sell += 1
+            sell += 1
+            buy += 1
+
+
         return profit
 # sell = 1
 # len(prices) = 6
@@ -23,3 +22,8 @@ class Solution:
 # profit = 7
 # 4, 5
 # 5, 6
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
