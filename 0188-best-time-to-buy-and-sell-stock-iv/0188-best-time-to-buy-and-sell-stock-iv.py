3233e083176447d2bf2bb573c3d09175b8665781
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-16 19:11:46 +0300

    Time: 66 ms (39.55%), Space: 24.2 MB (33.16%) - LeetHub - Time: O(n * k), Space: O(n * k)

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -2,26 +2,18 @@ class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         
         n = len(prices)
-        dp = [[[float('-inf')] * (k + 1) for _ in range(2)] for i in range(len(prices))]
+        dp = [[[0] * (k + 1) for _ in range(2)] for i in range(len(prices) + 1)]
 
-        def calcProfit(i, buy, t):
-            if  i == n:
-                return 0
-            if t == 0:
-                return 0
-            if dp[i][buy][t] != float('-inf'):
-                return dp[i][buy][t]
-
-            if buy:
-                profit = max(-prices[i] + calcProfit(i + 1, 0, t), calcProfit(i + 1, 1, t))
-            else:
-                profit = max(prices[i] + calcProfit(i + 1, 1, t - 1), calcProfit(i + 1, 0, t))
-            dp[i][buy][t] = profit
-            return profit
-        
-        return calcProfit(0, 1, k)
-
-
+        for i in range(len(prices) - 1, -1, -1):
+            for j in range(2):
+                for l in range(1,k + 1):
+                    if j:
+                        profit = max(-prices[i] + dp[i + 1][0][l], dp[i + 1][1][l])
+                    else:
+                        profit = max(prices[i] + dp[i + 1][1][l - 1], dp[i + 1][0][l])
+                    
+                    dp[i][j][l] = profit
+        return dp[0][1][k]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
