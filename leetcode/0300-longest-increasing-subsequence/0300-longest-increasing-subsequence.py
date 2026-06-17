641022659cdb9d3b365a149005283428394fb7f5
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-17 11:38:04 +0300

    Add solution for Longest Increasing Subsequence

--- leetcode/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ leetcode/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,15 +1,27 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [1] * n
+        res = [nums[0]]
+        def search(arr, target):
 
-        for i in range(n):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp)
+            l = 0
+            r = len(arr) - 1
+ 
+            while l <= r:
+                mid = l + (r - l)//2
+                if arr[mid] < target:
+                    l = mid + 1
 
-        
+                else:
+                    r = mid - 1
+            return l
+
+        for i in range(1, len(nums)):
+            if nums[i] > res[-1]:
+                res.append(nums[i])
+            else:
+                idx = search(res, nums[i])
+                res[idx] = nums[i]
+        return len(res)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
