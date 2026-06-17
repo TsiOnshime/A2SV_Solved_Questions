67169303b8661d475deb9ae73f07073ec02ba0d3
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-17 06:27:40 +0300

    Time: 3 ms (61.34%), Space: 19.5 MB (68.41%) - LeetHub - Time: O(n), Space: O(1)

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,20 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[[0] * (2) for _ in range(2)] for _ in range(n + 1)]
+        front = [[0] * (2) for _ in range(2)] 
+        curr = [[0] * (2) for _ in range(2)]
 
         for i in range(n - 1, -1, -1):
             for j in range(2):
                 for k in range(2):
                     if k:
-                        dp[i][j][1] = dp[i + 1][j][0]
+                        curr[j][1] = front[j][0]
                         continue
                     if j:
-                        profit = max(-prices[i] + dp[i + 1][0][k], dp[i + 1][1][k])
+                        profit = max(-prices[i] + front[0][k], front[1][k])
                     else:
-                        profit = max(prices[i] + dp[i + 1][1][1], dp[i + 1][0][k])
-                    dp[i][j][k] = profit
-        return dp[0][1][0]
+                        profit = max(prices[i] + front[1][1], front[0][k])
+                    
+                    curr[j][k] = profit
+            front = [row[:] for row in curr]
+        return front[1][0]
 
 
 
