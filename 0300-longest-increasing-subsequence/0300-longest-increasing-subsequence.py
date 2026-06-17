c15fde898cdeec44ccb9d7b3f82b9c0f45639736
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-17 11:38:00 +0300

    Time: 7 ms (82.85%), Space: 19.6 MB (32.63%) - LeetHub - Time: O(n log n), Space: O(n)

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -5,15 +5,15 @@ class Solution:
 
             l = 0
             r = len(arr) - 1
-            ans = 0
+ 
             while l <= r:
                 mid = l + (r - l)//2
-                if nums[mid] < target:
+                if arr[mid] < target:
                     l = mid + 1
-                    ans = mid
+
                 else:
                     r = mid - 1
-            return ans
+            return l
 
         for i in range(1, len(nums)):
             if nums[i] > res[-1]:
