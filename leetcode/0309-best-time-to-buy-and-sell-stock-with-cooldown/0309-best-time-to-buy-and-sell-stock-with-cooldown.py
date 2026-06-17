a27ea892e5bba182a8e0dbe170a5424abd8c040b
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-17 06:22:35 +0300

    Add solution for Best Time to Buy and Sell Stock with Cooldown

--- leetcode/0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ leetcode/0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,23 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[[float('-inf')] * (2) for _ in range(2)] for _ in range(n)]
-        def calcProfit(i, b, c):
-            if i == n:
-                return 0
-            if c:
-                return calcProfit(i + 1, b, 0)
-            if dp[i][b][c] != float('-inf'):
-                return dp[i][b][c]
+        dp = [[[0] * (2) for _ in range(2)] for _ in range(n + 1)]
 
-            if b:
-                profit = max(-prices[i] + calcProfit(i + 1, 0, c), calcProfit(i + 1, 1, c))
-            else:
-                profit = max(prices[i] + calcProfit(i + 1, 1, 1), calcProfit(i + 1, 0, c))
-            dp[i][b][c] = profit 
-            return profit
-        
-        return calcProfit(0, 1, 0)
+        for i in range(n - 1, -1, -1):
+            for j in range(2):
+                for k in range(2):
+                    if k:
+                        dp[i][j][1] = dp[i + 1][j][0]
+                        continue
+                    if j:
+                        profit = max(-prices[i] + dp[i + 1][0][k], dp[i + 1][1][k])
+                    else:
+                        profit = max(prices[i] + dp[i + 1][1][1], dp[i + 1][0][k])
+                    dp[i][j][k] = profit
+        return dp[0][1][0]
+
+
 
 
 # Synced seamlessly with LeetHub Pro
