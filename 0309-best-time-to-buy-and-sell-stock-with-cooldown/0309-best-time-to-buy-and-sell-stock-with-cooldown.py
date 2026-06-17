3832866031d83164e1b443105ef65eaf4c387b4d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-17 06:43:44 +0300

    Time: 0 ms (100%), Space: 19.3 MB (95.35%) - LeetHub - Time: O(n), Space: O(1)

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,19 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[0] * (2) for _ in range(n + 1)]
+
+        front = [0] * 2
+        curr = [0] * 2
+        frontahead = 0
 
         for i in range(n - 1, -1, -1):
             for j in range(2):
                 if j:
-                    profit = max(-prices[i] + dp[i + 1][0], dp[i + 1][1])
+                    profit = max(-prices[i] + front[0], front[1])
                 else:
                     if i + 2 <= n:
-                        profit = max(prices[i] + dp[i + 2][1], dp[i + 1][0])
+                        profit = max(prices[i] + frontahead, front[0])
                     else:
-                        profit = max(prices[i],dp[i + 1][0])
-                dp[i][j] = profit
-        return dp[0][1]
+                        profit = max(prices[i],front[0])
+                curr[j] = profit
+            frontahead = front[1]
+            front = curr.copy()
+        return front[1]
 
 
 
