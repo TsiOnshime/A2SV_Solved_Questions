8a17214222ca30a746152c6e49741b646d6408d9
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-17 10:01:47 +0300

    Time: 3263 ms (5%), Space: 19.4 MB (59.6%) - LeetHub - Time: O(n^2), Space: O(n)

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,19 +1,21 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        front = [0] * (n + 1)
+        curr = [0] * (n + 1)
 
         for i in range(n - 1, -1, -1):
             for j in range(-1, i):
                 # take
                 take = 0
                 if j == -1 or nums[i] > nums[j]:
-                    take = 1 + dp[i + 1][i + 1]
+                    take = 1 + front[i + 1]
                 # notake
-                notake = dp[i + 1][j + 1]
+                notake = front[j + 1]
                 val = max(take, notake)
-                dp[i][j + 1] = val   
-        return dp[0][0]           
+                curr[j + 1] = val   
+            front = curr.copy()
+        return front[0]           
         
 
 # Synced seamlessly with LeetHub Pro
