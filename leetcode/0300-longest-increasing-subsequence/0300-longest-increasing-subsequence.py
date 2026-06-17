4e65304677c66e8a7c2de8b4aabe90f14413eacc
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-17 10:00:28 +0300

    Add solution for Longest Increasing Subsequence

--- leetcode/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ leetcode/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,24 +1,20 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[-1] * (n + 1) for _ in range(n + 1)]
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-        def calcLength(i, prev_idx):
-            if i == n:
-                return 0
-            if dp[i][prev_idx + 1] != -1:
-                return dp[i][prev_idx + 1]
-            # take
-            take = 0
-            if prev_idx == -1 or nums[i] > nums[prev_idx]:
-                take = 1 + calcLength(i + 1, i)
-            # notake
-            notake = calcLength(i + 1, prev_idx)
-            val = max(take, notake)
-            dp[i][prev_idx + 1] = val
-            return val
-
-        return calcLength(0, -1)
+        for i in range(n - 1, -1, -1):
+            for j in range(-1, i):
+                # take
+                take = 0
+                if j == -1 or nums[i] > nums[j]:
+                    take = 1 + dp[i + 1][i + 1]
+                # notake
+                notake = dp[i + 1][j + 1]
+                val = max(take, notake)
+                dp[i][j + 1] = val   
+        return dp[0][0]           
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
