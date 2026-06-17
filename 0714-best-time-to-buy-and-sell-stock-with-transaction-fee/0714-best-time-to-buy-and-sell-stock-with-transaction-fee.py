d5b93e95c71c663c7a57e8e7337cf1ed803e682d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-17 07:05:08 +0300

    Time: 134 ms (43.25%), Space: 26.3 MB (55.85%) - LeetHub - Time: O(n), Space: O(1)

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,16 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
-        dp = [[0] * 2 for _ in range(n + 1)]
+        front = [0] * 2
+        curr = [0] * 2
 
         for i in range(n - 1, -1, -1):
             for j in range(2):
                 if j:
-                    profit = max(-prices[i] + dp[i + 1][0], dp[i + 1][1])
+                    profit = max(-prices[i] + front[0], front[1])
                 else:
-                    profit = max(prices[i] - fee + dp[i + 1][1], dp[i + 1][0])
-                dp[i][j] = profit
-        return dp[0][1]
+                    profit = max(prices[i] - fee + front[1], front[0])
+
+                curr[j] = profit
+            front = curr.copy()
+        return front[1]
 
 
 
