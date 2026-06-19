c1bd36ef0a8546ac052bf7c32cac250ffe3c1263
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-19 18:28:09 +0300

    Time: 522 ms (40.85%), Space: 19.5 MB (37.52%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        # [1,3,5,4,7]
+
+        # dp = [1, 2, 3, 3, 4]
+        #cnt = [1, 1, 1, 1, 2]
+        n = len(nums)
+        dp = [1] * n
+        count = [1] * n
+        maxi = 0
+        _max = float('-inf')
+        maxis = set()
+
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if dp[j] + 1 == dp[i]:
+                        count[i] += count[j]
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        count[i] = count[j]
+            
+            if dp[i] > dp[maxi]:
+                maxi = i
+                maxis = {maxi}
+            elif dp[i] == dp[maxi]:
+                maxis.add(i)
+
+        res = 0
+        for i in maxis:
+            res += count[i]
+        return res
+
+                
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
