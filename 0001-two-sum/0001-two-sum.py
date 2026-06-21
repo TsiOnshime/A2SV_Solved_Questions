15d59f0457326b4839c96bfbfb02b66943807b68
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-21 15:52:16 +0300

    Time: 1 ms (60.65%), Space: 20.5 MB (41.68%) - LeetHub - Time: O(n), Space: O(n)

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = {nums[i]: i for i in range(len(nums))}
-        # {2:0, 7:1, 11:2, 15:3}
-        # {3:1}
-        for i in range(len(nums)): # 0    1
-            complement = target - nums[i] # 3
-            if complement in hashMap and hashMap[complement] != i:
-                return [i,hashMap[complement]]
+        self.index = {nums[i]: i for i in range(len(nums))}
 
-        return []
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in self.index and i != self.index[complement]:
+                return [i, self.index[complement]]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
