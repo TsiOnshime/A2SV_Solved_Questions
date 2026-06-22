940da51808b98196e60d869324943139f048b02b
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-22 17:38:37 +0300

    Add solution for Partition Array for Maximum Sum

--- leetcode/1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ leetcode/1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,24 +1,19 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         
-        dp = [-1] * len(arr)
-        def partition(i):
-            if i == len(arr):
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-
+        dp = [-1] * (len(arr) + 1)
+        dp[len(arr)] = 0
+        for i in range(len(arr) - 1, -1, -1):
             max_sum = float('-inf')
             curr_max = 0
             for j in range(i, len(arr)):
                 if j - i + 1 <= k:
                     curr_max = max(curr_max, arr[j])
-                    _sum = (j - i + 1) * curr_max + partition(j + 1)
+                    _sum = (j - i + 1) * curr_max + dp[j + 1]
                     max_sum = max(max_sum, _sum)
             dp[i] = max_sum
-            return max_sum
-        
-        return partition(0)
+        return dp[0]
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
