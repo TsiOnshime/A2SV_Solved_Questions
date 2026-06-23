c0e00c00f82d5bcd36a1bc2bb8a72fdb1267aa06
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-23 19:02:11 +0300

    Time: 0 ms (100%), Space: 20.6 MB (49.89%) - LeetHub - Time: O(n), Space: O(n)

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -1,31 +1,23 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        if len(nums) == 1:
-            return 0
-        print(nums)
-        i, j = 0, 1
+        freq = {}
         count = 0
-
-        while i < len(nums) and j < len(nums):
-            while i < len(nums) and i > 0 and nums[i] == nums[i -1]:
-                i += 1
-            while j < len(nums) and j > 1 and nums[j] == nums[j - 1] and i != j - 1:
-                j += 1
-            if i < len(nums) and j < len(nums):
-                diff = nums[j] - nums[i]
-                if diff == k:
-                    if i != j:
-                        
-                        count += 1
-                    j += 1
-                elif diff > k:
-                    i += 1
-                elif diff < k:
-                    j += 1
-        return count
-    
+        for i in nums:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i] += 1
         
+        for key, val in freq.items():
+            if k == 0:
+                if val >= 2:
+                    count += 1
+            else:
+                otherval = key + k
+                if otherval in freq:
+                    count += 1
+
+        return count
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
