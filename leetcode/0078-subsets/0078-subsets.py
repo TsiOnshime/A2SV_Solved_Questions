03eee03708bc34391b5e88120a2257d3b4fcfa64
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-25 19:17:44 +0300

    Add solution for Subsets

--- leetcode/0078-subsets/0078-subsets.py
+++ leetcode/0078-subsets/0078-subsets.py
@@ -1,19 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        solution = []
-        state = []
-        def search(i):
-            if i >= len(nums):
-                solution.append(state.copy())
+        res = []
+        def dfs(i, state):
+            if i == len(nums):
+                res.append(state.copy())
                 return
             
-            # decision to include nums[i]
             state.append(nums[i])
-            search(i + 1)
-
-            # decision not to include nums[i]
+            dfs(i + 1, state)
             state.pop()
-            search(i + 1)
 
-        search(0)
-        return solution
+            dfs(i + 1, state)
+        
+        dfs(0, [])
+        return res
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
