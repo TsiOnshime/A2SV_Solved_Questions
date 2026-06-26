36c538a396d02a7eeed7768d0e59b33083c69403
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-26 07:31:54 +0300

    Time: 4 ms (10.06%), Space: 19.5 MB (33.35%) - LeetHub - Time: O(n*n!), Space: O(n)

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,20 +1,25 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        path = []
-        ans = []
-
-        def backtrack(nums):
-            if not nums:
-                ans.append(path.copy())
-
-            for i in nums:
-                path.append(i)
-
-                potential_candidate = nums[:]
-                potential_candidate.remove(i)
-                backtrack(potential_candidate)
-                path.pop()
         
+        res = []
 
-        backtrack(nums)
-        return ans
+        def permutiations(nums, state):
+            if not nums:
+                res.append(state.copy())
+                return
+            
+            for i in range(len(nums)):
+                state.append(nums[i])
+                new_nums = nums[:i] + nums[i + 1:]
+                permutiations(new_nums, state)
+                state.pop()
+
+        permutiations(nums, [])
+        return res
+
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
