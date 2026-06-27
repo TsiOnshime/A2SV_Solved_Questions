b40ea7c406a6fe0c67f15e75e1722a6e27ce3129
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-27 19:23:03 +0300

    Time: 5 ms (49.87%), Space: 20.5 MB (61.07%) - LeetHub - Time: O(, Space: O(?)

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,15 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l = r = 0
-
-        while r < len(nums):
-            if nums[r] == 0:
-                r += 1
+        new_nums = []
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                new_nums.append(nums[i])
+        
+        for i in range(len(nums)):
+            if i < len(new_nums):
+                nums[i] = new_nums[i]
             else:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r += 1
+                nums[i] = 0
+        
+        
 
-        return nums
-
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
