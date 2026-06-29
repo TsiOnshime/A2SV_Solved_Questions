056754900240173440a76ab47d087c851b58698e
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-29 09:53:15 +0300

    Time: 5550 ms (5.01%), Space: 57.2 MB (5.16%) - LeetHub - Time: O(n^2), Space: O(n^2)

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
@@ -2,28 +2,30 @@ class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
 
         pairs.sort(key=lambda x:x[1])
-        dp = [0] * len(pairs)
+        dp = [[0] * (len(pairs) + 1) for _ in range(len(pairs))]
+
         max_length = 0
         def findlength(i, prev):
             nonlocal dp
             if i == len(pairs):
                 return 0
-            if dp[i]:
-                return dp[i]
+            if dp[i][prev + 1]:
+                return dp[i][prev + 1]
+
 
             # take 
             take = 0
-            if prev < pairs[i][0]:
-                take = 1 + findlength(i + 1, pairs[i][1])
+            if prev == -1 or pairs[prev][1] < pairs[i][0]:
+                take = 1 + findlength(i + 1, i)
             # no take
             notake = findlength(i + 1, prev)
 
-            dp[i] = max(take, notake)
-            return dp[i]
+            dp[i][prev + 1] = max(take, notake)
+            return dp[i][prev + 1]
         
 
         for i in range(len(pairs)):
-            max_length = max(max_length, findlength(i, float('-inf')))
+            max_length = max(max_length, findlength(i, -1))
         
         return max_length
         
