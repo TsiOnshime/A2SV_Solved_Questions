5607227c7c1bf693694acb130c469bd323161a35
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-29 10:24:56 +0300

    Time: 3489 ms (5.3%), Space: 27.2 MB (5.81%) - LeetHub - Time: O(n^2), Space: O(n^2)

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
@@ -2,32 +2,22 @@ class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
 
         pairs.sort(key=lambda x:x[1])
-        dp = [[0] * (len(pairs) + 1) for _ in range(len(pairs))]
+        dp = [[0] * (len(pairs) + 1) for _ in range(len(pairs) + 1)]
 
         max_length = 0
-        def findlength(i, prev):
-            nonlocal dp
-            if i == len(pairs):
-                return 0
-            if dp[i][prev + 1]:
-                return dp[i][prev + 1]
 
-
-            # take 
-            take = 0
-            if prev == -1 or pairs[prev][1] < pairs[i][0]:
-                take = 1 + findlength(i + 1, i)
-            # no take
-            notake = findlength(i + 1, prev)
-
-            dp[i][prev + 1] = max(take, notake)
-            return dp[i][prev + 1]
-        
+        for i in range(len(pairs) - 1, -1, -1):
+            for j in range(-1, len(pairs)):
+                take = 0 
+                if j == -1 or pairs[j][1] < pairs[i][0]:
+                    take = 1 + dp[i + 1][i + 1]
+                notake = dp[i + 1][j + 1]
+                dp[i][j + 1] = max(take, notake)
 
         for i in range(len(pairs)):
-            max_length = max(max_length, findlength(i, -1))
-        
+            max_length = max(max_length, dp[i][0])
         return max_length
+
         
 
 # Synced seamlessly with LeetHub Pro
