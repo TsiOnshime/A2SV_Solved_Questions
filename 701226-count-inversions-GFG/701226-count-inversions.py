43a565b29a985e46c23affee1cabddfedf5ccd2c
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-30 11:00:06 +0300

    Added solution - LeetHub

--- 701226-count-inversions-GFG/701226-count-inversions.py
+++ 701226-count-inversions-GFG/701226-count-inversions.py
@@ -1,34 +1,46 @@
 class Solution:
     def inversionCount(self, arr):
         # Code Here
-        self.count = 0
-        def divide(l, r):
-            if l == r:
-                return
-            mid = l + (r - l)//2
-            divide(l, mid)
-            divide(mid + 1, r)
-            merge(l, mid, r)
-            
+        count = 0
         def merge(l, mid, r):
+            
+            first = l 
+            second = mid + 1
             temp = []
-            low = l
-            high = mid + 1
-            
-            while low <= mid and high <= r:
-                if arr[low] <= arr[high]:
-                    temp.append(arr[low])
-                    low += 1
+            cnt = 0
+            while first <= mid and second <= r:
+                if arr[first] <= arr[second]:
+                    temp.append(arr[first])
+                    first += 1
                 else:
-                    self.count += (mid - low + 1)
-                    temp.append(arr[high])
-                    high += 1
-                    
-            temp.extend(arr[low:mid + 1])
-            temp.extend(arr[high:r + 1])
-            
+                    cnt += mid - first + 1
+                    temp.append(arr[second])
+                    second += 1
+            while first <= mid:
+                temp.append(arr[first])
+                first += 1
+
+            while second <= r:
+                temp.append(arr[second])
+                second += 1   
             for i in range(l, r + 1):
                 arr[i] = temp[i - l]
+            return cnt
             
-        divide(0, len(arr) - 1)
-        return self.count
+            
+        def divide(l, r):
+            nonlocal count
+            if l >= r:
+                return 
+            mid = l + (r - l)//2
+            
+            divide(l,mid)
+            divide(mid + 1, r)
+            
+            count += merge(l, mid, r)
+        divide(0, len(arr) - 1) 
+        return count
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
