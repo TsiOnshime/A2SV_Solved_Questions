245cf2838b9abb5aa7e9e7e730f513eb0d369933
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-06-30 07:36:39 +0300

    Add solution for Palindrome Partitioning

--- leetcode/0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ leetcode/0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,31 +1,28 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        path = []
-        # i => start of the new partition
-        # j => 
-        def isPalindrome(i, j):
-            while i < j:
-                if s[i] != s[j]:
+        def check(l, r):
+            while l < r:
+                if s[l] != s[r]:
                     return False
-                i += 1
-                j -= 1
+                r -= 1
+                l += 1
             return True
-        def partS(i):
+        def palindrome(idx, state):
             nonlocal res
-            nonlocal path
-            if i == len(s):
-                res.append(path.copy())
+            if idx == len(s):
+                res.append(state.copy())
                 return
 
-            for k in range(i, len(s)):
-                if isPalindrome(i, k):
-                    path.append(s[i: k + 1])
-                    partS(k + 1)
-                    path.pop()
-                
-        partS(0)
+
+            for i in range(idx, len(s)):
+                if check(idx, i):
+                    state.append(s[idx:i + 1])
+                    palindrome(i + 1, state)
+                    state.pop()
+        palindrome(0, [])
         return res
+            
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
