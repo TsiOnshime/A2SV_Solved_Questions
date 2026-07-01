dc421cbeb1d1ecbe9c8e8e0874daf8fd4268a72a
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-01 11:29:20 +0300

    Add solution for Evaluate Reverse Polish Notation

--- leetcode/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ leetcode/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,26 +1,23 @@
-import math
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-
-        operators = {'+', '-', '*', '/'}
-       
-        stack = []
-
+        operands = []
+        ops = {"+", "-", "*", "/"}
         for i in range(len(tokens)):
-            if tokens[i] in operators:
-                op1 = int(stack.pop())
-                op2 = int(stack.pop())
-                if tokens[i] == "+":
-                    result = op1 + op2
-                elif tokens[i] == "-":
-                    result = op2 - op1
-                elif tokens[i] == "*":
-                    result = op2 * op1
-                else:
-                    result = int(op2 / op1)
-                stack.append(result)
-            
+            if tokens[i] not in ops:
+                operands.append(tokens[i])
             else:
-                stack.append(tokens[i])
-        
-        return int(stack[0])
+                
+                second = int(operands.pop())
+                first = int(operands.pop())
+                if tokens[i] == "+": res = first + second
+                elif tokens[i] == "-": res = first - second
+                elif tokens[i] == "*": res = first * second
+                else: res = int(first / second)
+                operands.append(res)
+        return int(operands[0])
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
