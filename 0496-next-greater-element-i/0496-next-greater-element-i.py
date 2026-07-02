b5e3c3a27d5eb5c17db239f23bfc9878997ecc33
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-02 07:45:01 +0300

    Time: 3 ms (40.91%), Space: 19.5 MB (28.17%) - LeetHub - Time: O(n + m), Space: O(n + m)

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,16 +1,20 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hashMap = {n: i for i, n in enumerate(nums1)}
+        nge = defaultdict(lambda: -1)
+        res =[]
         stack = []
-        res = [-1] * len(nums1)
+        for i in range(len(nums2) - 1, -1, -1):
+            while stack and stack[-1] <= nums2[i]:
+                stack.pop()
+            nge[nums2[i]] = -1 if not stack else stack[-1]
+            stack.append(nums2[i])
 
-        for i in range(len(nums2)):
-            while stack and stack[-1] < nums2[i]:
-                popped = stack.pop()
-                if popped in hashMap:
-                    res[hashMap[popped]] = nums2[i]
-            
-            if nums2[i] in hashMap:
-                stack.append(nums2[i])
-
+        for i in range(len(nums1)):
+            res.append(nge[nums1[i]])
         return res
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
