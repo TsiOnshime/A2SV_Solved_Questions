f098a36fcf27678cab30ce74eb5ada662d760e40
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-02 06:35:39 +0300

    Add solution for Plus One

--- leetcode/0066-plus-one/0066-plus-one.py
+++ leetcode/0066-plus-one/0066-plus-one.py
@@ -1,20 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        _sum = digits[-1] + 1
-        carry = _sum // 10
-        last_dig = _sum % 10
         
-        digits[-1] = last_dig
 
-        for i in range(len(digits) - 2, -1, -1):
-            _sum = digits[i] + carry
-            digits[i] = _sum % 10
-            carry = _sum // 10
-
-        if carry:
-            digits.insert(0, carry)
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        digits.insert(0, 1)
         return digits
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
