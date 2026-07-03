41c1bfea9801f379df3241a5ddb605a5f7b88cc9
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-03 16:09:15 +0300

    Add solution for Trapping Rain Water

--- leetcode/0042-trapping-rain-water/0042-trapping-rain-water.py
+++ leetcode/0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,31 +1,29 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l, r = 0, len(height) - 1
 
-        maxL, maxR = height[l], height[r]
+
 
         rain = 0
-        while l <= r:
-            if maxL <= maxR:
-                diff = maxL - height[l]
-                if diff > 0:
-                    rain += diff
-                l += 1
-                maxL = max(maxL, height[l - 1])
-
-            else:
-                diff = maxR - height[r]
-                if diff > 0:
-                    rain += diff
-                r -= 1
-                maxR = max(maxR, height[r + 1])
-
+        prefix_max = [height[0]] * len(height)
+        suffix_max = [height[-1]] * len(height)
+        
+        for i in range(1, len(height)):
+            prefix_max[i] = max(prefix_max[i - 1], height[i])
+            
+        for i in range(len(height) - 2, -1, -1):
+            suffix_max[i] = max(suffix_max[i + 1], height[i])
+            
+        for i in range(len(height)):
+            if height[i] < prefix_max[i] and height[i] < suffix_max[i]:
+                rain += min(prefix_max[i], suffix_max[i]) - height[i]
         return rain
-
-
-
-
+        
+    # [3, 0, 1, 0, 4, 0, 2]
+    
+    # [4, 0, 2]
+    
+    
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
