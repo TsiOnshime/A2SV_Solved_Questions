cd5ead614850ca71eadee6af93d10eab9d2cba23
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-04 18:53:22 +0300

    Add solution for Maximal Rectangle

--- leetcode/0085-maximal-rectangle/0085-maximal-rectangle.py
+++ leetcode/0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,40 +1,45 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        
-        n, m = len(matrix), len(matrix[0])
-
-        def findMaximumArea(arr):
-            stack = []  # (height, start_index)
-            max_area = 0
-
-            for i in range(m):
-                start = i
-
-                while stack and stack[-1][0] > arr[i]:
-                    height, idx = stack.pop()
-                    max_area = max(max_area, height * (i - idx))
-                    start = idx
-
-                stack.append((arr[i], start))
-
-            # Remaining bars extend to the end
-            while stack:
-                height, idx = stack.pop()
-                max_area = max(max_area, height * (m - idx))
-
-            return max_area
-
-        heights = [0] * m
+        rows, cols = len(matrix), len(matrix[0])
         max_area = 0
-        for i in range(n):
-            for j in range(m):
+        
+        heights = [0] * cols
+
+        def findMaxArea(heights):
+        # [3, 1, 2, 3, 2]
+        # stack = [1, 2, 4] p = 2, n = 4, area = 3 
+            stack = []
+            area = 0
+            for i in range(len(heights)):
+                while stack and heights[stack[-1]] > heights[i]:
+                    idx = stack.pop()
+                    pse = stack[-1] if stack else -1
+                    nse = i
+                    area = max(area, heights[idx] * (nse - pse - 1))
+                stack.append(i)
+
+            nse = len(heights)
+            while stack:
+                idx = stack.pop()
+                pse = stack[-1] if stack else -1
+                area = max(area, heights[idx] * (nse - pse - 1))
+    
+            return area
+
+
+
+        for i in range(rows-1, -1, -1):
+            for j in range(cols):
                 if matrix[i][j] == "1":
                     heights[j] += 1
-                else: heights[j] = 0
-
-            max_area = max(max_area, findMaximumArea(heights))
+                else:
+                    heights[j] = 0
+            max_area = max(max_area, findMaxArea(heights))
 
         return max_area
+
+
+
 
 
 # Synced seamlessly with LeetHub Pro
