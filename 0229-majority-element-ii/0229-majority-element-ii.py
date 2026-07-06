8246846268908d747b810788c0d2035a9d0ce90b
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-06 19:25:33 +0300

    Time: 13 ms (13.65%), Space: 22.5 MB (52%) - LeetHub - Time: O(?), Space: O(?)

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,18 +1,43 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        count = {}
-        n = len(nums)
-        for i in nums:
-            if i in count:
-                count[i] += 1
-            else:
-                count[i] = 1
         res = []
-        for key, val in count.items():
-            if val > n // 3:
-                res.append(key)
+        count1 = 0
+        elem1 = 0
 
+        count2 = 0
+        elem2 = 0
+
+        for i in range(len(nums)):
+            if count1 == 0 and nums[i] != elem2:
+                count1 = 1
+                elem1 = nums[i]
+            elif count2 == 0 and nums[i] != elem1:
+                count2 = 1
+                elem2 = nums[i]
+            elif nums[i] == elem1:
+                count1 += 1
+            elif nums[i] == elem2:
+                count2 += 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        count1 = count2 = 0
+        for i in range(len(nums)):
+            if nums[i] == elem1:
+                count1 += 1
+            elif nums[i] == elem2:
+                count2 += 1
+        if count1 > len(nums) // 3:
+            res.append(elem1)
+        if count2 > len(nums) // 3:
+            res.append(elem2)
         return res
+
+
+        
+            
+
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
