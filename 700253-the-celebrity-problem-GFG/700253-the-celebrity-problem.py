6ac7c5a6987c65a09dc92fb87359d43f2de42145
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-06 09:00:46 +0300

    Added solution - LeetHub

--- 700253-the-celebrity-problem-GFG/700253-the-celebrity-problem.py
+++ 700253-the-celebrity-problem-GFG/700253-the-celebrity-problem.py
@@ -1,23 +1,20 @@
 class Solution:
     def celebrity(self, mat):
-        # code here
-        rows, cols = len(mat), len(mat[0])
-        known = [0] * cols
         
-        for i in range(rows):
-            for j in range(cols):
-                known[j] += mat[i][j]
-        count, ind = 0, -1
-        for i in range(cols):
-            if known[i] == cols:
-                count += 1
-                ind =i
-        if count != 1:
-            return -1
-        for i in range(cols):
-            if ind != i and mat[ind][i] != 0:
-                return -1
-        return ind
+        knowme = [0] * len(mat[0])
+        iknow = [0] * len(mat[0])
+        
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j] == 1:
+                    knowme[j] += 1
+                    iknow[i] += 1
+        for i in range(len(mat)):
+            if knowme[i] == len(mat) and iknow[i] == 1:
+                return i
+        return -1
+                
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
