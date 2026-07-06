2ad3c066b98c3070e5e67b6ee38e03ce2fdbc1f6
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-06 21:03:39 +0300

    Add solution for Shortest Path in Binary Matrix

---
+++ leetcode/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        visited = set()
+        queue = deque()
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+        if grid[0][0] != 0:
+            return -1
+        def is_valid(r, c):
+            if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == 0 and (r, c) not in visited:
+                return True
+            return False
+        queue.append((0, 0, 1))
+        visited.add((0, 0))
+
+        while queue:
+            r, c, pathLen = queue.popleft()
+            if r == len(grid) - 1 and c == len(grid[0]) - 1:
+                return pathLen
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if is_valid(nr, nc):
+                    queue.append([nr, nc, pathLen + 1])
+                    visited.add((nr, nc))
+
+        return -1
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
