aebbe52b256583ea9ec551df20287bfcb3f5e90e
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-06 17:37:06 +0300

    Time: 250 ms (67.32%), Space: 19.3 MB (36.13%) - LeetHub - Time: O, Space: O(?)

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -7,29 +7,22 @@ class Solution:
             while j >= 0 and k < len(string) and string[j] == string[k]:
                 j -= 1
                 k += 1
-            return [k - j + 1 - 2, j, k]
+            return [k - j + 1, j + 1, k - 1]
+
         for i in range(len(string)):
             # when it is the middle
-            j, k = i - 1, i + 1
+            j, k = i, i
             res, st, en = findMax(j, k)
             if res > max_length:
                 max_length = res
                 s, e = st, en
-            
             j, k = i, i + 1
             res, st, en = findMax(j, k)
             if res > max_length:
                 max_length = res
                 s, e = st, en
-            j, k = i - 1, i
-            res, st, en = findMax(j, k)
-            if res > max_length:
-                max_length = res
-                s, e = st, en
-            # when it is the left char
-            
-            # when it is the right char
-        return string[s + 1:e]
+
+        return string[s:e + 1]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
