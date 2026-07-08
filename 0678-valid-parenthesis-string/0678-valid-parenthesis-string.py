5737d3c0ec93d505c8aae07d262bf8998e5d5ed9
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-08 09:18:53 +0300

    Time: 0 ms (100%), Space: 19.3 MB (29.99%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,27 +1,24 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        dp = [[float('-inf')]*len(s) for _ in range(len(s))]
 
-        def is_valid(i, count):
-            if count < 0:
-                return False
-            if i == len(s):
-                return count == 0
-            if dp[i][count] != float('-inf'):
-                return dp[i][count]
+        _min = 0 # represents the minimum number of opening brackets till i
+        _max = 0 # represents the maximum number of opening brackets till i
 
+        for i in range(len(s)):
             if s[i] == "(":
-                dp[i][count] = is_valid(i + 1, count + 1)
-                return dp[i][count]
+                _min += 1
+                _max += 1
             elif s[i] == ")":
-                dp[i][count] = is_valid(i + 1, count - 1)
-                return dp[i][count]
+                _min -= 1
+                _max -= 1
             else:
-                dp[i][count] = is_valid(i + 1, count + 1) or is_valid(i + 1, count) or is_valid(i + 1, count - 1)
-                return dp[i][count]
-
-
-        return is_valid(0, 0)
+                _min -= 1
+                _max += 1
+            if _min < 0:
+                _min = 0
+            if _max < 0:
+                return False
+        return _min == 0
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
