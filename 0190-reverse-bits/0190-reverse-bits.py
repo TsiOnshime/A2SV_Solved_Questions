fe96ed9b2c55145af1bcc80ac92e94587f56d490
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-10 17:57:16 +0300

    Time: 54 ms (11.04%), Space: 19.1 MB (63.76%) - LeetHub - Time: O(1), Space: O(1)

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,11 +1,11 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        res = []
+        res = 0
         for i in range(32):
             if n & (1 << i) != 0:
                 
-                res.append((2) ** (31 - i))
-        return sum(res)
+                res = res | (2) ** (31 - i)
+        return res
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
