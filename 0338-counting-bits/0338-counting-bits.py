c6626f14ed547843bb90fd73473b7a7ccd92c504
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-10 11:47:27 +0300

    Time: 6 ms (49.79%), Space: 20.2 MB (43.05%) - LeetHub - Time: O(n), Space: O(n)

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,13 +1,13 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        for i in range(n + 1):
-            count = 0
-            while i:
-                count += i & 1
-                i = i >> 1
-            ans.append(count)
-        return ans
+        dp = [0] * (n + 1)
+
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
