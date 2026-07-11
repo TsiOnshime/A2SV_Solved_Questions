fa36359ddfdfca838281ba513d286b3feb1767b3
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-11 13:00:42 +0300

    Add solution for Single Number II

--- leetcode/0137-single-number-ii/0137-single-number-ii.py
+++ leetcode/0137-single-number-ii/0137-single-number-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-
         ones = 0
         twos = 0
 
         for i in range(len(nums)):
             ones = (ones ^ nums[i]) & ~twos
             twos = (twos ^ nums[i]) & ~ones
-
+        
         return ones
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
