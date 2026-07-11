b62205dc4419bb7c90e23d7e961211965bf30809
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-11 20:57:27 +0300

    Time: 0 ms (100%), Space: 19.4 MB (18.71%) - LeetHub - Time: O(?), Space: O(?)

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
@@ -1,11 +1,16 @@
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
+        xor = 0
 
-        
-        output = start + (2 * 0)
+ 
+        i = 0
 
-        
-        for i in range(1, n):
-            output ^= (start + (2 * i))
+        while i < n:
+            num = start + 2 * i
+            xor ^= num
+            i += 1
+        return xor
 
-        return output
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
