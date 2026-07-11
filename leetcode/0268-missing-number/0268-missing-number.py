47cdb3283a0c0bf02b508b206ec881596dda2629
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-11 12:28:24 +0300

    Add solution for Missing Number

--- leetcode/0268-missing-number/0268-missing-number.py
+++ leetcode/0268-missing-number/0268-missing-number.py
@@ -1,21 +1,42 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        i = 0
-        n = len(nums)
-# [0,1,2,3,4,5,6,9,7]
-#                  i 
-        while i < n:
-            while i < n and nums[i] < n and nums[i] != i:
-                temp = nums[i]
-                nums[i], nums[temp] = nums[temp], nums[i]
-            i += 1
+        actual = 0
+        for i in range(len(nums)):
+            actual ^= nums[i]
+        
+        def xorAllNumbers(n):     
+            if n % 4 == 0:
+                return n
+            if n % 4 == 1:
+                return 1
+            if n % 4 == 2:
+                return n + 1
+            else: 
+                return 0
 
-        for i in range(n):
-            if nums[i] != i:
-                return i
+        expected = xorAllNumbers(len(nums))
+        print(expected)
+        res = actual ^ expected
+        return res
 
-        return n
+
+
+# 0 ^ 1 = 1
+# 0 ^ 1 ^ 2 = 3
+# 0 ^ 1 ^ 2 ^ 3 = 0
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 = 4
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 = 1
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 = 7
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 = 0
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 ^ 8 = 8
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 ^ 8 ^ 9 = 1
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 ^ 8 ^ 9 ^ 10 = 11
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 ^ 8 ^ 9 ^ 10 ^ 11 = 0
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 ^ 8 ^ 9 ^ 10 ^ 11 ^ 12 = 12
+# 0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 ^ 8 ^ 9 ^ 10 ^ 11 ^ 12 ^ 13 = 1 
+
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
