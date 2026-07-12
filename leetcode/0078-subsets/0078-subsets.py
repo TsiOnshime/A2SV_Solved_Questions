ab3a164df6cdec38ca46cc5a34d3afcf7c44faad
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-12 08:55:01 +0300

    Add solution for Subsets

--- leetcode/0078-subsets/0078-subsets.py
+++ leetcode/0078-subsets/0078-subsets.py
@@ -7,10 +7,9 @@ class Solution:
             for i in range(len(nums)):
                 if number & (1 << i):
                     subset.append(nums[i])
-            res.append(subset.copy())
-        
-        return res
+            res.append(subset)
 
+        return res
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
