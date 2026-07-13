4f0e10797d12faaf965202ee842f6178c4c09553
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-13 12:51:58 +0300

    Time: 38 ms (13.17%), Space: 19.7 MB (7.23%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,22 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_freq = defaultdict(int)
-        length = 0
+        max_length = 0
+        count = defaultdict(int)
+
         l = 0
-        # r = 3 l = 1
-        # abcabcbb
-        # {a: 1, b: 1, c: 1}
-        for r in range(len(s)):
-            char_freq[s[r]] += 1
-            while char_freq[s[r]] > 1:
-                char_freq[s[l]] -= 1
+        r = 0
+        while r < len(s):
+            count[s[r]] += 1
+            while count[s[r]] > 1:
+
+                count[s[l]] -= 1
                 l += 1
-            length = max(length, r - l + 1)
-
-        return length
-
-
+            print(l)
+            max_length = max(max_length, r - l + 1)
+            r += 1
+        return max_length
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
