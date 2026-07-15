b7173b5230aaabdd4fcd2bb5bca346402b24814d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-15 06:17:05 +0300

    Time: 199 ms (27.75%), Space: 25.8 MB (64.32%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        picked = {}
+        length = 0
+        l, r = 0, 0
+
+        while r < len(fruits):
+            if fruits[r] in picked:
+                picked[fruits[r]] += 1
+            else:
+                picked[fruits[r]] = 1
+            
+            while len(picked) > 2:
+                picked[fruits[l]] -= 1
+                if picked[fruits[l]] == 0:
+                    del picked[fruits[l]]
+                l += 1
+            length = max(length, r - l + 1)
+            r += 1
+        return length
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
