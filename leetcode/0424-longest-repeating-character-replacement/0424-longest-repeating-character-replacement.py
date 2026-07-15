7eb5d341c13197a7645065d0e58b64b95eac8a67
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-15 10:13:42 +0300

    Add solution for Longest Repeating Character Replacement

---
+++ leetcode/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        length = 0
+        maxFreq = 0
+        max_length = 0
+        l, r = 0, 0
+        chars = [0] * 26
+        offset = ord('A')
+
+        while r < len(s):
+            chars[ord(s[r]) - offset] += 1
+            maxFreq = max(maxFreq, chars[ord(s[r]) - offset])
+            length += 1
+            while length - maxFreq > k:
+                chars[ord(s[l]) - offset] -= 1
+                maxFreq = max(chars)
+                l += 1
+                length -= 1
+            max_length = max(max_length, length)
+            r += 1
+        return max_length
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
