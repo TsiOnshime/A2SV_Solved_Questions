c9ea5a67297dd3c44fa324552e5b1c1ce2cb5731
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-15 10:37:36 +0300

    Time: 79 ms (75.9%), Space: 19.6 MB (50.05%) - LeetHub - Time: O(n), Space: O(1)

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,19 +1,26 @@
 class Solution:
-    def characterReplacement(self, s, k):
-        
-        count = {}
-        res = 0
+    def characterReplacement(self, s: str, k: int) -> int:
+        length = 0
+        maxFreq = 0
+        max_length = 0
+        l, r = 0, 0
+        chars = [0] * 26
+        offset = ord('A')
 
-        l = 0
-
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-
-            while (r - l + 1) - max(count.values()) > k:
-                count[s[l]] -= 1
+        while r < len(s):
+            chars[ord(s[r]) - offset] += 1
+            maxFreq = max(maxFreq, chars[ord(s[r]) - offset])
+            length += 1
+            if length - maxFreq > k:
+                chars[ord(s[l]) - offset] -= 1
                 l += 1
+                length -= 1
+            if length - maxFreq <= k:
+                max_length = max(max_length, length)
+            r += 1
+        return max_length
 
 
-            res = max(res, r - l + 1)
-
-        return res
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
