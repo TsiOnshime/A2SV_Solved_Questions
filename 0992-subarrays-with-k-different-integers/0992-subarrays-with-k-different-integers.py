4a187167f503bfae71bb1a7b3828e49e96ced73a
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-15 18:58:21 +0300

    Time: 142 ms (41.26%), Space: 22 MB (40.25%) - LeetHub - Time: O(n), Space: O(n)

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,42 +1,33 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        # sliding window using three pointers
-        # use three pointers left_far, left_near, right 
-        # initialize the lefts to zer0
-        # initialize a count dic
-        # insert the first element of nums and 1 as its value
-        # for r 1...n-1
-        # add the value of nums[r] into our count
-        # in a while loop condition: check if we have excess of nums[near]
-        # if so increment near by 1
-        # in another while loop condition: check if len(count) > k:
-        # if so near += 1
-        # far = near
-        # outside of the loops
-        # res += far - near
-    
-        res = 0
-        left_far = left_near = 0
-        count = defaultdict(int)
         
+        def countSubarrays(nums, k):
+            if k == 0:
+                return 0
 
-        for r in range(len(nums)):
-            count[nums[r]] += 1
+            l, r = 0, 0
+            chars = {}
+            count = 0
 
-            while len(count) > k:
-                count[nums[left_near]] -= 1
-                if count[nums[left_near]] <= 0:
-                    del count[nums[left_near]]
-                left_near += 1
-                left_far = left_near
-            while count[nums[left_near]] > 1:
-                count[nums[left_near]] -= 1
-                left_near += 1
+            while r < len(nums):
+                if nums[r] not in chars:
+                    chars[nums[r]] = 1
+                else:
+                    chars[nums[r]] += 1
+                while len(chars) > k:
+                    chars[nums[l]] -= 1
+                    if chars[nums[l]] == 0:
+                        del chars[nums[l]]
+                    l += 1
+                count += (r - l + 1)
+                r += 1
+            return count
+        return countSubarrays(nums, k) - countSubarrays(nums, k - 1)
 
-            if len(count) == k:
-                res += left_near - left_far + 1
-        return res
-    
+      
 
+                
 
-     
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
