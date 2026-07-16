4d4a27b77e0a653657225b64634e968f01643029
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-16 09:28:10 +0300

    Add solution for Find All Anagrams in a String

--- leetcode/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ leetcode/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,27 +1,27 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        l, r= 0, 0
+        if len(p) > len(s): return []
         chars_s = {}
         chars_p = {}
         res = []
-        for char in p:
-            chars_p[char] = chars_p.get(char, 0) + 1
-     
-        
+
+        for i in range(len(p)):
+            chars_p[p[i]] = chars_p.get(p[i], 0) + 1
+            chars_s[s[i]] = chars_s.get(s[i], 0) + 1  
+        if chars_p == chars_s:
+            res.append(0)
+        l, r= 0, len(p)
         while r < len(s):
-            if s[r] not in chars_p:
-                r += 1
-                l = r 
-                chars_s = {}
-                continue
             chars_s[s[r]] = chars_s.get(s[r], 0) + 1
-            while chars_s[s[r]] > chars_p[s[r]]:
-                chars_s[s[l]] -= 1
-                l += 1
+            chars_s[s[l]] -= 1
+            if chars_s[s[l]] == 0:
+                del chars_s[s[l]]
+            l += 1
             if chars_s == chars_p:
                 res.append(l)
             r += 1
         return res
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
