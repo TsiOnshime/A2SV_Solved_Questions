cd859adc7822901a7fb42b9fd3dc08718e646ec4
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-17 10:59:30 +0300

    Time: 77 ms (5.23%), Space: 31 MB (37.66%) - LeetHub - Time: O(n), Space: O(1)

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,22 +1,17 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # change all negatives, zeros, and numbers greater than n to 1
-        containsOnes = False
-        for i in range(len(nums)):
-            if nums[i] == 1:
-                containsOnes = True
-            elif nums[i] <= 0 or nums[i] > len(nums):
-                nums[i] = 1
-        if not containsOnes: return 1
+        i = 0
+        while i < len(nums):
+            index = nums[i] - 1
+            if nums[i] < 1 or nums[i] > len(nums) or nums[index] == nums[i]:
+                i += 1
+            elif 1 <= nums[i] <= len(nums):
+                index = nums[i] - 1
+                nums[i], nums[index] = nums[index], nums[i]
 
-        # change the index where an element belongs to negative
+
         for i in range(len(nums)):
-            index = abs(nums[i]) - 1
-            if nums[index] > 0: nums[index] = -nums[index]
-        
-        # search for the first index which is positive
-        for i in range(len(nums)):
-            if nums[i] > 0:
+            if nums[i] != i + 1:
                 return i + 1
         return len(nums) + 1
 
