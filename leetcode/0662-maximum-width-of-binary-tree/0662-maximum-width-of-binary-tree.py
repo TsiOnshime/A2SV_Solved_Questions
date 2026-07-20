9b78bc71f94a26b175de954f44716a4d3403c938
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-20 17:57:33 +0300

    Add solution for Maximum Width of Binary Tree

--- leetcode/0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ leetcode/0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -6,37 +6,26 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # level: [verticals] [0]
         max_width = 1
-        queue = deque()
-        queue.append([root, 0])
-        r = l = 0
-        offset = 0
+        hashmap = defaultdict(list)
 
-        while queue:
-            Found = False
-            n = len(queue)
-            for _ in range(n):
-                node, idx = queue.popleft()
-                if _ == 0:
-                    l = idx
-                if _ == n - 1:
-                    r = idx
-                if not Found:
-                    offset = idx
-                    Found = True
-                if node.left:
-                    i = (idx - offset) * 2 + 1
-                    queue.append([node.left, i])
-                if node.right:
-                    i = (idx - offset) * 2 + 2
-                    queue.append([node.right, i])
-            max_width = max(max_width, r - l + 1)
+        def maxWidth(root, level, index):
+            nonlocal hashmap
+            if not root:
+                return 
+            hashmap[level].append(index)
+            maxWidth(root.left, level + 1, 2 * index + 1)
+            maxWidth(root.right, level + 1, 2 * index + 2)
+        maxWidth(root, 0, 0)
+        for key, val in hashmap.items():
+            first = val[0]
+            second = val[-1]
+            max_width = max(max_width, second - first + 1)
         
         return max_width
-                
-
-
+            
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
