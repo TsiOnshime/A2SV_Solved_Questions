097ba959a6ed2bf90c427da81b49b84ad799de21
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-20 12:16:17 +0300

    Add solution for Symmetric Tree

--- leetcode/0101-symmetric-tree/0101-symmetric-tree.py
+++ leetcode/0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,49 +6,17 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        if (root.left and not root.right) or (not root.left and root.right):
-            return False
-        if not root.left and not root.right:
-            return True
+        
 
-        queue1 = deque()
-        queue2 = deque()
-# q1 = [3, 4] n1 = 2
-# q2 = [3, 4] n2 = 2
-        queue1.append(root.left)
-        queue2.append(root.right)
-
-        temp1 = [root.left.val]
-        temp2 = [root.right.val]
-
-        while queue1 and queue2:
-            if temp1 != temp2:
+        def symmetric(root1, root2):
+            if not root1 or not root2:
+                return root1 == root2
+            if root1.val != root2.val:
                 return False
-            temp1 = []
-            temp2 = []
-            n = len(queue1)
-            for i in range(n):
-                node1 = queue1.popleft()
-                node2 = queue2.popleft()
-                if node1:
-                    for neigh in [node1.left, node1.right]:
-                        if not neigh:
-                            temp1.append(None)
-                        else:
-                            temp1.append(neigh.val)
-                        queue1.append(neigh)
-                if node2:
-                    for neigh in [node2.right, node2.left]:
-                        if not neigh:
-                            temp2.append(None)
-                        else:
-                            temp2.append(neigh.val)
-                        queue2.append(neigh)
-        return True
-                
-                
+            
+            return symmetric(root1.left, root2.right) and symmetric(root1.right, root2.left)
+        
+        return symmetric(root.left, root.right)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
