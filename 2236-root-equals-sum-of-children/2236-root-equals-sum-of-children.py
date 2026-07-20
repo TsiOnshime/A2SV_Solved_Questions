ba38d4ee63c0e55eb97ab2f223b8402105b88264
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-20 18:09:00 +0300

    Time: 0 ms (100%), Space: 19.2 MB (43.99%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        return root.val == root.left.val + root.right.val
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
