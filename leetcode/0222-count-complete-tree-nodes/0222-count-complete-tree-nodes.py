678f3b2f94eee22ea8309cc7b1761ad14165236d
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-21 19:14:20 +0300

    Add solution for Count Complete Tree Nodes

--- leetcode/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ leetcode/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -6,18 +6,36 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        count = 0
-        if not root:
-            return 0
-        queue = deque([root])
-        while queue:
-            node = queue.popleft()
-            count += 1
+        def FindLeftHeight(root):
+            height = 1
+            curr = root
+            while curr:
+                curr = curr.left
+                height += 1
+            return height
+        
+        def FindRightHeight(root):
+            height = 0
+            curr = root
+            while curr:
+                curr = curr.right
+                height += 1
+            return height
 
-            for neigh in [node.left, node.right]:
-                if neigh:
-                    queue.append(neigh)
-        return count
+        def count_nodes(root):
+            if not root: 
+                return 0
+            left_height = FindLeftHeight(root)
+            right_height = FindRightHeight(root)
+            print(left_height, right_height)
+            if left_height == right_height:
+                return (2**left_height) - 1
+            else:
+                return 1 + count_nodes(root.left) + count_nodes(root.right)
+        
+        return count_nodes(root)
+
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
