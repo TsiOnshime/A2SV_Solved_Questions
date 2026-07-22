0a74134619caaa221ee881816d491dfb8cb68c0f
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-22 10:22:23 +0300

    Add solution for Serialize and Deserialize Binary Tree

--- leetcode/0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ leetcode/0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -1,46 +1,70 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Codec:
-    
-    # Encodes a tree to a single string.
-    def serialize(self, root: Optional[TreeNode]) -> str:
-        self.string = []
-        def dfs(root):
-            if not root:
-                self.string.append("N")
-                self.string.append(":")
-                return
-            self.string.append(str(root.val))
-            self.string.append(":")
-            dfs(root.left)
-            dfs(root.right)
-        dfs(root)
-        return "".join(self.string)
-        
-        
-    # Decodes your encoded data to tree.
-    def deserialize(self, data: str) -> Optional[TreeNode]:
-        val = data.split(":")
-        self.i = 0
-        def dfs():
-            if val[self.i] == "N" or self.i > len(data):
-                self.i += 1
-                return 
-            
-            root = TreeNode(val[self.i])
-            self.i += 1
 
-            root.left = dfs()
-            root.right = dfs()
-
-            return root
-        return dfs()
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        none_count = 0
+        queue = deque([root])
+        res = []
+        while queue:
+            node = queue.popleft()
+            if node:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                res.append("#")
             
 
+        print(res)
+        return ",".join(res)
 
 
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        data = data.split(",")
+        if data[0] == "#":
+            return 
+        idx = 1
+        root = TreeNode(int(data[0]))
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+            left = TreeNode(int(data[idx])) if data[idx] != "#" else None
+            right = TreeNode(int(data[idx + 1])) if data[idx + 1] != "#" else None
+            if left:
+                node.left = left
+                queue.append(left)
+            if right:
+                node.right = right
+                queue.append(right)
+            idx += 2
+        return root
+            
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
