1a69751d1f8cfcbba04526f10dd318972b0d2f0f
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-25 07:08:33 +0300

    Add solution for Two Sum IV - Input is a BST

--- leetcode/0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ leetcode/0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -4,20 +4,67 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class BinarySearchTreeIterator:
+    def __init__(self, root: Optional[TreeNode], reverse):
+        self.root = root
+        self.stack = [self.root]
+        self.reverse = reverse
+
+        if not self.reverse:
+            curr = self.root.left
+            self.pushAll(curr, self.reverse)
+
+        else:
+            curr = self.root.right
+            self.pushAll(curr, self.reverse)
+
+
+            
+    def next(self):
+        if not self.stack:
+            return
+        curr = self.stack.pop()
+        elem = curr.val
+        if not self.reverse:
+            if curr.right:
+                curr = curr.right
+                self.pushAll(curr, self.reverse)
+        else:
+            if curr.left:
+                curr = curr.left
+                self.pushAll(curr, self.reverse)
+            
+        return elem
+
+    def pushAll(self, curr, reverse):
+        while curr:
+            self.stack.append(curr)
+            curr = curr.left if not reverse else curr.right
+    
+    def hasNext(self):
+        return len(self.stack) > 0
+                
+
+
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        elem = set()
-
-        def findK(root):
-            if not root:
-                return False
-            complement = k - root.val
-            if complement in elem:
+        l = BinarySearchTreeIterator(root, False)
+        r = BinarySearchTreeIterator(root, True)
+        
+        i = l.next()
+        j = r.next()
+        while i < j:
+            if i + j == k:
                 return True
-            elem.add(root.val)
-            return findK(root.right) or findK(root.left)
+            elif i + j < k:
+                i = l.next()
+            else:
+                j = r.next()
 
-        return findK(root)
+        return False
+
+
+
 
 
 
