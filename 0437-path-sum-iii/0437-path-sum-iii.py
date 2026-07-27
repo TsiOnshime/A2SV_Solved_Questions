6fde47b1219c0c5434722f03acc4dc3d2f33ca23
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-27 08:02:40 +0300

    Time: 3 ms (89.36%), Space: 20.8 MB (16.82%) - LeetHub - Time: O(N), Space: O(N)

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,30 +6,28 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        
         self.count = 0
-        pSums = defaultdict(int)
-        pSums[0] = 1
-
-        def path(curr, prevSum):
-
-            if not curr:
-                return 
+        sum_count = defaultdict(int)
+        sum_count[0] = 1
+        def countPathSum(root, currSum):
+            if not root:
+                return
+            currSum += root.val
+            complement = currSum - targetSum
+            if sum_count[complement] != 0:
+                self.count += sum_count[complement]
             
-            currSum = curr.val + prevSum
+            sum_count[currSum] += 1
 
-            x = currSum - targetSum
-
-            if x in pSums:
-                self.count += pSums[x]
-            pSums[currSum] += 1
-
-            path(curr.left, currSum)
-            path(curr.right, currSum)
-            pSums[currSum] -= 1
-
-
-        path(root, 0)
+            countPathSum(root.left, currSum)
+            countPathSum(root.right, currSum)
+            
+            sum_count[currSum] -= 1
+        
+        countPathSum(root, 0)
         return self.count
 
 
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
