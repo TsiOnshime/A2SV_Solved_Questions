6c9d9128c375fb80aa60251a3d3660be4dbf4ba4
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-27 11:08:33 +0300

    Add solution for Lowest Common Ancestor of a Binary Tree

--- leetcode/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ leetcode/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,45 +7,29 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.commonAncestor = None
-
         def lowestCommon(root):
             if not root:
-                # found, ancestor
-                return False
-            
+                return 
             left = lowestCommon(root.left)
             right = lowestCommon(root.right)
 
-            if not left and not right:
-                if root.val == q.val or root.val == p.val:
-                    return True
-                return False
-
-            if left and right:
-                if self.commonAncestor is None:
-                    self.commonAncestor = root
-                return True
-            
-            if left:
+            if left == None and right == None:
                 if root.val == p.val or root.val == q.val:
-                    if self.commonAncestor is None:
-                        self.commonAncestor = root
-                return True
-            if right:
+                    return root
+                return 
+            elif left != None and right != None:
+                return root
+            elif left != None:
                 if root.val == p.val or root.val == q.val:
-                    if self.commonAncestor is None:
-                        self.commonAncestor = root
-                return True
-            return False
+                    return root
+                return left
+            elif right != None:
+                if root.val == p.val or root.val == q.val:
+                    return root
+                return right
 
-        lowestCommon(root)
-        return self.commonAncestor
-
-                
-
-
-            
+        return lowestCommon(root) 
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
