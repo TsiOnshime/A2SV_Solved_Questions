832f82ee3f68e7f4f1fce843c7ef01a0291c284f
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-28 06:25:02 +0300

    Add solution for Validate Stack Sequences

--- leetcode/0946-validate-stack-sequences/0946-validate-stack-sequences.py
+++ leetcode/0946-validate-stack-sequences/0946-validate-stack-sequences.py
@@ -1,20 +1,20 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
         i = 0
         j = 0
-        stack = []
         while i < len(pushed) and j < len(popped):
+            stack.append(pushed[i])
             while stack and stack[-1] == popped[j]:
                 stack.pop()
                 j += 1
-            stack.append(pushed[i])
             i += 1
+
         while j < len(popped) and stack[-1] == popped[j]:
             stack.pop()
             j += 1
-        
-        return len(stack) == 0
 
+        return len(stack) == 0
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
