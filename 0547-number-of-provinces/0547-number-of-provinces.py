3f7933ba1a1dd7e48c65c8a7ba7631a8e8a378cd
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-28 17:35:02 +0300

    Time: 3 ms (86.56%), Space: 21.7 MB (9.48%) - LeetHub - Time: O(n^2), Space: O(n)

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -5,16 +5,16 @@ class Solution:
         count = 0
 
         def dfs(city1):
-            if city1 in visited:
-                return 0
             visited.add(city1)
 
             for city2 in range(n):
                 if isConnected[city1][city2] == 1 and city2 not in visited:
                     dfs(city2)
-            return 1
+          
         for city in range(n):
-            count += dfs(city)
+            if city not in visited:
+                dfs(city)
+                count += 1
         
         return count
 
