58befd033818bb1a390560e77281839ef791db19
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-28 17:47:17 +0300

    Add solution for Number of Islands

--- leetcode/0200-number-of-islands/0200-number-of-islands.py
+++ leetcode/0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols = len(grid), len(grid[0])
+        number_of_islands = 0
         visited = set()
-        count = 0
-        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        rows, cols = len(grid), len(grid[0])
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 
+        def is_valid(r, c):
+            if 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1" and (r, c) not in visited:
+                return True
+            return False
+        def dfs(r, c):
+            visited.add((r, c))
+
+            for dr, dc in directions:
+                nr, nc = dr + r, dc + c
+                if is_valid(nr, nc):
+                    dfs(nr, nc)
+            
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    self.explore(r, c, visited, rows, cols, directions, grid)
-                    count += 1
-        return count
-
-    def is_valid(self, r, c, visited, rows, cols, grid):
-        if 0 <= r < rows and 0 <= c < cols and (r, c) not in visited and grid[r][c] == "1":
-            return True
-        return False
-    
-    def explore(self, r, c, visited, rows, cols, directions, grid):
-        if not self.is_valid(r, c, visited, rows, cols, grid):
-            return 
-        visited.add((r, c))
-        for dr, dc in directions:
-            self.explore(r + dr, c + dc, visited, rows, cols, directions, grid)
-            
-
-
+                if (r, c) not in visited and grid[r][c] == "1":
+                    dfs(r, c)
+                    number_of_islands += 1
+        
+        return number_of_islands
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
