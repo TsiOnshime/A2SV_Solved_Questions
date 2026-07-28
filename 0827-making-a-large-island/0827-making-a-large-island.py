60ee2acf88fd6491106e9e0a98e5a19df42b5934
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-28 17:08:53 +0300

    Time: 767 ms (80.17%), Space: 36.4 MB (65.05%) - LeetHub - Time: O(rows * cols), Space: O(rows * cols)

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,34 +1,31 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-        visited = set()
         rows, cols = len(grid), len(grid[0])
         marker_area = {}
         marker = 2
         max_area = 0
 
-        
-        def is_valid(r, c):
-            if 0 <= r < rows and 0 <= c < cols and grid[r][c] == 1:
-                return True
-            return False
-
         def dfs(r, c, marker):
-            nonlocal size
-            size += 1
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != 1:
+                return 0
+            
+
             grid[r][c] = marker
-            for dr, dc in directions:
-                nr, nc = dr + r, dc + c
-                if is_valid(nr, nc):
-                    dfs(nr, nc, marker)
+            left = dfs(r, c - 1, marker)
+            right = dfs(r, c + 1, marker)
+            up = dfs(r - 1, c, marker)
+            down = dfs(r + 1, c, marker)
+
+            return 1 + left + right + up + down
        
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    size = 0
-                    dfs(r, c, marker)
+                    size = dfs(r, c, marker)
                     marker_area[marker] = size
+                    max_area = max(max_area, size)
                     marker += 1
         
         for r in range(rows):
@@ -36,23 +33,13 @@ class Solution:
                 markers_connected = set()
                 area = 1
                 if grid[r][c] == 0:
-                    if r - 1 >= 0 and grid[r-1][c] != 0 and grid[r-1][c] not in markers_connected:
-                        area += marker_area[grid[r-1][c]]
-                        markers_connected.add(grid[r-1][c])
-                    if r + 1 < rows and grid[r + 1][c] != 0 and grid[r + 1][c] not in markers_connected:
-                        area += marker_area[grid[r + 1][c]]
-                        markers_connected.add(grid[r + 1][c])
-                    if c - 1 >= 0 and grid[r][c - 1] != 0 and grid[r][c - 1] not in markers_connected:
-                        area += marker_area[grid[r][c - 1]]
-                        markers_connected.add(grid[r][c - 1])
-                    if c + 1 < cols and grid[r][c + 1] != 0 and grid[r][c + 1] not in markers_connected:
-                        area += marker_area[grid[r][c + 1]]
-                        markers_connected.add(grid[r][c + 1])
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] != 0 and grid[nr][nc] not in markers_connected:
+                            area += marker_area[grid[nr][nc]]
+                            markers_connected.add(grid[nr][nc])
                     
                     max_area = max(max_area, area)
-        for mark, area in marker_area.items():
-            max_area = max(area,  max_area)
-
         return max_area
 
 
