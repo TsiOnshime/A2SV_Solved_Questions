3768ab2dfa32dbe73a53e09160aa7c0ee4aa4506
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-30 06:07:32 +0300

    Time: 44 ms (99.34%), Space: 20.7 MB (99.66%) - LeetHub - Time: O(rows * cols), Space: O(1)

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,39 +1,29 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        queue = deque()
         rows, cols = len(mat), len(mat[0])
-        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-        distance = [[0] * cols for _ in range(rows)]
-        visited = [[0] * cols for _ in range(rows)]
 
-        def is_valid(r, c):
-            if 0 <= r < rows and 0 <= c < cols and visited[r][c] != 1:
-                return True
-            return False
+        for r in range(rows - 1, -1, -1):
+            for c in range(cols - 1, -1, -1):
+                if mat[r][c] == 0:
+                    continue
+                else:
+                    right = mat[r][c + 1] if c + 1 < cols else float('inf')
+                    down = mat[r + 1][c] if r + 1 < rows else float('inf')
 
+                    mat[r][c] = min(right + 1, down + 1)
+
+        
         for r in range(rows):
             for c in range(cols):
                 if mat[r][c] == 0:
-                    queue.append([(r, c), 0])
-                    visited[r][c] = 1
-        
+                    continue
+                else:
+                    left = mat[r][c - 1] if c - 1 >= 0 else float('inf')
+                    up = mat[r - 1][c] if r - 1 >= 0 else float('inf')
 
-        while queue:
-            node, dist = queue.popleft()
-            r, c = node
-            for dr, dc in directions:
-                nr, nc = dr + r, dc + c
-                if is_valid(nr, nc):
-                    distance[nr][nc] = dist + 1
-                    visited[nr][nc] = 1
-                    queue.append([(nr, nc), dist + 1])
+                    mat[r][c] = min(left + 1, up + 1, mat[r][c])
 
-        return distance
-
-
-
-
-
+        return mat
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
