8b4f428f6979e01cbbe0f37e954ff814f600fc4b
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-30 18:53:08 +0300

    Add solution for Is Graph Bipartite?

--- leetcode/0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ leetcode/0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -3,27 +3,25 @@ class Solution:
         n = len(graph)
         group = [-1] * n
 
-        def bfs(node):
-            queue = deque()
-            group[node] = 0
-            queue.append(node)
-            while queue:
-                node = queue.popleft()
-                for neigh in graph[node]:
-                    if group[neigh] == -1:
-                        group[neigh] = not group[node]
-                        queue.append(neigh)
-                    elif group[neigh] == group[node]:
-                        return False
+        def dfs(node):
 
+            for neigh in graph[node]:
+                if group[neigh] == -1:
+                    group[neigh] = not group[node]
+                    if not dfs(neigh):
+                        return False
+                elif group[neigh] == group[node]:
+                    return False
             return True
 
         for i in range(n):
             if group[i] == -1:
-                if not bfs(i):
+                group[i] = 0
+                if not dfs(i):
                     return False
 
         return True
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
