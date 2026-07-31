7008ed1d07e9b2a023ad0b1a754f91229c9870a6
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-31 09:01:49 +0300

    Time: 52 ms (83.7%), Space: 23 MB (16.56%) - LeetHub - Time: O(N * n^2), Space: O(N * n^2)

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,37 +2,38 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
+        
+        # all words have the same length
 
-        wordList.append(beginWord)
+        # map pattern with list of words that satisfy that pattern
+        adj_list = defaultdict(list)
         n = len(beginWord)
-        adj = defaultdict(set)
+        wordList.append(beginWord)
+
+        for i in range(len(wordList)):
+            for j in range(n):
+                pat = wordList[i][:j] + "*" + wordList[i][j + 1:]
+                adj_list[pat].append(wordList[i])
+
+        
         queue = deque()
         visited = set()
 
-        for i in range(len(wordList)):
-            word = wordList[i]
-            for j in range(n):
-                pat = word[:j] + "*" + word[j + 1:]
-                adj[pat].add(word)
-
         queue.append([beginWord, 1])
         visited.add(beginWord)
+
         while queue:
-            node, dist = queue.popleft()
-            if node == endWord:
-                return dist
-            for j in range(n):
-                pat = node[:j] + "*" + node[j + 1:]
-                for neigh in adj[pat]:
+            word, dist = queue.popleft()
+            if word == endWord:
+                return dist 
+            for i in range(n):
+                pat = word[:i] + "*" + word[i + 1:]
+                for neigh in adj_list[pat]:
                     if neigh not in visited:
                         queue.append([neigh, dist + 1])
                         visited.add(neigh)
-
         return 0
-
-
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
