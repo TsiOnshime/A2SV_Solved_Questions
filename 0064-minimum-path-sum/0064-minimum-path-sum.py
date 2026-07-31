6635234df73468919f0a728d35834d63b9e8a894
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-07-31 07:25:17 +0300

    Time: 11 ms (54.81%), Space: 21.4 MB (67.18%) - LeetHub - Time: O(rows * cols), Space: O(1)

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,29 +1,19 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
+        rows, cols = len(grid), len(grid[0])
 
-        dp = [0] * n
-    #l = 8 , u = 6
-        # [6, 8, 7]
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    dp[i] = grid[i][j]
-                else:
-                    left = float('inf')
-                    up = float('inf')
+        for c in range(cols-2,-1,-1):
+            grid[rows-1][c] += grid[rows-1][c + 1]
+        
+        for r in range(rows - 2, -1, -1):
+            for c in range(cols - 1, -1, -1):
+                down = grid[r + 1][c] if r + 1 < rows else float('inf')
+                right = grid[r][c + 1] if c + 1 < cols else float('inf')
+                grid[r][c] = grid[r][c] + min(down, right)
 
-                    if j - 1 >= 0:
-                        left = dp[j - 1]
-                    if i - 1 >= 0:
-                        up = dp[j]
-                    val = min(left, up)
-                    dp[j] = val + grid[i][j]
-
-        return dp[-1]
-
+        return grid[0][0]
 
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
