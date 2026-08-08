086a24e32428d2dabc49bca8ec388baac7e3e762
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-08 07:11:39 +0300

    Added solution - Time: O(E log E), Space: O(V + E)

--- 700343-minimum-spanning-tree-GFG/700343-minimum-spanning-tree.py
+++ 700343-minimum-spanning-tree-GFG/700343-minimum-spanning-tree.py
@@ -1,32 +1,65 @@
 import heapq
 from collections import defaultdict
+
+class UnionFind:
+    def __init__(self, V):
+        self.rank = [0] * V
+        self.parent = [i for i in range(V)]
+    
+    def find(self, node):
+        if node != self.parent[node]:
+            self.parent[node] = self.find(self.parent[node])
+        
+        return self.parent[node]
+    
+    def union(self, node1, node2):
+        parent1, parent2 = self.find(node1), self.find(node2)
+        
+        if parent1 == parent2:
+            return False
+        
+        if self.rank[parent1] > self.rank[parent2]:
+            self.parent[parent2] = parent1
+        elif self.rank[parent2] > self.rank[parent1]:
+            self.parent[parent1] = parent2
+        else:
+            self.parent[parent1] = parent2
+            self.rank[parent2] += 1
+            
+        return True
+        
+        
 class Solution:
     def spanningTree(self, V: int, edges: list[list[int]]) -> int:
-        # code here
+       
+        edges.sort(key=lambda x: x[2])
+        
+        uf = UnionFind(V)
+        
         _sum = 0
-        adj_list = defaultdict(list)
-        
+        edges_used = 0
         for u, v, wt in edges:
-            adj_list[u].append([v, wt])
-            adj_list[v].append([u, wt])
-        
-        # wt, node
-        min_heap = []
-        heapq.heappush(min_heap, [0, 0])
-        
-        visited = set()
-        
-        while min_heap:
-            wt, node = heapq.heappop(min_heap)
-            if node in visited:
-                continue
-            _sum += wt
-            visited.add(node)
-            for neigh, dist in adj_list[node]:
-                if neigh not in visited:
-                    heapq.heappush(min_heap, [dist, neigh])
-                    
+            if uf.union(u, v):
+                _sum += wt
+                edges_used += 1
+                if edges_used == V - 1:
+                    break
         return _sum
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
