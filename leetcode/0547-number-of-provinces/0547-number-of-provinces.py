cbc8465b985f2c46a47dbad560ee1d31eb5a2448
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-08 07:34:23 +0300

    Add solution for Number of Provinces

--- leetcode/0547-number-of-provinces/0547-number-of-provinces.py
+++ leetcode/0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,22 +1,46 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+    
+    def find(self, node):
+        if node != self.parent[node]:
+            self.parent[node] = self.find(self.parent[node])
+        
+        return self.parent[node]
+    
+    def union(self, node1, node2):
+        parent1, parent2 = self.find(node1), self.find(node2)
+
+        if parent1 == parent2:
+            return False
+        
+        if self.rank[parent1] < self.rank[parent2]:
+            self.parent[parent1] = parent2
+        elif self.rank[parent2] < self.rank[parent1]:
+            self.parent[parent2] = parent1
+        else:
+            self.parent[parent2] = parent1
+            self.rank[parent1] = parent2
+        
+        return True
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
-        visited = set()
-        count = 0
-
-        def dfs(city1):
-            visited.add(city1)
-
-            for city2 in range(n):
-                if isConnected[city1][city2] == 1 and city2 not in visited:
-                    dfs(city2)
-          
-        for city in range(n):
-            if city not in visited:
-                dfs(city)
-                count += 1
         
-        return count
+        n = len(isConnected)
+        provinces = n
+
+        uf = UnionFind(n)
+
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                if isConnected[i][j] == 1 and uf.union(i, j):
+                    provinces -= 1
+
+        return provinces
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
