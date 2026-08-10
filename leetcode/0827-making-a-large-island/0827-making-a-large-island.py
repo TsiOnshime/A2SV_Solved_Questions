d80ae1771ae30279133b58d7a88789b85fd79dcd
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-10 18:39:56 +0300

    Add solution for Making A Large Island

--- leetcode/0827-making-a-large-island/0827-making-a-large-island.py
+++ leetcode/0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,48 +1,60 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1] * n
+    
+    def find(self, node):
+        if node != self.parent[node]:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+    
+    def union(self, node1, node2):
+        parent1, parent2 = self.find(node1), self.find(node2)
+        if parent1 == parent2:
+            return 
+        if self.size[parent1] > self.size[parent2]:
+            self.parent[parent2] = parent1
+            self.size[parent1] += self.size[parent2]
+        else:
+            self.parent[parent1] = parent2
+            self.size[parent2] += self.size[parent1]
+
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
         rows, cols = len(grid), len(grid[0])
-        marker_area = {}
-        marker = 2
-        max_area = 0
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        max_size = 1
+        uf = UnionFind(rows * cols)
 
-        def dfs(r, c, marker):
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != 1:
-                return 0
-            
-
-            grid[r][c] = marker
-            left = dfs(r, c - 1, marker)
-            right = dfs(r, c + 1, marker)
-            up = dfs(r - 1, c, marker)
-            down = dfs(r + 1, c, marker)
-
-            return 1 + left + right + up + down
-       
+        def is_valid(r, c):
+            if 0 <= r < rows and 0 <= c < cols and grid[r][c] == 1:
+                return True
+            return False
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    size = dfs(r, c, marker)
-                    marker_area[marker] = size
-                    max_area = max(max_area, size)
-                    marker += 1
+                    node = r * cols + c
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if is_valid(nr, nc):
+                            neigh = nr * cols + nc
+                            uf.union(node, neigh)
         
         for r in range(rows):
             for c in range(cols):
-                markers_connected = set()
-                area = 1
+                visited = set()
                 if grid[r][c] == 0:
+                    size = 1
                     for dr, dc in directions:
-                        nr, nc = r + dr, c + dc
-                        if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] != 0 and grid[nr][nc] not in markers_connected:
-                            area += marker_area[grid[nr][nc]]
-                            markers_connected.add(grid[nr][nc])
-                    
-                    max_area = max(max_area, area)
-        return max_area
-
-
+                        nr, nc = dr + r, dc + c
+                        if is_valid(nr, nc):
+                            visited.add(uf.find(nr * cols + nc))
+                    for i in visited:
+                        size += uf.size[i]
+                
+                    max_size = max(size, max_size)
+        return max(max(uf.size), max_size)
 
 
 
