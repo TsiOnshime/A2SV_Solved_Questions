a0d7cb69b59c9d9a9789c0ead0ce1ea6cbbab7ce
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-11 19:23:26 +0300

    Add solution for Integer to Roman

--- leetcode/0012-integer-to-roman/0012-integer-to-roman.py
+++ leetcode/0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,34 +1,35 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        
-        Int_Rom = {
-            1000: "M",
-            900: "CM",
-            500: "D",
-            400: "CD", 
-            100: "C",
-            90: "XC", 
-            50: "L",
-            40: "XL", 
-            10: "X",
-            9: "IX",
-            5: "V",
-            4: "IV",
-            1: "I"
-
+        rom_int = {
+            "M":1000,
+            "CM":900,
+            "D":500,
+            "CD":400,
+            "C":100,
+            "XC":90,
+            "L":50,
+            "XL":40,
+            "X":10,
+            "IX":9,
+            "V":5,
+            "IV":4,
+            "I":1
         }
+        roman = []
+        while num:
+            for key, val in rom_int.items():
+                quotient = num // val
+                if quotient > 0:
+                    roman.append(key * quotient)
+                    num %= val
+                    break
+        return "".join(roman)
 
-        rom = []
-        for key, val in Int_Rom.items():
-            freq = num // key
-            if freq != 0:
-                rom.append(Int_Rom[key] * freq)
-                num %= key
 
-        return "".join(rom)
-
-            
+# 3749
+# q = 3
+# r = 
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
