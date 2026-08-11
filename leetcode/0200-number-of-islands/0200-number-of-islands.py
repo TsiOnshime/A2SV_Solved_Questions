3bc871e0022c8b71351dec448eb86e7cb6eec571
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-11 19:33:43 +0300

    Add solution for Number of Islands

--- leetcode/0200-number-of-islands/0200-number-of-islands.py
+++ leetcode/0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,34 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        number_of_islands = 0
-        visited = set()
         rows, cols = len(grid), len(grid[0])
-        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        visited = set()
+        count = 0
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
         def is_valid(r, c):
             if 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1" and (r, c) not in visited:
                 return True
             return False
-        def dfs(r, c):
+        def bfs(r, c):
+            queue = deque()
+            queue.append([r, c])
             visited.add((r, c))
+            while queue:
+                r, c = queue.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + r, c + dc
+                    if is_valid(nr, nc):
+                        queue.append([nr, nc])
+                        visited.add((nr, nc))
 
-            for dr, dc in directions:
-                nr, nc = dr + r, dc + c
-                if is_valid(nr, nc):
-                    dfs(nr, nc)
-            
+
         for r in range(rows):
             for c in range(cols):
-                if (r, c) not in visited and grid[r][c] == "1":
-                    dfs(r, c)
-                    number_of_islands += 1
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    count += 1
+                    bfs(r, c)
         
-        return number_of_islands
+        return count
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
