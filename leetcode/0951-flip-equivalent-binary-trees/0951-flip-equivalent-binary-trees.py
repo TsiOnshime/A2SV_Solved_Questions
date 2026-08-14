f5769dd1856791f1ed38efe2f73a34b86552dadc
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-14 18:27:07 +0300

    Add solution for Flip Equivalent Binary Trees

---
+++ leetcode/0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def flipCheck(root1, root2):
+            if not root1 and not root2:
+                return True
+            if not root1 or not root2 or root1.val != root2.val:
+                return False
+            firstCheck = flipCheck(root1.left, root2.left) and flipCheck(root1.right, root2.right)
+            secondCheck = flipCheck(root1.left, root2.right) and flipCheck(root1.right, root2.left)
+
+            return firstCheck or secondCheck
+        
+        return flipCheck(root1, root2)
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
