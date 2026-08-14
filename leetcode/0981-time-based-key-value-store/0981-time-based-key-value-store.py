f91491fc0d0c5eb41f8b37439d039d5019232514
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-14 16:30:34 +0300

    Add solution for Time Based Key-Value Store

--- leetcode/0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ leetcode/0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,31 +1,36 @@
 class TimeMap:
 
     def __init__(self):
-        self.timeMap = defaultdict(list)
+        self.timeStamp = defaultdict(list)
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        self.timeMap[key].append([value, timestamp])
-
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.timeMap:
-            return ""
-        
-        l = 0 
-        r = len(self.timeMap[key]) - 1
+    def binarySearch(self, l, r, key, timestamp):
+        ans = ""
         while l <= r:
             mid = l + (r - l)//2
-            if self.timeMap[key][mid][1] == timestamp:
-                return self.timeMap[key][mid][0]
-            elif self.timeMap[key][mid][1] > timestamp:
+            if self.timeStamp[key][mid][1] > timestamp:
                 r = mid - 1
             else:
+                ans = self.timeStamp[key][mid][0]
                 l = mid + 1
-        if l - 1 >= 0:
-            return self.timeMap[key][l -1][0]
-        return ""
+        return ans
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.timeStamp[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if len(self.timeStamp[key]) == 0:
+            return ""
+        l = 0
+        r = len(self.timeStamp[key]) - 1
+        return self.binarySearch(l, r, key, timestamp)
+        
 
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
