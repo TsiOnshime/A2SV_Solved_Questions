f96e34abe79ecf3f287c97e35fc9acaa6acff22f
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-15 20:40:29 +0300

    Add solution for Minimum Remove to Make Valid Parentheses

--- leetcode/1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ leetcode/1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,20 +1,29 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
+        to_be_removed = set()
         stack = []
-        arr = list(s)
-        print(arr)
-    
+
         for i in range(len(s)):
-            
-            if s[i] == ")":
+            if s[i] == "(":
+                stack.append(i)
+            elif s[i] == ")":
                 if stack:
                     stack.pop()
                 else:
-                    arr[i] = ""
-            if s[i] == "(":
-                stack.append(i)
-        print(stack)
-        for i in stack:
-            arr[i] = ""
+                    to_be_removed.add(i)
+        while stack:
+            to_be_removed.add(stack.pop())
+
+        res = []
+        for i in range(len(s)):
+            if i in to_be_removed:
+                continue
+            res.append(s[i])
         
-        return "".join(arr)
+        return "".join(res)
+                
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
