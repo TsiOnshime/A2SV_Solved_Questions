1fbffbcfa3ac07ca55194edc31654206559d78d3
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-17 18:27:12 +0300

    Add solution for Search Insert Position

--- leetcode/0035-search-insert-position/0035-search-insert-position.py
+++ leetcode/0035-search-insert-position/0035-search-insert-position.py
@@ -1,13 +1,17 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        
-        l = 0
-        u = len(nums) - 1
-        while l <= u:
-            mid = l + (u - l)//2
+        ans = len(nums)
+        l, r = 0, len(nums) - 1
 
+        while l <= r:
+            mid = l + (r - l)//2
             if nums[mid] >= target:
-                u = mid - 1
+                ans = mid
+                r = mid - 1
             else:
                 l = mid + 1
-        return l
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
