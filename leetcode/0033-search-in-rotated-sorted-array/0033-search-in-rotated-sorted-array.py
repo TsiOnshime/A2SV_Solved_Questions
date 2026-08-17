ecb76c698278c8fc0e82e9befa6280c03b368de3
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-17 20:06:29 +0300

    Add solution for Search in Rotated Sorted Array

--- leetcode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ leetcode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,38 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-# find the pivot where nums[p - 1] < nums[p] < nums[p + 1]
-# if target > nums[l] search (l:p)
-# if target < nums[r] search(p:r)
+        l,r = 0, len(nums) - 1
 
-
-# calculate mid
-
-# check if it is in the left sorted array
-#  how? if nums[l] < nums[mid]: we are in the left sorted array
-#        check if our target belongs in that sub array how? if nums[l] < target: r = mid - 1
-#         else: l = mid + 1
-
-# else: we are on the right sorted array:
-#       if nums[mid] < target < nums[r]: l = mid + 1
-#       else: r = mid - 1
-        l, r = 0, len(nums) - 1
         while l <= r:
-            mid = l + (r - l) // 2
+            mid = l + (r - l)//2
             if nums[mid] == target:
                 return mid
-            elif nums[l] <= nums[mid]:
-                if nums[l] <= target < nums[mid]:
+            # left sorted
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target and nums[mid] >= target:
                     r = mid - 1
                 else:
                     l = mid + 1
+            # right sorted
             else:
-                if nums[mid] < target <= nums[r]:
-                    l = mid + 1
+                if nums[mid] <= target and nums[r] >= target:
+                    l = mid +  1
                 else:
-                    r =mid - 1
-
+                    r = mid - 1
         return -1
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
