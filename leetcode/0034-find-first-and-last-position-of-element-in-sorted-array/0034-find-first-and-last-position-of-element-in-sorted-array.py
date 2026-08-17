ec7fda04e3f997e759f2d620c3c49cdbf0fb7f3c
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-17 19:15:04 +0300

    Add solution for Find First and Last Position of Element in Sorted Array

--- leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,32 +1,33 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l, r = 0, len(nums) - 1
-        first = -1
-        last = -1
-        while l <= r:
-            mid = l + (r - l)//2
-
-            if nums[mid] == target:
-                first = mid
-                r = mid - 1
-            elif nums[mid] < target:
-                l = mid + 1
-            else:
-                r = mid - 1
-        if first == -1:
-            return [-1, -1]
-        l, r = first, len(nums) - 1
-        while l <= r:
-            mid = l + (r - l)//2
-            if nums[mid] == target:
-                last = mid
-                l = mid + 1
-            elif nums[mid] < target:
-                l = mid + 1
-            else:
-                r = mid - 1
-
-        return [first, last]
+        def first_occurence():
+            ans = -1
+            low, high = 0, len(nums) - 1
+            while low <= high:
+                mid = low + (high - low) // 2
+                if nums[mid] > target:
+                    high = mid - 1
+                elif nums[mid] == target:
+                    ans = mid
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return ans
+        def last_occurrence():
+            ans = -1
+            low, high = 0, len(nums) - 1
+            while low <= high:
+                mid = low + (high - low) // 2
+                if nums[mid] > target:
+                    high = mid - 1
+                elif nums[mid] == target:
+                    ans = mid
+                    low = mid + 1
+                else:
+                    low = mid + 1
+            return ans
+        
+        return [first_occurence(), last_occurrence()]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
