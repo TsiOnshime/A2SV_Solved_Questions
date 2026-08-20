3d2a8c06fa050d95838c15337483a97844a5d4d3
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-08-20 14:47:12 +0300

    Time: 248 ms (5.08%), Space: 20.8 MB (12.12%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,27 +1,29 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-     # to calculate ceil(pile/k) we can do (piles + k - 1) // k
-
-        def possible(mid):
+        l = 1
+        r = sum(piles)
+        ans = sum(piles)
+# [3,6,7,11], h = 8
+# l = 1,   r = 27
+# mid = 14
+        def is_valid(k):
             time = 0
-
             for pile in piles:
-                time += math.ceil(pile / mid)
+                time += math.ceil(pile / k)
+
             return time <= h
 
-
-        l = 1 
-        r = max(piles)
-
-        ans = 0
-
+                        
         while l <= r:
-
             mid = l + (r - l)//2
-            if possible(mid):
+            if is_valid(mid):
                 ans = mid
                 r = mid - 1
             else:
                 l = mid + 1
-
         return ans
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
