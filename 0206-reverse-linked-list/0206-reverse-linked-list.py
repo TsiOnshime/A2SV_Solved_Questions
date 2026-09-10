fda6748b02dfcb8a9f4a770a82e56a2e1a01503f
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-09-10 14:19:07 +0300

    Time: 0 ms (100%), Space: 20.5 MB (66.22%) - LeetHub - Time: O(n), Space: O(1)

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,19 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
+        if not head or not head.next:
             return head
-        def reverse(curr):
-            if not curr.next:
-                return curr
+        dummy = ListNode()
 
-            new_head = reverse(curr.next)
-            curr.next.next = curr
-            curr.next = None
+        prev = None
+        curr = head
+        nxt = head.next
 
-            return new_head
+        while curr:
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            if nxt:
+                nxt = nxt.next
+        
+        return prev
 
-        return reverse(head)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
