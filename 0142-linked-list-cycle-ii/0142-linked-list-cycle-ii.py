fffc413f74624bec844dfcaf7f706c51153a219a
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-09-15 19:27:45 +0300

    Time: 55 ms (42.41%), Space: 22.3 MB (75.57%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+        
+        fast = head
+
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+        
+        return slow
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
