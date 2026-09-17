4ea4762c0db955eba274a2d2f9a16e81f2223e79
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-09-17 07:50:06 +0300

    Time: 50 ms (42.16%), Space: 20.1 MB (57.88%) - LeetHub - Time: O(n), Space: O(1)

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,22 +9,35 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        old_to_new = {None:None}
-
+        
         curr = head
         while curr:
-            old_to_new[curr] = Node(curr.val)
-            curr = curr.next
+            temp = Node(curr.val)
+            nxt = curr.next
+            curr.next = temp
+            temp.next = nxt
+            curr = curr.next.next
+        
         curr = head
-       # {[7, 13, 1]: [7: 13, 1], [13, 11, 7]: [13, 11, 7], [11, 10, 1]: [11, 10, 1], [10, 1, 11]: [10, 1, 11], [1, None, 7]: [1, None, 7]}
-
-       
         while curr:
-            old_to_new[curr].next = old_to_new[curr.next]
-            old_to_new[curr].random = old_to_new[curr.random]
-            curr = curr.next
-        return old_to_new[head]
+            temp = curr.next
+            temp.random = curr.random.next if curr.random else None
+
+            curr = curr.next.next
+        curr = head
+        dummy = Node(0)
+        res = dummy
+        while curr:
+            temp = curr.next.next if curr.next else None
+            res.next = curr.next
+            curr.next = temp
+            curr = temp
+            res = res.next
+        return dummy.next
+        
+
+            
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
