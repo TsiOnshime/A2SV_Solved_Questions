14957c80753bee0acff52b408ea9beb1050614a6
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2026-09-20 20:31:29 +0300

    Time: 63 ms (30.13%), Space: 20 MB (6.77%) - LeetHub - Time: O(N log N + M log N), Space: O(N)

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,20 +1,38 @@
 class Solution:
-    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        set1 = set(list1)
-        set2 = set(list2)
-        common_strings = set1 & set2
-        
-        least_index_sum = float('inf')
+    def findRestaurant(self, list1: list[str], list2: list[str]) -> list[str]:
+        min_sum = float('inf')
+        for i in range(len(list2)):
+            list2[i] = [list2[i], i]
+        list2.sort()
+        res = []
 
-        element_sum = {}
+        def find_word(word):
+            l, r = 0, len(list2) - 1
+            ans = float('inf')
+            while l <= r:
+                mid = l + (r - l)//2
+                if list2[mid][0] == word:
+                    ans = mid
+                    r = mid - 1
+                elif list2[mid][0] > word:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return list2[ans][1] if ans < float('inf') else float('inf')
 
-        for element in common_strings:
-            element_sum[element] = list1.index(element) + list2.index(element)
+        for i in range(len(list1)):
+            idx = find_word(list1[i])
 
+            if idx == float('inf'):
+                continue
+            if idx + i < min_sum:
+                res = [list1[i]]
+                min_sum = idx + i
+            elif idx + i == min_sum:
+                res.append(list1[i])
+            
+        return res
 
-
-        for key, value in element_sum.items():
-            least_index_sum = min(least_index_sum, value)
-
-        return [key for key, value in element_sum.items() if value == least_index_sum]
-        
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
